263787da5480c0271385b4c6bacb6ef29560e031
timrich <tim@rewire.it>
2014-11-25 09:22:39 +0000

    compile less with broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -36,6 +36,5 @@ var app = new EmberApp({
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-app.import('bower_components/semantic-ui/src/theme.less');
 
 module.exports = app.toTree();
