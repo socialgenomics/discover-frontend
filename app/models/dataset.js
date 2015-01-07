eb4922042a7066c664f27801366b9eab634afcd9
timini <tim@rewire.it>
2015-01-07 17:54:53 +0000

    update bower requirements

--- app/models/dataset.js
+++ app/models/dataset.js
@@ -1,12 +1,13 @@
 import DS from 'ember-data';
 
 var Dataset = DS.Model.extend({
-  name: DS.attr('string'),
+  title: DS.attr('string'),
   description: DS.attr('string'),
+  metadata: DS.attr('string'),
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date'),
-  tags: DS.hasMany('tag'),
-  user: DS.belongsTo('user'),
+  //tags: DS.hasMany('tag'),
+  //user: DS.belongsTo('user'),
   //TODO: add properties field with custom datatype - json
 });
 
