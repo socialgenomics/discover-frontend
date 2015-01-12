3e67b487d0eb844910ec54e183dff474ff0fb971
timini <tim@rewire.it>
2015-01-12 18:38:43 +0000

    update proxy and diable json-to-html helper

--- app/helpers/json-html.js
+++ app/helpers/json-html.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 import html from 'npm:json-to-html';
 
 export function jsonHtml(input) {
-  var text = html(JSON.parse(input));
-  return new Ember.Handlebars.SafeString(text);
+  //TODO: this only  accepts a sting at the minute, eve if I pass it json object from the template
+  if (typeof(input) == "string"){
+      input = JSON.parse(input);
+  }
+  var htmlString = html(input);
+  return new Ember.Handlebars.SafeString(htmlString);
 };
 
 export default Ember.Handlebars.makeBoundHelper(jsonHtml);
