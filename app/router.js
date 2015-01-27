7fb8df365d4512c739312adc301300e37a9d77c8
Sebastian Place <sebplace@SP.local>
2015-01-27 14:17:41 +0000

    express server for dummy data

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
 	this.resource('datasets',function(){
-		this.route('dataset',{path:'/:dataset_id'});
+		this.resource('dataset',{path:'/:id'});
 	});
 	
 });
--- app/routes/dataset.js
+++ app/routes/dataset.js
@@ -1,6 +1,6 @@
 import Ember from "ember";
 export default Ember.Route.extend({
 	model: function(params){
-		return this.store.find('dataset',params.dataset_id);
+		return this.store.find('dataset',params.id);
 	}
 });
