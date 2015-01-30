4408362286f8622265834764a9167ac65b0bd66b
Sebastian Place <sebplace94@gmail.com>
2015-01-30 19:31:50 +0000

    modal added, need to link modal to dataset

--- app/controllers/datasets.js
+++ app/controllers/datasets.js
@@ -2,23 +2,23 @@ import Ember from "ember";
 export default Ember.ArrayController.extend({
   datasets: function () {
     //var length = this.get('length');
-    var length = 2;
+    var length = 4;
     return this.map(function (dataset, i) {
       // Checks if it’s the last dataset
-      if ((i + 1) == length) {
+      if ((i + 1) === length) {
         dataset.last = true;
       } else {
         dataset.last = false;
 
         // Checks if it’s the first dataset
-        if (i == 0) {
+        if (i === 0) {
           dataset.first = true;
         } else {
           dataset.first = false;
         }
 
         // Checks if it’s the last dataset of a row
-        if ((i + 1) % 3 == 0) {
+        if ((i + 1) % 3 === 0) {
           dataset.lastOfRow = true;
         } else {
           dataset.lastOfRow = false;
--- app/routes/datasets.js
+++ app/routes/datasets.js
@@ -3,5 +3,24 @@ import Ember from "ember";
 export default Ember.Route.extend({
 	model: function(){
 		return this.store.find('dataset');
+	},
+
+	// Stuff below added
+	actions: {
+	    showModal: function(name, model) {
+	      this.render(name, {
+	        into: 'datasets',
+	        outlet: 'modal',
+	        model: model
+	      });
+          
+	    },
+
+	    removeModal: function() {
+	      this.disconnectOutlet({
+	        outlet: 'modal',
+	        parentView: 'datasets'
+	      });
+	    }
 	}
 });
