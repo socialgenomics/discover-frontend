3daf0e3907f36aac18141477294202ae7e90a739
Sebastian Place <sebplace94@gmail.com>
2015-02-06 15:44:02 +0000

    commented Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -3,9 +3,10 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
-  sassOptions: {
-      inputFile:'prototype.scss',
-  }
+  // Uncomment this & restart server to use the prototype.scss file.
+  // sassOptions: {
+  //     inputFile:'prototype.scss',
+  // }
 });
 
 
