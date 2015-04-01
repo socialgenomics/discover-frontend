3fb05bc371986cac4028963715eac47f7f2e4f92
timini <tim@rewire.it>
2015-04-01 18:16:15 +0100

    add development deploy

--- config/deploy.js
+++ config/deploy.js
@@ -1,4 +1,17 @@
 module.exports = {
+  development: {
+    buildEnv: 'development', // Override the environment passed to the ember asset build. Defaults to 'production'
+    store: {
+      type: 'redis', // the default store is 'redis'
+      host: '192.168.59.103',
+      port: 6379
+    },
+    assets: {
+      accessKeyId: 'AKIAJVMZYC2JCT2XY53Q',
+      secretAccessKey: process.env['AWS_ACCESS_KEY'],
+      bucket: 'testing.repositive.io'
+    }
+  },
   staging: {
     buildEnv: 'staging', // Override the environment passed to the ember asset build. Defaults to 'production'
     store: {
