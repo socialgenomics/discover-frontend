3ce350e551177d0df4907987cade0b5ccb773b50
timini <tim@rewire.it>
2015-04-02 14:15:23 +0100

    readd materalize

--- Brocfile.js
+++ Brocfile.js
@@ -57,4 +57,6 @@ var app = new EmberApp({
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('bower_components/materialize/sass/materialize.scss');
+
 module.exports = app.toTree();
