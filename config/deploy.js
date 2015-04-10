79c36cfb3aa6968e0c45a35c106cbc71e2d24999
timini <tim@rewire.it>
2015-04-10 21:23:26 +0100

    add testing

--- config/deploy.js
+++ config/deploy.js
@@ -12,18 +12,18 @@ module.exports = {
       bucket: 'repositive.io'
     }
   },
-//  staging: {
-//    buildEnv: 'staging', // Override the environment passed to the ember asset build. Defaults to 'production'
-//    store: {
-//      host: 'staging-redis.example.com',
-//      port: 6379
-//    },
-//    assets: {
-//      accessKeyId: 'AKIAJVMZYC2JCT2XY53Q',
-//      secretAccessKey: process.env['AWS_ACCESS_KEY'],
-//      bucket: 'testing.repositive.io'
-//    }
-//  },
+  testing: {
+    buildEnv: 'testing', // Override the environment passed to the ember asset build. Defaults to 'production'
+    store: {
+      host: 'platform.repositive.io',
+      port: 6379,
+    },
+    assets: {
+      accessKeyId: 'AKIAJVMZYC2JCT2XY53Q',
+      secretAccessKey: process.env['AWS_ACCESS_KEY'],
+      bucket: 'testing.repositive.io'
+    }
+  },
   production: {
     store: {
       host: 'platform.repositive.io',
