d40a399a9e761ab6230bf7cdcc481bdb00a8aa0b
timini <tim@rewire.it>
2015-04-10 11:21:16 +0100

    update settings for oauth callback in production

--- config/environment.js
+++ config/environment.js
@@ -3,8 +3,9 @@ var _ = require('underscore');
 
 
 module.exports = function(environment) {
+  var env = environment;
   var APIBaseURL, APIRoutes;
-  if (environment === 'production'){
+  if (env === 'production'){
     APIBaseURL = 'http://api.repositive.io'
   }
   else {
@@ -16,6 +17,10 @@ module.exports = function(environment) {
   }, function(path, key, obj){
     obj[key] =  APIBaseURL + path;
   });
+  var redirectUri = (function(){ 
+    if (env === 'development') { return 'http://localhost:4200'; }
+    else { return 'http://platform.repositive.io'; }
+  }());
   var ENV = {
     modulePrefix: 'repositive.io',
     environment: environment,
@@ -55,11 +60,11 @@ module.exports = function(environment) {
       providers: {
         'google-oauth2': {
           apiKey:      '677526813069-6tmd1flqovp5miud67phqlks49bqdo8i.apps.googleusercontent.com',
-          redirectUri: 'http://localhost:4200'
+          redirectUri: redirectUri
         },
         'linked-in-oauth2': {
           apiKey:      '75fsbvpfgi1667',
-          redirectUri: 'http://localhost:4200'
+          redirectUri: redirectUri
         }
       }
     }
