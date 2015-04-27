ce181d11fbde29e9341ea5ab2c094957d0e192a0
timini <tim@rewire.it>
2015-04-27 20:35:44 +0100

    some templateing for the detailed card view

--- app/models/dataset.js
+++ app/models/dataset.js
@@ -5,4 +5,6 @@ export default DS.Model.extend({
   properties : DS.belongsTo('property'),
   tags: DS.hasMany('tag'),
   comments: DS.hasMany('comments'),
+  createdAt: DS.attr('isodate'),
+  updatedAt: DS.attr('isodate'),
 });
---
+++ app/transforms/isodate.js
@@ -0,0 +1,19 @@
+import DS from "ember-data";
+import moment from "npm:moment";
+
+
+export default DS.Transform.extend({  
+  deserialize: function (serialized) {
+    if (serialized) {
+      return moment(serialized).toDate();
+    }
+    return serialized;
+  },
+
+  serialize: function (deserialized) {
+    if (deserialized) {
+      return moment(deserialized).format();
+    }
+    return deserialized;
+  }
+});
