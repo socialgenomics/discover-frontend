f819cfa7a92bb8a6919ae1f51387656e0dd00849
timini <tim@rewire.it>
2015-04-29 12:09:43 +0100

    updated models

--- app/serializers/dataset.js
+++ app/serializers/dataset.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
-  primaryKey: 'DatasetUuid',
+  primaryKey: 'uuid',
   attrs: {
     properties: {embedded: 'always'},
     tags: {embedded: 'always'}
