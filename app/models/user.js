7013009f6ba0eed9dfdad9fd84ca4f59e8f10f11
timini <tim@rewire.it>
2015-05-06 14:01:17 +0100

    change submodule to correct spelling

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,9 @@
 import DS from "ember-data";
 
-var User = DS.Model.extend({
+export default DS.Model.extend({
 	email: DS.attr('string'),
 	username: DS.attr('string'),
+	firstname: DS.attr('string'),
+	lastname: DS.attr('string'),
   isCurrentUser: DS.attr('boolean', {defaultValue: false})
 });
-
-export default User;
