a8a33c332be994d3ac82ff108c821d2ee1485ef4
Sebastian Place <sebplace94@gmail.com>
2015-05-13 17:06:19 +0100

    unfinished homepage

--- app/controllers/home-page.js
+++ app/controllers/home-page.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  cards: function(){
+    var ids= ["3607ecaf-852d-4cd5-b696-5114edc94f78","80fcd240-c619-47de-ab16-cf0be04c76a7","a5d5443c-a898-4d8f-b642-29f9713a21e2"]
+    let cards = this.store.find('dataset',{ids:ids});
+    return cards;
+  }.property(),
+  actions:{
+
+  }
 });
