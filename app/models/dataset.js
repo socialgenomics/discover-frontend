d7d4140c21e4496d3af0929ef7408397010cf907
timini <tim@rewire.it>
2015-05-21 11:52:47 +0100

    added repository

--- app/models/dataset.js
+++ app/models/dataset.js
@@ -3,6 +3,7 @@ import DS from "ember-data";
 
 export default DS.Model.extend({
   properties : DS.belongsTo('property'),
+  repository: DS.hasMany('repository'),
   tags: DS.hasMany('tag'),
   comments: DS.hasMany('comments'),
   createdAt: DS.attr('isodate'),
