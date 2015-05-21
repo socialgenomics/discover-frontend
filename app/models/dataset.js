7e5a5d01f01ea98c2c047730fe4e17762822de4e
timini <tim@rewire.it>
2015-05-21 12:27:08 +0100

    correct repo relation

--- app/models/dataset.js
+++ app/models/dataset.js
@@ -3,7 +3,7 @@ import DS from "ember-data";
 
 export default DS.Model.extend({
   properties : DS.belongsTo('property'),
-  repository: DS.hasMany('repository'),
+  repository: DS.belongsTo('repository'),
   tags: DS.hasMany('tag'),
   comments: DS.hasMany('comments'),
   createdAt: DS.attr('isodate'),
