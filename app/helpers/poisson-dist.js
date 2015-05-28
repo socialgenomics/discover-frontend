aed6208cc87da6571b447de2ba0fe89d5b9678c0
timini <tim@rewire.it>
2015-05-28 19:06:02 +0100

    updated helper

--- app/helpers/poisson-dist.js
+++ app/helpers/poisson-dist.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
+import { poisson } from 'repositive.io/utils/distributions';
 
 export function poissonDist(params/*, hash*/) {
-  return params;
+  let fallback = 1;
+  let lambda = params[0] || fallback;
+  if (typeof lambda[0] !== 'number'){ lambda = fallback; }
+  return poisson(lambda);
 }
 
 export default Ember.HTMLBars.makeBoundHelper(poissonDist);
