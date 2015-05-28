ed7250ebaac3d8b79209f3603d109baf9168886e
Jana Grajciarova <jana@rummble.com>
2015-05-28 17:27:07 +0100

    input field, profile

--- app/controllers/datasets/detail.js
+++ app/controllers/datasets/detail.js
@@ -5,11 +5,24 @@ export default Ember.Controller.extend({
   tab: "comments",
   isPublic:function(){
     var accessType = this.get('model.repository.accessType');
-    if(accessType == "public"){
+    if(accessType === "public"){
       return true;
     }
   }.property('accessType'),
-  
+  isRestricted:function(){
+    var accessType = this.get('model.repository.accessType');
+    var accessIcon;
+    if(accessType === "restricted") {
+      accessIcon = "mdi-action-lock";
+      return true;
+    }
+  }.property('accessType','accessIcon'),
+  isPrivate: function() {
+    var accessType = this.get('model.repository.accessType');
+    if (accessType === "private") {
+      return true;
+    }
+  }.property('accessType'),
   actions: {
     addComment:function(text){
       var cmnt = this.store.createRecord('comment', {
