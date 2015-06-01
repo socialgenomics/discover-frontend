fb126e694390dfeb4b243f5165b9c934c2e220f4
Sebastian Place <sebplace94@gmail.com>
2015-06-01 17:36:57 +0100

    user datasets for profile

---
+++ app/controllers/user/index.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+
+
+  actions:{
+    // filter datasets into registrations & requests
+    getRegistered:function(){
+      // var datasets = this.get('')
+      // var datasets = this.store.find('dataset',{userId:user.get('id')});
+      // console.log(datasets.filterBy('isRequest',true));
+    },
+  }
+
+});
