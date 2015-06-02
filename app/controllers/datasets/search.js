1da2cdfff657e341bdcf0f87001d30365c9bccc5
timini <tim@rewire.it>
2015-06-02 22:11:20 +0100

    change assaykey position

--- app/controllers/datasets/search.js
+++ app/controllers/datasets/search.js
@@ -1,18 +1,30 @@
 import Ember from 'ember';
+import _ from 'npm:underscore';
+import { titleCase } from 'repositive.io/utils/case';
 
 export default Ember.Controller.extend({
-  queryParams: ['q','ordering','assayType','tags',],
+  queryParams: ['q','ordering','assayType','tags','repository'],
   q: null,
   ordering: null,
   assayType: null,
   tags: null,
 
   filters: function(){
-    return [
-      {
-        name: 'Assay Type',
-        value: this.assayType,
-      },
-    ]
-  }.property('assayType'),
+    let _this = this;
+    let filters = this.get('queryParams');
+    filters.removeAt(filters.indexOf('q'))
+    console.log(filters)
+    return _.map(filters, function(param){
+      return {
+        name: titleCase(param),
+        value: _this.get(param),
+      }
+    });
+  }.property('queryParams'),
+
+  isFiltered: function(){
+    let filters = this.get('filters');
+    return filters.filterBy('value')   
+  }.property("filters.@each.value"),
+
 });
