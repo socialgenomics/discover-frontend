1c80d4d8e7b3adcdffe00910334ada5dd5afdabd
timini <tim@rewire.it>
2015-06-02 17:20:30 +0100

    forgot to add

---
+++ app/components/navbar-dropdown.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  currentUser: Ember.computed(function(){
+    return this.get('session.user');
+  }),
+
+  afterRenderEvent: function(){
+    //dropdown initialization
+    this.$('.dropdown-button').dropdown({
+      inDuration: 500,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on click
+      alignment: 'right', // Aligns dropdown to left or right edge (works with constrain_width)
+      gutter: 0, // Spacing from edge
+      belowOrigin: true // Displays dropdown below the button
+    });
+  },
+
+  actions: {
+
+    logout: function(){
+      this.get("session").invalidate();
+    },
+  }
+});
