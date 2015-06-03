942e913fc802f58e0fb9aa568d81e9fdf9d3fdcf
timini <tim@rewire.it>
2015-06-03 21:40:31 +0100

    assay-label on detail view, short description

--- app/models/property.js
+++ app/models/property.js
@@ -6,4 +6,12 @@ export default DS.Model.extend({
   assayType: DS.attr('string'),
   dataset: DS.belongsTo('dataset'),
   downloadURL: DS.attr('string'),
+  shortDescription: function(){
+    let length = 100;
+    let description = this.get('description');
+    if (description.length > length){ 
+      description = description.substr(0, length) + ' ..'; 
+    }
+    return description;
+  }.property('description'),
 });
