0702e40e64289d079a9450841f2894b438595758
timini <tim@rewire.it>
2015-06-04 09:56:17 +0100

    add templates

--- app/router.js
+++ app/router.js
@@ -9,32 +9,33 @@ Router.map(function() {
 
   this.route('root', {path:'/'});
 
-  this.route('help');
   this.route('about');
   this.route('team');
   this.route('faq');
   this.route('press');
+  this.route('jobs');
+//  this.route('help');
 
-  this.resource('users', function(){
-    this.route('signup');
-    this.route('login');
-    this.route('settings');
-    this.route('profile');
-    this.route('references');
-    this.route('trust');
-  });
-
-  this.resource('user', {path: '/user/:username'}, function(){
-  });
-
-  this.resource('datasets', function(){
-    this.route('detail', {path:':id'});
-    this.route('tags', {path:'/tag/:tag'});
-    this.route('assay', {path:'/assay/:assay'});
-    this.route('search');
-    this.route('register');
-    this.route('request');
-  });
+//  this.resource('users', function(){
+//    this.route('signup');
+//    this.route('login');
+//    this.route('settings');
+//    this.route('profile');
+//    this.route('references');
+//    this.route('trust');
+//  });
+//
+//  this.resource('user', {path: '/user/:username'}, function(){
+//  });
+//
+//  this.resource('datasets', function(){
+//    this.route('detail', {path:':id'});
+//    this.route('tags', {path:'/tag/:tag'});
+//    this.route('assay', {path:'/assay/:assay'});
+//    this.route('search');
+//    this.route('register');
+//    this.route('request');
+//  });
 
   this.route('404', { path: '/*path' });
 
