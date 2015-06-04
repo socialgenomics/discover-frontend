1ad7f4107c0219ddbedc2efde36b3912a961ae72
Jana Grajciarova <jana@rummble.com>
2015-06-04 12:01:34 +0100

    cleaning assay filter, results, textarea for froms

--- app/components/navbar-dropdown.js
+++ app/components/navbar-dropdown.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
       inDuration: 500,
       outDuration: 225,
       constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on click
+      hover: false, // Activate on click
       alignment: 'right', // Aligns dropdown to left or right edge (works with constrain_width)
       gutter: 0, // Spacing from edge
       belowOrigin: true // Displays dropdown below the button
