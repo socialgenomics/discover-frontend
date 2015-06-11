dbcc8372092a345c42119ee3bd7a617240572244
Sebastian Place <sebplace94@gmail.com>
2015-06-11 12:25:31 +0100

    comment component css/hbs redo, actions for adding & cancelling

--- app/components/add-comment.js
+++ app/components/add-comment.js
@@ -2,18 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isActive:false,
-  classNames:"add-comment",
+  classNames:"write-comment",
   classNameBindings:['isActive:active'],
   comment:null,
   actions: {
     focusedIn:function(){
       this.set ("isActive", true);
     },
-    // focusedOut:function(){
-    //   this.set ("isActive", false);
-    // },
+    cancel:function(){
+      this.set('isActive',false);
+      this.set('comment', null);
+    },
     addComment:function(){
       this.sendAction('addComment',this.comment);
+      this.send('cancel');
     },
   },
 });
