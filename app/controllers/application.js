12ad6b0790a5b2c39a488c53d26b124d21a1b869
Sebastian Place <sebplace94@gmail.com>
2015-07-09 17:02:13 +0100

    number of comments on cards

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   bgColour: function(){
-    if(['users.login', 'users.signup'].indexOf(this.get('currentPath')) != -1){
+    if(['users.login','users.signup'].indexOf(this.get('currentPath')) != -1){
       return 'white'
     }
     else{
