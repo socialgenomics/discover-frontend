2fe7775ee7215ed089839a68bdb4459374ca2fc2
Sebastian Place <sebplace94@gmail.com>
2015-07-10 16:45:50 +0100

    app-landing-page buttons functional

--- app/routes/users/signup.js
+++ app/routes/users/signup.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // beforeModel:function(){
-  //   //if not verified transition to verify route
-  //   if(!this.controllerFor("Application").get("isVerified")){
-  //     this.transitionTo('verify');
-  //   }
-  // }
+  beforeModel:function(){
+    //if not verified transition to verify route
+    if(!this.controllerFor("Application").get("isVerified")){
+      this.transitionTo('verify');
+    }
+  }
 });
