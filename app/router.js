8374743184caab9675329403f1a3de0bb66ae377
Jana Grajciarova <jana@rummble.com>
2015-07-21 14:15:01 +0100

    bg-color for signup/login fix, filters design

--- app/router.js
+++ app/router.js
@@ -42,4 +42,21 @@ Router.map(function() {
 
 });
 
+Ember.Route.reopen({
+  activate: function() {
+    var cssClass = this.toCssClass();
+    // you probably don't need the application class
+    // to be added to the body
+    if (cssClass != 'application') {
+      Ember.$('body').addClass(cssClass);
+    }
+  },
+  deactivate: function() {
+    Ember.$('body').removeClass(this.toCssClass());
+  },
+  toCssClass: function() {
+    return this.routeName.replace(/\./g, '-').dasherize();
+  }
+});
+
 export default Router;
