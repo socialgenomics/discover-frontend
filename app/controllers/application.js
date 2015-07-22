57c6137c324f17ae431089a8b127d8ff7485177d
Jana Grajciarova <jana@rummble.com>
2015-07-22 12:31:30 +0100

    loading bg-image removed, line-limit on dataset card

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,15 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isVerified:false,
-  bgColour: function(){
-    if(['users.login','users.signup'].indexOf(this.get('currentPath')) != -1){
-      return 'white';
-    }
-    else{
-      return 'bg-grey';
-    }
-  }.property('currentPath'),
-
   isLandingPage:function(){
     var currentPath = this.get('currentPath');
     var isAuthenticated = this.get('session.isAuthenticated');
