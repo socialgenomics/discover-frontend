55d8b916f3352c1a07db45e8005c81e114640c8a
timini <tim@rewire.it>
2015-09-25 18:01:00 +0100

    comments

--- app/routes/datasets/detail.js
+++ app/routes/datasets/detail.js
@@ -5,9 +5,4 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(params){
     return this.store.find('dataset', params.id);
   },
-  afterModel: function(model){
-    model.get('comments').forEach((comment)=>{
-      this.store.query('profile', {UserId: comment.get('UserId')});
-    });
-  }
 });
