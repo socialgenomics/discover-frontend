fa18d65e686dbc10214452f372041afa8378cc6f
timini <tim@rewire.it>
2015-09-25 18:56:20 +0100

    get avatars on comments

--- app/routes/datasets/detail.js
+++ app/routes/datasets/detail.js
@@ -5,4 +5,9 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(params){
     return this.store.find('dataset', params.id);
   },
+  afterModel: function(model){
+    model.get('comments.@each.UserId').forEach((id)=>{
+      this.store.query('profile', {UserId: id});
+    });
+  },
 });
