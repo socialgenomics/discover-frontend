bb1a8c864b68cfe4df1e189006ef460c5d8324db
danielgynn <danielgynn@hotmail.co.uk>
2015-10-23 13:52:48 +0100

    updates

--- app/controllers/users/profile.js
+++ app/controllers/users/profile.js
@@ -5,40 +5,19 @@ export default Ember.Controller.extend({
 
   actions: {
 
-      useGravatar: function() {
-        this.set('model.profile.gravatar', true);
-      },
-      useAvatar: function() {
-        this.set('model.profile.gravatar', false);
-      },
+    useGravatar: function() {
+      this.set('model.profile.gravatar', true);
+    },
+    useAvatar: function() {
+      this.set('model.profile.gravatar', false);
+    },
 
-      save:function(model){
+    save:function(){
+      this.get('model.user').save(),
+      this.get('model.profile').save().then((model)=>{
         this.set('saved', true);
-
-        this.get('model.user').save(),
-        this.get('model.profile').save().then((model)=>{
-          console.log(model + " SAVED");
-        });
-      },
-
-
-      // save:function(){
-      //   let user = this.get('model.user');
-      //   let profile = this.get('model.profile');
-      //
-      //   user.save(),
-      //   profile.save().then((values)=>{
-      //     this.set('saved', true);
-      //     return {
-      //       user: values[0],
-      //       profile: values[1].get('firstObject'),
-      //     };
-      //   })
-      //   .catch((err)=>{
-      //     Ember.Logger.error(err);
-      //   });
-      // },
-
-
+        console.log(model + " SAVED");
+      });
+    },
   },
 });
