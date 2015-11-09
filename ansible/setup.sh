9aae00ba819fccc0ae242fee257b3a24009614e8
Jose Moreira <jose@repositive.io>
2015-11-09 12:26:00 +0000

    Fixed pre setup bug and externalised nodejs role

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -2,7 +2,7 @@
 # Installs Ansible and plugins in the Vagrant VM before running the main
 # provisioner.
 
-DEBIAN_FRONTEND=noninteractive sudo apt-get update && sudo apt-get install -y python-pip
+DEBIAN_FRONTEND=noninteractive sudo apt-get update && sudo apt-get install -y python-pip git
 sudo pip install -q ansible==1.9.4
 cd /home/vagrant/app
 mkdir -p tmp/roles
