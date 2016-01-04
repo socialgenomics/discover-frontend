4f028eaff656bf77f174c9f6f42aa50c8fe18a9c
timini <tim@rewire.it>
2016-01-04 15:54:22 +0000

    use env var for deploy config

--- config/deploy.js
+++ config/deploy.js
@@ -1,6 +1,19 @@
 module.exports = function(env) {
-  // TODO: make this better
-  let deployConfig = require('./' + env + '.json');
-  deployConfig.s3.accessKeyId = process.env.AWS_KEY_ID;
-  deployConfig.s3.secretAccessKey = process.env.AWS_ACCESS_KEY;
+  let deployConfig
+  /*
+    Use ths env var `EMBER_CLI_DEPLOY_CONFIG_PATH` to direct ember-cli-deploy
+    to a deploy configuration file to use.
+   */
+  if (process.env.EMBER_CLI_DEPLOY_CONFIG_PATH) {
+    deployConfig = require(process.env.EMBER_CLI_DEPLOY_CONFIG_PATH)
+  } else {
+    deployConfig = require('./' + env + '.json');
+  }
+  /**
+   * Add secrets from env variables
+   */
+  deployConfig.s3.accessKeyId = deployConfig.s3.accessKeyId || process.env.AWS_KEY_ID;
+  deployConfig.s3.secretAccessKey = deployConfig.s3.accessKeyId || process.env.AWS_ACCESS_KEY;
+
+  return deployConfig;
 };
