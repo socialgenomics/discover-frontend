efa9b5a41372d6820d16dcfbcf4f52318e3fe951
danielgynn <danielgynn@hotmail.co.uk>
2016-01-15 15:02:35 +0000

    fix isOwnProfile bug

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -6,8 +6,11 @@ export default Ember.Controller.extend({
     return this.get('session.secure.user');
   }),
 
+  /*
+  * session.secure returns number and model.user returns string, hence double =
+  */
   isOwnProfile: function() {
-    return this.get('session.secure.user.id') === this.get('model.user.id');
+    return this.get('session.secure.user.id') == this.get('model.user.id');
   }.property('model'),
 
   isEmailValidated: function() {
