af31d3ae7a0fa1f3407802c69a6a33618f8b6b8e
timini <tim@rewire.it>
2016-01-26 15:40:31 +0000

    remove views

--- app/components/mini-dataset.js
+++ app/components/mini-dataset.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNameBindings: ['dataset.isRequest:request'],
 
+  didRender() {
+    this._super(...arguments);
+    this.$('.tooltipped').tooltip({ delay: 300 });
+  },
+
   actions: {
     toggleModal() {
       this.sendAction();
