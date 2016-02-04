0cd8631ecdea69352e0db7d09af8dfb306828eb9
danielgynn <danielgynn@hotmail.co.uk>
2016-02-04 15:39:54 +0000

    Create message-card component

--- app/components/message-card.js
+++ app/components/message-card.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  attributeBindings: ['heading', 'subheading', 'image', 'button', 'link', 'linkText', 'buttonRoute', 'buttonSubRoute', 'buttonAction'],
+
+  currentUser: Ember.computed(function() {
+    return this.get('session.secure.user');
+  }),
+
+  imgsrc: Ember.computed(function() {
+    var image = this.get('image');
+    return '/assets/images/' + image;
+  })
+
 });
