091c8991c2346f0c5be2c6d6fbb50c7504b95cfc
danielgynn <danielgynn@hotmail.co.uk>
2016-02-04 15:41:45 +0000

    Use comp in 404.hbs

--- app/components/message-card.js
+++ app/components/message-card.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  attributeBindings: ['heading', 'subheading', 'image', 'button', 'link', 'linkText', 'buttonRoute', 'buttonSubRoute', 'buttonAction'],
+  attributeBindings: ['heading', 'subheading', 'image', 'button', 'link', 'linkText', 'buttonRoute', 'buttonAction'],
 
   currentUser: Ember.computed(function() {
     return this.get('session.secure.user');
