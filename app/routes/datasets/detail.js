cf27d2de61432249decb37f545e2aabff90a297b
danielgynn <danielgynn@hotmail.co.uk>
2016-02-09 14:26:31 +0000

    Fix dataset routes

--- app/routes/datasets/detail.js
+++ app/routes/datasets/detail.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(params) {
     return this.store.findRecord('dataset', params.id);
   },
@@ -9,8 +10,8 @@ export default Ember.Route.extend({
     //   this.store.query('profile', { UserId: comment.get('UserId') });
     // });
   },
-  actions:{
-    didTransition: function(){
+  actions: {
+    didTransition: function() {
       this.get('metrics').trackPage();
       return true;
     }
