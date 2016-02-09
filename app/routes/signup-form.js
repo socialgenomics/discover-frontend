99de0c033edc6e5097c91f6c341b44357fd96a78
danielgynn <danielgynn@hotmail.co.uk>
2016-02-09 14:45:53 +0000

    don't allow typeform to be seen when authenticated

---
+++ app/routes/signup-form.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  beforeModel: function(transition) {
+    if (this.get('session.isAuthenticated')) {
+      this.transitionTo('root');
+    }
+  }
+});
