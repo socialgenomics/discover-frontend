7b61ea7efb2daffafc759c8b7706defcbcad2702
timini <tim@rewire.it>
2016-02-11 16:23:22 +0000

    posiible fix for #95

--- app/services/session.js
+++ app/services/session.js
@@ -6,21 +6,24 @@ export default SessionService.extend({
   setAuthenticatedUser: Ember.observer('data.authenticated.user', function() {
     if (this.get('isAuthenticated')) {
       let userData = this.get('data.authenticated.user');
-      let profileData = this.get('data.authenticated.profile');
-      let settingData = this.get('data.authenticated.setting');
 
-      let userId = userData.id;
-      let profileId = profileData.id;
-      let settingId = settingData.id;
+      if (!userData) {
+        /*
+          stale session data - force logout
+         */
+        this.invalidate();
+      } else {
+        let userId = userData.id;
 
-      this.get('store')
-      .findRecord('user', userId)
-      .then(user => {
-        // HACK: add the private stuff from the session data.
-        user.set('email', userData.email);
-        user.set('isEmailValidated', userData.isEmailValidated);
-        this.set('authenticatedUser', user);
-      });
+        this.get('store')
+        .findRecord('user', userId)
+        .then(user => {
+          // HACK: add the private stuff from the session data.
+          user.set('email', userData.email);
+          user.set('isEmailValidated', userData.isEmailValidated);
+          this.set('authenticatedUser', user);
+        });
+      }
     }
   })
 });
