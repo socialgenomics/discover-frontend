c8dd78a43c9e03dfa087840ddf4c9ef02a58e258
timini <tim@rewire.it>
2016-02-12 18:09:56 +0000

    hcak fixed #91

--- app/routes/root.js
+++ app/routes/root.js
@@ -14,9 +14,17 @@ export default Ember.Route.extend({
         this.store.query('dataset', { repository: 'REPOSITIVE', isRequest: false })
       ])
       .then(data => {
+        // this.get('store').pushPayload(data[1].datasets);
+        // let trending = data[1].datasets.map(ds => {
+        //   return this.store.peekRecord('dataset', ds.id);
+        // });
+        // HACK - pushPayload isnt working so get the dataset again!!
+        let trending = data[1].datasets.map(ds => {
+          return this.store.findRecord('dataset', ds.id);
+        });
         return {
           stats: data[0],
-          datasets: data[1].datasets,
+          datasets: trending,
           requests: data[2],
           registered: data[3]
         };
