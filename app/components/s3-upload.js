aaa941b9e9a8875a3bd95b51c342cad9a703cf82
timini <tim@rewire.it>
2016-02-12 15:56:38 +0000

    linting

--- app/components/s3-upload.js
+++ app/components/s3-upload.js
@@ -3,17 +3,15 @@ import EmberUploader from 'ember-uploader';
 
 export default EmberUploader.FileField.extend({
 
-  url: "/api/sign",
+  url: '/api/sign',
 
   filesDidChange: (function() {
-    let uploadUrl = this.get('url'),
-        files = this.get('files'),
-        uploader = EmberUploader.S3Uploader.create({
-          url: uploadUrl
-        });
+    let uploadUrl = this.get('url');
+    let files = this.get('files');
+    let uploader = EmberUploader.S3Uploader.create({ url: uploadUrl });
 
-    if(!Ember.isEmpty(files))
+    if (!Ember.isEmpty(files)) {
       uploader.upload(files[0]);
-
+    }
   }).observes('files')
 });
