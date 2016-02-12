aecb6579799cd3bb373a35b430dbbf679ff38097
timini <tim@rewire.it>
2016-02-12 18:23:06 +0000

    oilerplate

--- app/controllers/users/profile.js
+++ app/controllers/users/profile.js
@@ -1,12 +1,29 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(
+  EmberValidations,
+{
   session: Ember.inject.service(),
+  firstname: null,
+  lastname: null,
   saved: false,
+  validations: {
+    firstname: {
+      presence: {
+        message: 'Can\'t be blank.'
+      }
+    },
+    lastname: {
+      presence: {
+        message: 'can\'t be blank'
+      },
+    },
+  }
   actions: {
     save: function() {
       this.get('session.authenticatedUser').save();
-      //this.get('session.authenticatedUser.profile').save();
+      this.get('session.authenticatedUser.profile').save();
       this.set('saved', true);
     }
   }
