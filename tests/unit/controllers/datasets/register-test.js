310d43524ada1c13f6a8fc8fc0d013afb0f907b7
timini <tim@rewire.it>
2016-02-16 14:41:04 +0000

    remove a load of unittests and fix some with ember metrics injections

--- tests/unit/controllers/datasets/register-test.js
+++ tests/unit/controllers/datasets/register-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleFor('controller:datasets/register', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:metrics']
 });
 
 // Replace this with your real tests.
