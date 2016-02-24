socialgenomics/discover-frontend

commit dabec6da7c735779e244c718f3dd167f3ded80c4

calq test

app/controllers/users/login.js
@@ -48,7 +48,7 @@ export default Ember.Controller.extend(
   actions: {
     submitForm: function() {
       // fix for password manager bug
-      $('#login-form').find('input').trigger('change');
+      this.$('#login-form').find('input').trigger('change');
 
       this.set('loading', true);
       this.set('formSubmitted', true);

app/app.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import Resolver from './resolver';
-import loadInitializers from 'ember/load-initializers';
+import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 let App;

app/metrics-adapters/calq.js
@@ -92,5 +92,9 @@ export default BaseAdapter.extend({
   },
 
   alias() {
+  },
+
+  willDestroy() {
+    delete window.calq;
   }
 });

tests/unit/metrics-adapters/calq-test.js
@@ -1,12 +1,35 @@
 import { moduleFor, test } from 'ember-qunit';
+import sinon from 'sinon';
+
+let sandbox, config;
 
 moduleFor('metrics-adapter:calq', 'calq adapter', {
-  // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
+  beforeEach() {
+    sandbox = sinon.sandbox.create();
+    config = {
+      id: 'GSN-092810-X'
+    };
+  },
+  afterEach() {
+    sandbox.restore();
+  }
 });
 
 // Replace this with your real tests.
-test('it exists', function(assert) {
-  var adapter = this.subject();
-  assert.ok(adapter);
+test('#identify calls `calq.identify` with the right arguments', function(assert) {
+  const adapter = this.subject({ config });
+  const stub1 = sandbox.stub(window.calq.user, 'identify', () => {
+    return true;
+  });
+  const stub2 = sandbox.stub(window.calq.user, 'profile', () => {
+    return true;
+  });
+  adapter.identify({
+    email: 'la@la.com',
+    inviteCode: 'aaaa',
+    firstname: 'bbbb',
+    lastname: 'cccc',
+    username: 1
+  });
+  assert.ok(stub1.calledWith(1), 'it sends the correct arguments');
 });

tests/unit/mixins/google-analytics-tracking-mixin-test.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
-import GoogleAnalyticsTrackingMixinMixin from '../../../mixins/google-analytics-tracking-mixin';
+import TrackingMixinMixin from '../../../mixins/tracking-mixin';
 import { module, test } from 'qunit';
 
-module('Unit | Mixin | google analytics tracking mixin');
+module('Unit | Mixin | tracking mixin');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  var GoogleAnalyticsTrackingMixinObject = Ember.Object.extend(GoogleAnalyticsTrackingMixinMixin);
-  var subject = GoogleAnalyticsTrackingMixinObject.create();
+  var TrackingMixinObject = Ember.Object.extend(TrackingMixinMixin);
+  var subject = TrackingMixinObject.create();
   assert.ok(subject);
 });

tests/unit/utils/distributions-test.js
@@ -1,12 +1,10 @@
-import {
-  poissonDist
-} from '../../../helpers/poisson-dist';
+import { poisson } from '../../../utils/distributions';
 import { module, test } from 'qunit';
 
-module('PoissonDistHelper');
+module('Unit | Utility | distributions');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  var result = poissonDist(42);
+  let result = poisson(100);
   assert.ok(result);
 });

commit f647dd987c85fc4b509dc99c6014e027ef22a6d3

Fix login issue introduced by dabec6d

app/controllers/users/login.js
@@ -48,7 +48,7 @@ export default Ember.Controller.extend(
   actions: {
     submitForm: function() {
       // fix for password manager bug
-      this.$('#login-form').find('input').trigger('change');
+      Ember.$('#login-form').find('input').trigger('change');
 
       this.set('loading', true);
       this.set('formSubmitted', true);

tests/acceptance/user-can-login-test.js
@@ -15,7 +15,7 @@ module('Acceptance | user can login', {
 test('user can login and go to homepage', function(assert) {
   visit('/users/login');
   andThen(function() {
-    fillIn('input[type="email"]', 'test@repositive.io');
+    fillIn('input[type="email"]', 'test1@repositive.io');
     fillIn('input[type="password"]', '12345678');
     click('button:contains("LOGIN")');
     andThen(function () {