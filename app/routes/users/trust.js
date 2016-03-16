fba09a10f75e9556cbef23c7c231eed4d2bd0bd4
Jose Moreira <jose@repositive.io>
2016-03-16 16:12:01 +0000

    Fixed harcoding of email validation resend endpoint

--- app/routes/users/trust.js
+++ app/routes/users/trust.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import ajax from 'ic-ajax';
+import ENV from 'repositive/config/environment';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
@@ -10,7 +11,8 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     resendVerifyEmail: function() {
       this.set('sentEmail', true);
       ajax({
-        url: '/api/users/verify/resend/' + this.get('session.authenticatedUser.email'), // t,
+        url: ENV.APIRoutes['verify-email-resend'] + '/' + this.get('session.authenticatedUser.email'),
+        //url: '/api/users/verify/resend/' + this.get('session.authenticatedUser.email'), // t,
         type: 'GET'
       })
       .then(() => {
