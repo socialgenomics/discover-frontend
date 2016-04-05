5726860017ca3f4bba01f3ef7a370225afd4f738
danielgynn <danielgynn@hotmail.co.uk>
2016-04-05 11:20:24 +0100

    Setup signup routing

--- app/routes/onboard-form.js
+++ app/routes/onboard-form.js
@@ -5,9 +5,5 @@ export default Ember.Route.extend({
   session: Ember.inject.service(),
 
   actions: {
-    // user clicks button on welcome page to enter site
-    completeForm: function() {
-      this.transitionTo('welcome');
-    }
   }
 });
