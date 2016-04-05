0f5e2bf2ca7922cc0b333ccd730c20423ec7f383
danielgynn <danielgynn@hotmail.co.uk>
2016-04-05 10:47:19 +0100

    New signup page

--- app/router.js
+++ app/router.js
@@ -20,7 +20,9 @@ Router.map(function() {
   this.route('signup-form');
   this.route('beta-signup-form');
   this.route('video');
-  this.route('onboard-form', { path: 'onboard' });
+  this.route('onboard-form', {
+    path: 'onboard'
+  });
 
   this.route('users', { resetNamespace: true }, function() {
     this.route('signup');
