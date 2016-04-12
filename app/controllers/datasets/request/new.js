c21e791b28d69a426e98a1552fc6e2189a3b0b71
danielgynn <danielgynn@hotmail.co.uk>
2016-04-12 11:07:20 +0100

    Move controller logic to new

--- app/controllers/datasets/request/new.js
+++ app/controllers/datasets/request/new.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend(
-  EmberValidations,
-{
+export default Ember.Controller.extend(EmberValidations, {
   title: null,
   description: null,
   validations: {
