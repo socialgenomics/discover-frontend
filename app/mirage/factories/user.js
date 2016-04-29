4ce91110b5a9e965ec3ccbc9ecced561c7444c5e
danielgynn <danielgynn@hotmail.co.uk>
2016-04-29 16:53:29 +0100

    Remove mirage (tmp)

--- app/mirage/factories/user.js
+++
@@ -1,8 +0,0 @@
-import Mirage, { faker } from 'ember-cli-mirage';
-
-export default Mirage.Factory.extend({
-  email: 'test1@repositive.io',
-  firstname: 'Test',
-  lastname: 'Name',
-  password: '12345678'
-});
