d49a0bd9e0d969dfe7ad78d65d33cc41389544c8
Kirill Shavaran <alltouch@ukr.net>
2016-05-20 11:50:06 +0300

    fix other tests

--- app/components/markdown-modal.js
+++ app/components/markdown-modal.js
@@ -5,5 +5,8 @@ export default Ember.Component.extend({
     toggleMarkdownModal: function() {
       this.sendAction();
     }
-  }
+  },
+  destinationElementId: function () {
+    return Ember.testing ? 'ember-testing' : undefined;
+  }.property()
 });
