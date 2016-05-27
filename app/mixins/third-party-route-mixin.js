86797104c458e243874347d69a0bd330e6eb2f5d
Kirill Shavaran <alltouch@ukr.net>
2016-05-27 11:34:39 +0300

    enable google& linkedin login buttons

--- app/mixins/third-party-route-mixin.js
+++ app/mixins/third-party-route-mixin.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
-import Base from 'ember-simple-auth/authenticators/base';
-import Torii from 'ember-simple-auth/authenticators/torii';
-import ajax from 'ic-ajax';
-import ENV from 'repositive/config/environment';
 
 export default Ember.Mixin.create({
   session: Ember.inject.service(),
@@ -10,13 +6,11 @@ export default Ember.Mixin.create({
   actions: {
 
     thirdPartyAuth: function(providerName) {
-      this.get('torii').open(providerName)
-      .then((data)=> {
-        this.get('session').authenticate('authenticator:repositive', data);
-      })
-      .catch((err)=> {
-        Ember.Logger.error(err.trace);
-      });
+      this.get('session')
+          .authenticate('authenticator:torii', providerName)
+          .catch((err)=> {
+            Ember.Logger.error(err.trace);
+          });
     }
   }
 });
