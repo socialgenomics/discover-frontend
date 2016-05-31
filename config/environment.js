a76708eb85c74883665a54c1b1c6cfa50517ef20
danielgynn <danielgynn@hotmail.co.uk>
2016-05-31 12:16:00 +0100

    Enable gs in staging

--- config/environment.js
+++ config/environment.js
@@ -122,7 +122,7 @@ module.exports = function(environment) {
         },
         {
           name: 'Gosquared',
-          environments: ['production'],
+          environments: ['staging', 'production'],
           config: {
             token: 'GSN-041822-M',
             signature: '5157e5f51f0967aee05141aff8037b69'
