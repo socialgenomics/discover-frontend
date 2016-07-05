f22f157bfc7d2a655502aa441bdb3bc3246dbb8e
danielgynn <danielgynn@hotmail.co.uk>
2016-07-05 15:49:31 +0100

    Fix settings route

--- app/routes/users/user_settings.js
+++ app/routes/users/user_settings.js
@@ -3,7 +3,19 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
-  model: function() {
-    return this.store.queryRecord('user_settings', { UserId: this.get('session.data.authenticatedUser.id') });
+
+  model: function(params) {
+    return this.store.query('user', { id: params.id }).then(users => {
+      let user = users.get('firstObject');
+      return new Ember.RSVP.all([
+        this.store.query('userSetting', { user_id: user.get('id') })
+      ])
+      .then(values => {
+        return {
+          user: user,
+          user_settings: values[0].get('firstObject')
+        };
+      });
+    });
   }
 });
