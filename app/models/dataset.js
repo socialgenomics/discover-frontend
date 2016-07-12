f2e05120eac7f46b449f8d1a6de67affc1b4ab1d
Ruben Paz <me@ruben.io>
2016-07-12 18:25:25 +0100

    Add url no dataset model

--- app/models/dataset.js
+++ app/models/dataset.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
   userId: DS.belongsTo('user'),
   title: DS.attr('string'),
   description: DS.attr('string'),
+  url: DS.attr('string'),
   tech: DS.attr('string'),
   assay: DS.attr('string'),
   //properties : DS.belongsTo('property'),
--- app/packages/search/mappings.js
+++ app/packages/search/mappings.js
@@ -1,7 +1,6 @@
 export default {
   'assay': 'assay',
   'access': 'access',
-  'datasourceId': 'datasource.id',
   'datasource': 'datasource.short_name',
   'tags': 'dataset.tags'
 };
