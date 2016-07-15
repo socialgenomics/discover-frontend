58cc9c01f96c311f5de3f1ddcea1098c469087c1
danielgynn <danielgynn@hotmail.co.uk>
2016-07-15 11:38:26 +0100

    Activate third-party login icons

--- app/mixins/third-party-route-mixin.js
+++ app/mixins/third-party-route-mixin.js
@@ -4,7 +4,6 @@ export default Ember.Mixin.create({
   session: Ember.inject.service(),
 
   actions: {
-
     thirdPartyAuth: function(providerName) {
       this.get('session')
       .authenticate('authenticator:torii', providerName)
