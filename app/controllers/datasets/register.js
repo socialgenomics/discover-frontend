a6053d0f08b825d94a81658e89df1205a6adfafe
Sebastian Place <sebplace94@gmail.com>
2016-07-21 13:41:13 +0100

    Refactor register.js to make use of composability

--- app/controllers/datasets/register.js
+++ app/controllers/datasets/register.js
@@ -30,13 +30,14 @@ export default Ember.Controller.extend(
     addDataset: function() {
       if (this.get('isValid')) {
         this.set('loading', true);
+        //TODO: Remove hard-coded URL - Define it in config/environment
         ajax({ url: 'https://backend-dev.repositive.io/datasources?short_name=REPOSITIVE', type: 'GET' })
         .then(datasource => {
           return this.store.findRecord('datasource', datasource.datasources[0].id);
         })
         .then(datasourceModel => {
           const userModel = this.get('session.authenticatedUser');
-          const dataset = this.store.createRecord('dataset', {
+          return this.store.createRecord('dataset', {
             title: this.title,
             description: this.description,
             url: this.url,
@@ -44,24 +45,32 @@ export default Ember.Controller.extend(
             externalID: userModel.id + Date.now(),
             userId: userModel
           });
-          dataset.save()
-          .then((created) => {
-            this.flashMessages.add({
-              message: 'Dataset successfully registered',
-              type: 'success',
-              timeout: 7000,
-              class: 'fadeInOut'
-            });
-            this.transitionToRoute('datasets.detail', created.id);
-            this.get('metrics').trackEvent({
-              category: 'dataset',
-              action: 'register',
-              label: created.get('id')
-            });
+        })
+        .then(dataset =>{
+          return dataset.save();
+        })
+        .then(created => {
+          this.flashMessages.add({
+            message: 'Dataset successfully registered',
+            type: 'success',
+            timeout: 7000,
+            class: 'fadeInOut'
+          });
+          this.transitionToRoute('datasets.detail', created.id);
+          this.get('metrics').trackEvent({
+            category: 'dataset',
+            action: 'register',
+            label: created.get('id')
           });
         })
         .catch(err => {
           this.set('loading', false);
+          this.flashMessages.add({
+            message: 'Oh dear. There was a problem registering your dataset.',
+            type: 'warning',
+            timeout: 7000,
+            class: 'fadeInOut'
+          });
           Ember.Logger.error(err);
         });
       }
