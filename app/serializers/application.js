0b9c454471a812a2877dac612851a21e94d70b6b
Sebastian Place <sebplace94@gmail.com>
2016-07-22 13:56:43 +0100

    Serialization method to remove root element of JSON request #259

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -18,5 +18,22 @@ export default DS.RESTSerializer.extend({
   },
   keyForAttribute: function(attr, method) {
     return Ember.String.underscore(attr).toLowerCase();
+  },
+  serializeIntoHash: function(hash, type, record, options) {
+    Ember.merge(hash, this.serialize(record, options));
   }
+  // normalizeSingleResponse: function(store, primaryModelClass, payload, id, requestType) {
+  //   let typeKey = primaryModelClass.modelName;
+  //   payload[typeKey] = payload;
+  //   //delete payload['data'];
+  //   console.log(payload);
+  //   return this._normalizeResponse(store, primaryModelClass, payload, id, requestType, true);
+  // },
+  // normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+  //   let newPayload = {
+  //     [primaryModelClass.modelName] : payload
+  //   };
+  //   console.log(newPayload);
+  //   return this._super(store, primaryModelClass, newPayload, id, requestType);
+  // }
 });
