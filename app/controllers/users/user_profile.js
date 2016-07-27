5bd45028d3ca2ae4fcf48ec2c004a994fe84eb87
Sebastian Place <sebplace94@gmail.com>
2016-07-27 15:29:10 +0100

    Update profile edit logic. Closes #258

--- app/controllers/users/user_profile.js
+++ app/controllers/users/user_profile.js
@@ -7,8 +7,7 @@ export default Ember.Controller.extend(
   session: Ember.inject.service(),
   firstname: Ember.computed.alias('session.authenticatedUser.firstname'),
   lastname: Ember.computed.alias('session.authenticatedUser.lastname'),
-  affiliation: Ember.computed.alias('session.authenticatedUser.user_profile.work_organisation'),
-  saved: false,
+  affiliation: Ember.computed.alias('session.authenticatedUser.userProfile.workOrganisation'),
   validations: {
     firstname: {
       presence: {
@@ -23,9 +22,28 @@ export default Ember.Controller.extend(
   },
   actions: {
     save: function() {
-      this.get('session.authenticatedUser').save();
-      //this.get('session.authenticatedUser.profile').save();
-      this.set('saved', true);
+      console.log(this.get('affiliation'));
+      this.get('session.authenticatedUser').save()
+      .then((resp) => {
+        return resp.profile.save();
+      })
+      .then(() => {
+        this.flashMessages.add({
+          message: 'Your profile has been updated.',
+          type: 'success',
+          timeout: 7000,
+          class: 'fadeInOut'
+        });
+      })
+      .catch((err) => {
+        Ember.Logger.error(err);
+        this.flashMessages.add({
+          message: 'Sorry. There was a problem saving your changes.',
+          type: 'warning',
+          timeout: 7000,
+          class: 'fadeInOut'
+        });
+      });
     }
   }
 });
