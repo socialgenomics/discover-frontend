e40705f99389b077518037b6a0be0f2338d4628a
Sebastian Place <sebplace94@gmail.com>
2016-07-28 16:38:42 +0100

    Refactor user_settings route.

--- app/routes/users/user_settings.js
+++ app/routes/users/user_settings.js
@@ -4,18 +4,22 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
 
-  model: function(params) {
-    return this.store.query('user', { id: params.id }).then(users => {
-      let user = users.get('firstObject');
+  model: function() {
+    const userId = this.get('session.authenticatedUser.id');
+    return this.store.findRecord('user', userId)
+    .then(user => {
       return new Ember.RSVP.all([
+        user,
         this.store.query('userSetting', { user_id: user.get('id') })
-      ])
-      .then(values => {
-        return {
-          user: user,
-          user_settings: values[0].get('firstObject')
-        };
-      });
+      ]);
+    })
+    .then(values => {
+      return {
+        user: values[0],
+        user_settings: values[1].get('firstObject')
+      };
+    }).catch(err => {
+      Ember.Logger.error(err);
     });
   }
 });
