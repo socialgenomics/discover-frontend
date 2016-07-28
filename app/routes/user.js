02cddec2bcabb06bc85c570451c2007b063980ec
Sebastian Place <sebplace94@gmail.com>
2016-07-28 15:55:50 +0100

    Fix user model permission issue. Flatten Promises.

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,21 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.query('user', { id: params.id }).then(users => {
-      let user = users.get('firstObject');
+    return this.store.findRecord('user', params.id)
+    .then(user => {
       return new Ember.RSVP.all([
+        user,
         this.store.query('userProfile', { user_id: user.get('id') }),
         this.store.query('dataset', { user_id: user.get('id') }),
         this.store.query('request', { user_id: user.get('id') })
-      ])
-      .then(values => {
-        return {
-          user: user,
-          user_profile: values[0].get('firstObject'),
-          registrations: values[1],
-          requests: values[2]
-        };
-      });
+      ]);
+    })
+    .then(values => {
+      return {
+        user: values[0],
+        user_profile: values[1].get('firstObject'),
+        registrations: values[2],
+        requests: values[3]
+      };
+    })
+    .catch(err => {
+      Ember.Logger.err(err);
     });
   }
 });
