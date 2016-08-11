d84b50785522cc876329ad3a6c69598e2a377ba3
Sebastian Place <sebplace94@gmail.com>
2016-08-11 15:31:30 +0100

    Fix isOwnProfile property. Closes #306

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -3,15 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
 
-  isOwnProfile: function() {
+  isOwnProfile: Ember.computed('model.user.id', 'session.authenticatedUser.id', function(){
     const sessionUser = this.get('session.authenticatedUser.id');
     const profileUser = this.get('model.user.id');
     if (sessionUser === profileUser) {
       return true;
     }
-  }.property('id'),
-
-  actions: {
-
-  }
+  })
 });
