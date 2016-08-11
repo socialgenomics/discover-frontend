60e70b1f16a1003e0e60b062dceaccbef405b134
Sebastian Place <sebplace94@gmail.com>
2016-08-11 15:32:57 +0100

    Forgot to make it pretty for JSCS

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
 
-  isOwnProfile: Ember.computed('model.user.id', 'session.authenticatedUser.id', function(){
+  isOwnProfile: Ember.computed('model.user.id', 'session.authenticatedUser.id', function() {
     const sessionUser = this.get('session.authenticatedUser.id');
     const profileUser = this.get('model.user.id');
     if (sessionUser === profileUser) {
