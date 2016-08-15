54aa31210f3320b2d31bd7572e99b3d57dd66640
danielgynn <danielgynn@hotmail.co.uk>
2016-08-15 12:33:34 +0100

    Fix for Typeform flow on third-party signup #263

--- app/mixins/third-party-route-mixin.js
+++ app/mixins/third-party-route-mixin.js
@@ -5,8 +5,12 @@ export default Ember.Mixin.create({
 
   actions: {
     thirdPartyAuth: function(providerName) {
-      this.get('session')
-      .authenticate('authenticator:torii', providerName)
+      this.get('session').authenticate('authenticator:torii', providerName)
+      .then(() => {
+        this.get('session').set('data.firstVisit', true)
+        .then(() => this.get('session').set('data.displayWelcomeMessage', false))
+        .catch(this.displayMessage.bind(this));
+      })
       .catch((err)=> {
         Ember.Logger.error(err.trace);
       });
