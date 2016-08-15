63cbe2742643013eafdbe440bead12ad66f495d2
danielgynn <danielgynn@hotmail.co.uk>
2016-08-15 12:48:01 +0100

    Check if third-party auth is signup or login

--- app/mixins/third-party-route-mixin.js
+++ app/mixins/third-party-route-mixin.js
@@ -4,12 +4,14 @@ export default Ember.Mixin.create({
   session: Ember.inject.service(),
 
   actions: {
-    thirdPartyAuth: function(providerName) {
+    thirdPartyAuth: function(providerName, type) {
       this.get('session').authenticate('authenticator:torii', providerName)
       .then(() => {
-        this.get('session').set('data.firstVisit', true)
-        .then(() => this.get('session').set('data.displayWelcomeMessage', false))
-        .catch(this.displayMessage.bind(this));
+        if (type === 'signup') {
+          this.get('session').set('data.firstVisit', true)
+          .then(() => this.get('session').set('data.displayWelcomeMessage', false))
+          .catch(this.displayMessage.bind(this));
+        }
       })
       .catch((err)=> {
         Ember.Logger.error(err.trace);
