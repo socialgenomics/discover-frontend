6830d3cb60ebe0119906f457d4c7915f142aedd7
Sebastian Place <sebplace94@gmail.com>
2016-08-31 10:17:00 +0100

    Fix loading of request commnet images. #388

--- app/initializers/cleancache.js
+++ app/initializers/cleancache.js
@@ -3,15 +3,15 @@ import Ember from 'ember';
 
 export function initialize(application) {
   let version = Ember.libraries._getLibraryByName('Repositive').version.split('+')[0];
-	let previous = localStorage.getItem('version');
+  let previous = localStorage.getItem('version');
 
-	if (version !== previous) {
-		localStorage.clear();
-		localStorage.setItem('version', version);
-	}
+  if (version !== previous) {
+    localStorage.clear();
+    localStorage.setItem('version', version);
+  }
 }
 
 export default {
   name: 'cleancache',
   initialize: initialize
-}
+};
