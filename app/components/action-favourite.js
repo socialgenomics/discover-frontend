e0e04230f9e1d67317b8e895ddefa19697b7c767
Sebastian Place <sebplace94@gmail.com>
2016-09-09 15:29:57 +0100

    Create actions service #296

--- app/components/action-favourite.js
+++ app/components/action-favourite.js
@@ -1,34 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  store: Ember.inject.service(),
-  session: Ember.inject.service(),
-
+  // store: Ember.inject.service(),
+  // session: Ember.inject.service(),
+  actions: Ember.inject.service(),
   tagName: 'a',
   isStarred: false,
 
   classNameBindings: ['isStarred:starred'],
   click() {
-    const store = this.get('store');
-    const currentUser = this.get('session.authenticatedUser');
-    const currentModel = this.model; //can be request or dataset
+    let actionsService = this.get('actions');
+    let favourites = actionsService.get('favouriteList');
+    console.log(favourites);
+    // favourites.content.map(favourite => {
+    //   console.log(favourite._data.properties.value);
+    //   // return favourite;
+    // })
 
-    //TODO:
-    //All user actions will be loaded when the session starts.
-    //Check actions for favourite associated with this dataset/request.
-    //If none -> make one
-    //Else -> toggle properties.value
-
-    //get the actions associated with currentModel
-    //load actionable for model
-
-    store.findRecord('actionable', currentModel.id)
-    .then(actionable => {
-      console.log(actionable);
-    })
-    .catch(err => {
-      Ember.logger.error(err);
-    });
+    // const store = this.get('store');
+    // const currentUser = this.get('session.authenticatedUser');
+    // const currentModel = this.model; //can be request or dataset
+    //
+    // //TODO:
+    // //All user actions will be loaded when the session starts.
+    // //Check actions for favourite associated with this dataset/request.
+    // //If none -> make one
+    // //Else -> toggle properties.value
+    //
+    // //get the actions associated with currentModel
+    // //load actionable for model
+    //
+    // store.findRecord('actionable', currentModel.id)
+    // .then(actionable => {
+    //   console.log(actionable);
+    // })
+    // .catch(err => {
+    //   Ember.logger.error(err);
+    // });
 
 
     // --- Create new favourite ---
---
+++ app/services/actions.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+const { inject: { service }, Service, computed } = Ember;
+
+export default Service.extend({
+  store: service(),
+  session: service(),
+  favouriteList: computed('session.authenticatedUser', 'store', function() {
+    const currentUser = this.get('session.authenticatedUser');
+    const store = this.get('store');
+    let favourites = store.query('action', {
+      user_id: currentUser.id,
+      type: 'favourite'
+    })
+    .then(resp => {
+      return resp;
+    })
+    return favourites;
+  })
+});
