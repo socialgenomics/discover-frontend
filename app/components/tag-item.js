f9484d2dccd3bb92ac350207a8ac6724a55fe97b
Sebastian Place <sebplace94@gmail.com>
2016-09-22 14:56:42 +0100

    Updated tag styling. Fix tag layout. #410

--- app/components/tag-item.js
+++ app/components/tag-item.js
@@ -4,6 +4,8 @@ const { inject: { service }, Component } = Ember;
 export default Component.extend({
   session: service(),
   actionsService: service('actions'),
+  tagName: 'span',
+  classNames: ['tag'],
 
   belongsToUser: Ember.computed('session', function () {
     const currentUserId = this.get('session.session.authenticated.user.id');
