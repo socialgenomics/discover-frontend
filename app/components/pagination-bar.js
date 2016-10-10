c55adc076d8f1bcc7e63bc3a8f0de9ab9b1ee625
Sebastian Place <sebplace94@gmail.com>
2016-10-10 15:36:31 +0100

    Decouple model from pagination component https://github.com/repositive/report-issues/issues/75. Limit searches to 9 results. Closes #417.

--- app/components/pagination-bar.js
+++ app/components/pagination-bar.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  resultsPerPage: 9,
+  totalPages: Ember.computed('totalResults', 'resultsPerPage', function() {
+    const resultsPerPage = this.get('resultsPerPage');
+    const totalResults = this.get('totalResults');
+    return Math.ceil(totalResults / resultsPerPage);
+  }),
+  currentPageNumber: Ember.computed('currentOffset', 'resultsPerPage', function() {
+    const resultsPerPage = this.get('resultsPerPage');
+    const offset = this.get('currentOffset') || 0;
+    return Math.ceil(offset / resultsPerPage) + 1;
+  }),
+
+  actions: {
+    nextPage() {
+      this.nextPage();
+    },
+    previousPage() {
+      this.previousPage();
+    }
+  }
 });
--- app/controllers/datasets/search.js
+++ app/controllers/datasets/search.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import searchMixin from 'repositive/packages/search/controllerMixin';
 
-
 export default Ember.Controller.extend(
   searchMixin,
   Ember.Evented,
@@ -12,29 +11,6 @@ export default Ember.Controller.extend(
   assay: null,
   datasource: null,
   access: null,
-  resultsPerPage: 30,
-  totalPages: Ember.computed('model.meta.total', 'resultsPerPage', function() {
-    const resultsPerPage = this.get('resultsPerPage');
-    const totalResults = this.get('model.meta.total');
-    return Math.ceil(totalResults / resultsPerPage);
-  }),
-  currentPageNumber: Ember.computed('model.offset', 'resultsPerPage', function() {
-    const resultsPerPage = this.get('resultsPerPage');
-    const offset = this.get('model.offset') || 0;
-    return Math.ceil(offset / resultsPerPage) + 1;
-  }),
-  //returns list of offsets
-  //element 0 = offset for firstpage etc.
-  pageNumberList: Ember.computed('totalPages', 'resultsPerPage', function() {
-    const totalPages = this.get('totalPages');
-    const resultsPerPage = this.get('resultsPerPage');
-    let pageList = [];
-    for (let i = 0; i < totalPages; i++) {
-      const pageOffset = i * resultsPerPage;
-      pageList.push(pageOffset);
-    }
-    return pageList;
-  }),
 
   modelLoadingDidChange: function() {
     if (!this.get('model.isLoading')) {
@@ -48,20 +24,12 @@ export default Ember.Controller.extend(
   }.observes('model.isLoading'),
 
   actions: {
-    changePage(pageNumber) {
-      const pageNumberList = this.get('pageNumberList');
-      this.set('model.isLoading', true);
-      this.get('model.datasets').clear();
-      this.get('model').updateOffset(pageNumberList[pageNumber - 1]);
-    },
-
     previousPage() {
       const resultsPerPage = this.get('resultsPerPage');
       this.set('model.isLoading', true);
       this.get('model.datasets').clear();
       this.get('model').updateOffset(resultsPerPage, 'decrement');
     },
-
     nextPage() {
       const resultsPerPage = this.get('resultsPerPage');
       this.set('model.isLoading', true);
