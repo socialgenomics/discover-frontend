db6656a134edae48109cea605f3a751392bcc039
Ruben Paz <me@ruben.io>
2016-10-11 21:02:16 +0200

    Fix filter in datasources list

--- app/routes/datasources/index.js
+++ app/routes/datasources/index.js
@@ -3,6 +3,6 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function() {
-    return this.store.query('collection', { 'type': 'datasource' });
+    return this.store.query('collection', { 'where.type': 'datasource' });
   }
 });
