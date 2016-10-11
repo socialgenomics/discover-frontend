01008eed5741354a3c75be1e0d032a0d0b33dccb
Sebastian Place <sebplace94@gmail.com>
2016-10-11 11:48:35 +0100

    Exclude datasources in collections routes

--- app/routes/collections/index.js
+++ app/routes/collections/index.js
@@ -3,6 +3,10 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function() {
-    return this.store.findAll('collection');
+    return this.store.query('collection', {
+      'where.type.$ne': 'datasource',
+      'offset': 0,
+      'limit': 9
+    });
   }
 });
