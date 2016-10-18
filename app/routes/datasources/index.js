7448fd43c796427d041782efacfbe24a79bdc9df
Ruben Paz <me@ruben.io>
2016-10-18 16:09:44 +0200

    Sort by creation and limit to 100

--- app/routes/datasources/index.js
+++ app/routes/datasources/index.js
@@ -3,6 +3,12 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function() {
-    return this.store.query('collection', { 'where.type': 'datasource' });
+    return this.store.query('collection', {
+      'where.type': 'datasource',
+      'order[0][0]': 'created_at',
+      'order[0][1]': 'DESC',
+      'offset': 0,
+      'limit': 100
+    });
   }
 });
