b962d4d0868b8c711f55b5779f43ccf8464d2d71
Sebastian Place <sebplace94@gmail.com>
2016-10-20 15:03:47 +0100

    isNew logic for featured-list-items

--- app/components/featured-list-item.js
+++ app/components/featured-list-item.js
@@ -1,5 +1,23 @@
 import Ember from 'ember';
+import moment from 'moment';
 
-export default Ember.Component.extend({
-  classNames: ['featured-list-item']
+const { Component } = Ember;
+
+export default Component.extend({
+  classNames: ['featured-list-item'],
+  didReceiveAttrs(){
+    if(this._checkIfNew()){
+      this.set('isNew', true);
+    }
+  },
+  _checkIfNew(){
+    let now = moment(new Date());
+    let dateUpdated = moment(this.get('model.updatedAt'));
+    let daysDifference = now.diff(dateUpdated, 'days');
+    if (daysDifference < 7) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 });
