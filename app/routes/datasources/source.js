aa70aafb135d45c44f0a9ad74334a0c08a886eb9
Ruben Paz <me@ruben.io>
2016-10-21 22:11:54 +0200

    Use temporary endpoint with raw query

--- app/routes/datasources/source.js
+++ app/routes/datasources/source.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import ajax from 'ic-ajax';
 import ENV from 'repositive/config/environment';
+
 export function model(params) {
   let token = this.get('session.session.content.authenticated.token');
   let authHeaders = {
@@ -18,14 +19,10 @@ export function model(params) {
         type: 'GET',
         headers: authHeaders
       }),
-      datasets: this.store.query('dataset', {
-        'include[0][model]': 'dataset_collection',
-        'include[0][where][collection_id]': collectionId,
-        'include[0][required]': true,
-        'offset': params.limit * (params.page - 1),
-        'limit': params.limit,
-        'order[0][0]': 'updated_at',
-        'order[0][1]': 'DESC'
+      datasets: ajax({
+        url: ENV.APIRoutes['collection-datasets'].replace('{collection_id}', collectionId),
+        type: 'GET',
+        headers: authHeaders
       })
     });
   })
