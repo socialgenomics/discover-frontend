3d6572a660f08d8ab8e70557f1d32c09cde8fe0f
Sebastian Place <sebplace94@gmail.com>
2016-10-21 14:44:47 +0100

    Refactor collections index

--- app/routes/collections/index.js
+++ app/routes/collections/index.js
@@ -2,30 +2,24 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import ajax from 'ic-ajax';
 import ENV from 'repositive/config/environment';
-const { inject: { service }, Route, RSVP } = Ember;
+const { Route, RSVP } = Ember;
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Route.extend(AuthenticatedRouteMixin, {
   model: function() {
     if (this.get('session.isAuthenticated')) {
-      let token = this.get('session.session.content.authenticated.token');
-      let authHeaders = {
+      const token = this.get('session.session.content.authenticated.token');
+      const authHeaders = {
         authorization: `JWT ${token}`
       };
-      return RSVP.all([
-        ajax({ url: ENV.APIRoutes['stats'] , type: 'GET', headers: authHeaders }),
-        this.store.query('collection', {
+      return RSVP.hash({
+        stats: ajax({ url: ENV.APIRoutes['stats'] , type: 'GET', headers: authHeaders }),
+        collections: this.store.query('collection', {
           'where.type': 'repositive_collection',
           'order[0][0]': 'created_at',
           'order[0][1]': 'DESC',
           'offset': 0,
           'limit': 100
         })
-      ])
-      .then(data => {
-        return {
-          stats: data[0],
-          collections: data[1]
-        };
       })
       .catch(err => {
         Ember.Logger.error(err);
