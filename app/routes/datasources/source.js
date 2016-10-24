e71d7035bab0e6e542af8b4d45a945fbb32efb2e
Ruben Paz <me@ruben.io>
2016-10-24 03:18:29 +0200

    Changes based on code review

--- app/routes/datasources/source.js
+++ app/routes/datasources/source.js
@@ -3,40 +3,33 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 import ajax from 'ic-ajax';
 import ENV from 'repositive/config/environment';
 
+const storeDatasets = (store) => (datasets) => datasets.map(dataset => store.push(store.normalize('dataset', dataset)));
+
 export function model(params) {
-  let token = this.get('session.session.content.authenticated.token');
-  let authHeaders = {
+  const token = this.get('session.session.content.authenticated.token');
+  const authHeaders = {
     authorization: `JWT ${token}`
   };
 
-  let store = this.store;
+  const store = this.store;
 
-  return store.findRecord('collection', params.id)
-  .then(collection => {
-    const collectionId = collection.get('id');
+  const collectionId = params.id;
+  const limit = params.limit;
+  const offset = limit * (params.page - 1);
+  const datasetsUrl = ENV.APIRoutes['collection-datasets'].replace('{collection_id}', collectionId) + `?limit=${params.limit}&offset=${offset}`;
 
-    let limit = params.limit;
-    let offset = limit * (params.page -1);
-    let datasetsUrl = ENV.APIRoutes['collection-datasets'].replace('{collection_id}', collectionId) + `?limit=${params.limit}&offset=${offset}`;
-    return new Ember.RSVP.hash({
-      collection: collection,
-      collectionStats: ajax({
-        url: ENV.APIRoutes['collection-stats'].replace('{collection_id}', collectionId),
-        type: 'GET',
-        headers: authHeaders
-      }),
-      datasets: ajax({
-        url: datasetsUrl,
-        type: 'GET',
-        headers: authHeaders
-      }).then(datasets => datasets.map(d => {
-        let dataset = store.normalize('dataset', d);
-        return store.push(dataset);
-      }))
-    });
-  })
-  .catch(err => {
-    Ember.Logger.error(err);
+  return new Ember.RSVP.hash({
+    collection: store.findRecord('collection', collectionId),
+    collectionStats: ajax({
+      url: ENV.APIRoutes['collection-stats'].replace('{collection_id}', collectionId),
+      type: 'GET',
+      headers: authHeaders
+    }),
+    datasets: ajax({
+      url: datasetsUrl,
+      type: 'GET',
+      headers: authHeaders
+    }).then(storeDatasets(store))
   });
 }
 
