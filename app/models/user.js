3055ca4f2c9f5c452fe127920060a0e9c0cab172
Sebastian Place <sebplace94@gmail.com>
2016-11-01 10:13:00 +0000

    Refactor displayName cp

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 import Ember from 'ember';
-
+const { computed, isPresent, get } = Ember;
 export default Model.extend({
   username: attr('string'),
   firstname: attr('string'),
@@ -14,13 +14,13 @@ export default Model.extend({
   updatedAt: attr('isodate'),
   isCurrentUser: attr('boolean', { defaultValue: false }), // checks the current authenticated user
   isEmailValidated: attr('boolean', { defaultValue: false }), // checks if email has been validated
-  displayName: function() {
-    if (Ember.isPresent(this.get('firstname')) || Ember.isPresent(this.get('lastname'))) {
-      var fn = this.get('firstname') || '';
-      var ln = this.get('lastname') || '';
-      return fn + ' ' + ln;
+  displayName: computed('username', 'firstname', 'lastname', function() {
+    if (isPresent(get(this, 'firstname')) || isPresent(get(this, 'lastname'))) {
+      const firstName = get(this, 'firstname') || '';
+      const lastName = get(this, 'lastname') || '';
+      return firstName + ' ' + lastName;
     } else {
-      return 'User' + this.get('username');
+      return 'User' + get(this, 'username');
     }
-  }.property('username', 'firstname', 'lastname')
+  })
 });
