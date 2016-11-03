fb500294991ae1fca0c5ba76816ec2ca18e4dafb
Sebastian Place <sebplace94@gmail.com>
2016-11-03 11:20:39 +0000

    Remove jQuery methods. Use vanilla scroll methods.

--- app/mixins/remember-scroll.js
+++ app/mixins/remember-scroll.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
-const { Mixin, get, set, $, run } = Ember;
+const { Mixin, get, set, run } = Ember;
 
 export default Mixin.create({
   activate: function() {
     this._super(...arguments);
     run.next(() => {
-      $(window).scrollTop(get(this, 'previousScrollPosition') || 0);
+      window.scrollTo(0, get(this, 'previousScrollPosition') || 0);
     });
   },
   deactivate: function() {
     this._super(...arguments);
-    set(this, 'previousScrollPosition', $(window).scrollTop());
+    set(this, 'previousScrollPosition', window.scrollY);
   }
 });
