eac414a6eedaf2b0ecf6df35a848f28c529d6e5a
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-11-03 19:51:23 +0100

    ISSUE-165, add new link with target blank extension to showdown

---
+++ app/initializers/register-showdown-extensions.js
@@ -0,0 +1,16 @@
+/* globals showdown */
+
+export function initialize() {
+  showdown.extension('targetBlankLinks', function() {
+    return [{
+      type: 'html',
+      regex: '<a',
+      replace: '<a target="_blank" rel="noopener noreferrer"'
+    }];
+  });
+}
+
+export default {
+  name: 'register-showdown-extensions',
+  initialize
+};
---
+++ tests/unit/initializers/register-showdown-extensions-test.js
@@ -0,0 +1,36 @@
+/* jshint expr:true */
+/* globals showdown */
+
+import { expect } from 'chai';
+import {
+  describe,
+  it,
+  beforeEach
+} from 'mocha';
+import { initialize } from 'repositive/initializers/register-showdown-extensions';
+
+describe('RegisterShowdownExtensionsInitializer', function() {
+  beforeEach(function() {
+    initialize();
+  });
+
+  it('it adds target="_blank" rel="noopener noreferrer" to each link in markdown', function() {
+    const converter = new showdown.Converter({ extensions: ['targetBlankLinks'] });
+    const dataProvider = [
+      {
+        markdown: '[google](google.com)',
+        html: '<a target="_blank" rel="noopener noreferrer" href="google.com">google</a>'
+      },
+      {
+        markdown: '[google](google.com)[google](google.com)[google](google.com)',
+        html: '<a target="_blank" rel="noopener noreferrer" href="google.com">google</a>' +
+        '<a target="_blank" rel="noopener noreferrer" href="google.com">google</a>' +
+        '<a target="_blank" rel="noopener noreferrer" href="google.com">google</a>'
+      }
+    ];
+
+    dataProvider.forEach(dataset => {
+      expect(converter.makeHtml(dataset.markdown)).to.equal(`<p>${dataset.html}</p>`);
+    });
+  });
+});
