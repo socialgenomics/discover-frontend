81134f8d5bf409278d93ab7c247815e0770e8a8b
Sebastian Place <sebplace94@gmail.com>
2016-11-08 16:55:33 +0000

    Refactor route/root

--- app/routes/root.js
+++ app/routes/root.js
@@ -2,23 +2,23 @@ import Ember from 'ember';
 import ajax from 'ic-ajax';
 import ENV from 'repositive/config/environment';
 
-const { inject: { service }, Route, RSVP } = Ember;
+const { inject: { service }, Route, RSVP, get, Logger } = Ember;
 
 export default Route.extend({
   session: service(),
 
   beforeModel: function() {
-    if (this.get('session.data.firstVisit', true) && this.get('session.isAuthenticated')) {
+    if (get(this, 'session.data.firstVisit') && get(this, 'session.isAuthenticated')) {
       this.transitionTo('beta-signup-form')
       .then(() => {
         // Don't display the 'verify email' message if user signed up with third party auth
-        if (!this.get('session.data.thirdPartySignup')) {
-          this.get('session').set('data.displayWelcomeMessage', true);
+        if (!get(this, 'session.data.thirdPartySignup')) {
+          get(this, 'session').set('data.displayWelcomeMessage', true);
         }
       });
     }
 
-    if (this.get('session.data.displayWelcomeMessage', true) && this.get('session.isAuthenticated')) {
+    if (get(this, 'session.data.displayWelcomeMessage') && get(this, 'session.isAuthenticated')) {
       this.flashMessages.add({
         message: 'Please check your email to verify your account',
         type: 'info',
@@ -30,42 +30,36 @@ export default Route.extend({
   },
 
   model: function() {
-    if (this.get('session.isAuthenticated')) {
-      //Get search data
-      //Get trending datasets
-      //Get recent requests and registrations
-      let token = this.get('session.session.content.authenticated.token');
-      let authHeaders = {
+    if (get(this, 'session.isAuthenticated')) {
+      const token = get(this, 'session.session.content.authenticated.token');
+      const authHeaders = {
         authorization: `JWT ${token}`
       };
 
-      return RSVP.all([
-        ajax({ url: ENV.APIRoutes['stats'] , type: 'GET', headers: authHeaders }),
-        ajax({ url: ENV.APIRoutes['datasets.trending'] , type: 'GET', headers: authHeaders }),
-        this.store.query('request', { 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC' }),
-        this.store.query('dataset', { 'where.user_id.$ne': 'null', 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC' }),
-        this.store.query('collection', { 'where.type': 'repositive_collection', 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC', 'limit': '3' }),
-        this.store.query('collection', { 'where.type': 'datasource', 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC', 'limit': '3' })
-      ])
+      return RSVP.hash({
+        stats: ajax({ url: ENV.APIRoutes['stats'] , type: 'GET', headers: authHeaders }),
+        datasets: ajax({ url: ENV.APIRoutes['datasets.trending'] , type: 'GET', headers: authHeaders }),
+        requests: this.store.query('request', { 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC' }),
+        registered: this.store.query('dataset', { 'where.user_id.$ne': 'null', 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC' }),
+        collections: this.store.query('collection', { 'where.type': 'repositive_collection', 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC', 'limit': '3' }),
+        datasources: this.store.query('collection', { 'where.type': 'datasource', 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC', 'limit': '3' })
+      })
       .then(data => {
         //Normalize and push trending datasets
-        let trending = data[1].map((datasetObj) => {
+        let trending = data.datasets.map((datasetObj) => {
           return this.store.push(this.store.normalize('dataset', datasetObj));
         });
 
         return {
-          stats: data[0],
+          stats: data.stats,
           datasets: trending,
-          requests: data[2],
-          registered: data[3],
-          collections: data[4],
-          datasources: data[5]
+          requests: data.requests,
+          registered: data.registered,
+          collections: data.collections,
+          datasources: data.datasources
         };
       })
-      .catch(err => {
-        Ember.Logger.error(err);
-        throw err;
-      });
+      .catch(Logger.error);
     } else {
       return ajax({ url: ENV.APIRoutes['stats'] , type: 'GET' })
       .then(stat => {
@@ -75,6 +69,6 @@ export default Route.extend({
   },
 
   deactivateWeclomeMesssage: function() {
-    this.get('session').set('data.firstVisit', false);
+    get(this, 'session').set('data.firstVisit', false);
   }.on('deactivate')
 });
