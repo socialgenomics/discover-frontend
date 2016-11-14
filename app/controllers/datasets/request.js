7d4fb55de7abac00c6135c79e1e6d63b230d438d
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-11-14 12:46:28 +0100

    ISSUE-204, reset register and request froms on route transition if object was created

--- app/controllers/datasets/request.js
+++ app/controllers/datasets/request.js
@@ -1,58 +1,80 @@
 import Ember from 'ember';
 import EmberValidations from 'ember-validations';
+import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
+import TrackEventsMixin from 'repositive/mixins/track-events-mixin';
 
-export default Ember.Controller.extend(
+const { Controller, computed, inject: { service }, get, getProperties, set, Logger } = Ember;
+const validationErrorMessage = 'This field can\'t be blank.';
+
+export default Controller.extend(
   EmberValidations,
-{
-  session: Ember.inject.service(),
+  FlashMessageMixin,
+  TrackEventsMixin,
+  {
+    session: service(),
 
-  title: null,
-  description: null,
-  loading: false,
-  validations: {
-    title: {
-      presence: { message: 'This field can\'t be blank.' }
-    },
-    description: {
-      presence: { message: 'This field can\'t be blank.' }
-    }
-  },
-  isInvalid: Ember.computed.not('isValid'),
+    title: null,
+    description: null,
+    didRequest: false,
+    loading: false,
 
-  actions: {
-    addRequest: function() {
-      if (this.get('isValid')) {
-        this.store.createRecord('request', {
-          userId: this.get('session.authenticatedUser'),
-          title: this.title,
-          description: this.description
-        })
-        .save()
-        .then((created)=> {
-          this.flashMessages.add({
-            message: 'Request created successfully.',
-            type: 'success',
-            timeout: 7000,
-            class: 'fadeInOut'
-          });
-          this.transitionToRoute('requests.detail', created.id);
-          this.get('metrics').trackEvent({
-            category: 'dataset',
-            action: 'request',
-            label: created.get('id')
-          });
-        })
-        .catch(err => {
-          this.set('loading', false);
-          this.flashMessages.add({
-            message: 'Oh dear. There was a problem submitting your dataset request.',
-            type: 'warning',
-            timeout: 7000,
-            class: 'fadeInOut'
-          });
-          Ember.Logger.error(err);
-        });
+    validations: {
+      title: {
+        presence: { message: validationErrorMessage }
+      },
+      description: {
+        presence: { message: validationErrorMessage }
       }
+    },
+
+    isInvalid: computed.not('isValid'),
+
+    actions: {
+      addRequest: function() {
+        if (get(this, 'isValid')) {
+          this._createRequest().then(this._createRequestSuccess.bind(this))
+          .catch(this._createRequestError.bind(this));
+        }
+      }
+    },
+
+    /**
+     * @desc creates new dataset request and saves it in backend
+     * @returns {Promise}
+     * @private
+     */
+    _createRequest() {
+      const { description, title } = getProperties(this, 'title', 'description');
+
+      return this.store.createRecord('request', {
+        userId: get(this, 'session.authenticatedUser'),
+        title,
+        description
+      }).save();
+    },
+
+    /**
+     * @desc create request promise resolve callback
+     * @param {DS.Model} request
+     * @private
+     */
+    _createRequestSuccess(request) {
+      const id = get(request, 'id');
+
+      set(this, 'didRequest', true);
+      this.addFlashMessage('Request created successfully.', 'success');
+      this.transitionToRoute('requests.detail', id);
+      this.trackEvent('dataset', 'request', id);
+    },
+
+    /**
+     * @desc create request promise reject callback
+     * @param {Error} error
+     * @private
+     */
+    _createRequestError(error) {
+      set(this, 'loading', false);
+      this.addFlashMessage('Oh dear. There was a problem submitting your dataset request.', 'warning');
+      Logger.error(error);
     }
-  }
-});
+  });
