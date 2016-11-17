25b016f11ab3726856b7e55ad1c949c37fffcba3
danielgynn <danielgynn@hotmail.co.uk>
2016-11-17 10:09:25 +0000

    Use Ember.get instead of this.get

--- app/components/panel-back-button.js
+++ app/components/panel-back-button.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
+const { Component, get } = Ember;
+
 export default Ember.Component.extend({
   actions: {
     trackBackClick() {
-      this.get('metrics').trackEvent({
+      Ember.get(this, 'metrics').trackEvent({
         category: this.get('type'),
         action: 'back button clicked'
       });
