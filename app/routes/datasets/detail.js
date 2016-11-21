455b27f23120d3805d8e72dc41e7131e1ba7be4b
Dennis Schwartz <dennis@repositive.io>
2016-11-21 12:27:18 +0000

    Added schemas.org json-ld to dataset detail page.

--- app/routes/datasets/detail.js
+++ app/routes/datasets/detail.js
@@ -9,13 +9,34 @@ export default Route.extend(ResetScrollMixin, LoadDetailRouteMixin, {
 
   model(params) {
     return this._getModelData(params, 'dataset')
-    .then(data => {
-      return RSVP.hash({
-        dataset: data.model,
-        stats: get(this, 'session.isAuthenticated') === false ? this._getStats() : null
-      });
-    })
-    .catch(Logger.error);
+      .then(data => {
+        return RSVP.hash({
+          dataset: data.model,
+          stats: get(this, 'session.isAuthenticated') === false ? this._getStats() : null,
+          tags: this._getTags(params.id)
+        })
+          .then(hash => {
+            let keywords = [];
+            hash.tags.forEach(t => keywords.push(get(t, 'properties').text));
+
+            let dataset = hash.dataset;
+            // Building schema.org JSON-LD
+            let schema = '<script type="application/ld+json">';
+            schema += '{\n"@context": "http:\/\/schema.org\/",\n"@type": "Dataset",\n';
+            schema += `"name": "${get(dataset, 'title')}",\n`;
+            schema += `"description": "${get(dataset, 'description')}",\n`;
+            schema += `"url": "${get(dataset, 'url')}",\n`;
+            schema += `"keywords": ${JSON.stringify(keywords)}\n`;
+            schema += '}\n</script>';
+
+            return RSVP.hash({
+              dataset: hash.dataset,
+              stats: hash.stats,
+              schema: schema
+            });
+          });
+      })
+      .catch(Logger.error);
   },
 
   afterModel(model) {
