e2b490d7a68074490f8581e85b344439af2f6204
Dennis Schwartz <dennis@repositive.io>
2016-11-21 13:43:16 +0000

    Moved datasources limit to backend.

--- app/routes/root.js
+++ app/routes/root.js
@@ -45,7 +45,7 @@ export default Route.extend({
         this.store.query('request', { 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC' }),
         this.store.query('dataset', { 'where.user_id.$ne': 'null', 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC' }),
         this.store.query('collection', { 'where.type': 'repositive_collection', 'order[0][0]': 'updated_at', 'order[0][1]': 'DESC', 'limit': '3' }),
-        ajax({ url: ENV.APIRoutes['datasources'] , type: 'GET', headers: authHeaders }),
+        ajax({ url: ENV.APIRoutes['datasources'] + '?limit=3' , type: 'GET', headers: authHeaders }),
         this.get('favouritesService').loadFavourites()
       ])
       .then(data => {
@@ -54,15 +54,13 @@ export default Route.extend({
           return this.store.push(this.store.normalize('dataset', datasetObj));
         });
 
-        let featuredSources = data[5].length > 3 ? data[5].slice(0,3) : data[5];
-
         return {
           stats: data[0],
           datasets: trending,
           requests: data[2],
           registered: data[3],
           collections: data[4],
-          datasources: featuredSources
+          datasources: data[5]
         };
       })
       .catch(err => {
