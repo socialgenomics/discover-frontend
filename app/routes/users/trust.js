0d93b08e7d1f1c9a3e4e5a5eb4a4253b96d4bf91
Sebastian Place <sebplace94@gmail.com>
2016-11-23 12:36:05 +0000

    Refactor trust route.

--- app/routes/users/trust.js
+++ app/routes/users/trust.js
@@ -1,38 +1,34 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import ajax from 'ic-ajax';
 import ENV from 'repositive/config/environment';
+import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
+
+const { get, set, inject: { service }, Logger, Route } = Ember;
 
 export function isVerified(credentials) {
-  return credentials.reduce((acc, curr) => acc || curr.get('verified'), false);
+  return credentials.reduce((acc, curr) => acc || get(curr, 'verified'), false);
 }
 
 export function mainCredential(credentials) {
-  return credentials.filter((c) => c.get('primary'))[0];
+  return credentials.filter((c) => get(c, 'primary'))[0];
 }
 
 export function verifyEmail() {
-  let email = this.get('session.authenticatedUser.credentials.content.currentState')[0].record.get('email');
-  this.set('sentEmail', true);
-  ajax({
-    url: ENV.APIRoutes['verify-email-resend'] + `/${email}`,
-    type: 'GET'
-  })
-  .then(() => {
-    this.flashMessages.add({
-      message: 'We have sent a verification email to your inbox',
-      type: 'info',
-      timeout: 7000,
-      class: 'fadeInOut'
+  const email = get(this, 'session.authenticatedUser.credentials.content.currentState')[0].record.get('email');
+  set(this, 'sentEmail', true);
+  get(this, 'ajax').request(ENV.APIRoutes['verify-email-resend'] + `/${email}`, { method: 'GET' })
+    .then(() => {
+      this._addFlashMessage('We have sent a verification email to your inbox', 'success');
+    })
+    .catch((err) => {
+      Logger.error(err);
+      this._addFlashMessage('Sorry, we couldn\'t send you the link. Please try again later.', 'warning');
     });
-  })
-  .catch(function(err) {
-    Ember.Logger.error(err);
-  });
 }
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  session: Ember.inject.service(),
+export default Route.extend(AuthenticatedRouteMixin, FlashMessageMixin, {
+  ajax: service(),
+  session: service(),
   sentEmail: false,
 
   actions: {
@@ -40,7 +36,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   },
 
   model: function() {
-    let userId = this.get('session.session.authenticated.user.id');
+    const userId = get(this, 'session.session.authenticated.user.id');
     return this.store.query('credential', { 'where.user_id': userId }).then((credentials) => {
       return {
         is_verified: isVerified(credentials),
