014aa36e435c61011622da7beb0a2715f3072f8a
Sebastian Place <sebplace94@gmail.com>
2016-11-23 11:09:49 +0000

    Refactor signup controller. Fixes https://github.com/repositive/report-issues/issues/284

--- app/controllers/users/signup.js
+++ app/controllers/users/signup.js
@@ -1,27 +1,23 @@
 import Ember from 'ember';
 import EmberValidations from 'ember-validations';
 import ENV from 'repositive/config/environment';
-import ajax from 'ic-ajax';
 import { validator } from 'ember-validations';
-const{ get, computed } = Ember;
+import { isBadRequestError } from 'ember-ajax/errors';
+import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
 
-export default Ember.Controller.extend(
-   EmberValidations,
-{
-  session: Ember.inject.service(),
+const{ get, getProperties, set, setProperties, computed, Controller, inject: { service }, observer } = Ember;
 
-  fullname: null,
-  firstname: null,
-  lastname: null,
+export default Controller.extend(EmberValidations, FlashMessageMixin, {
+  session: service(),
+  ajax: service(),
+
+  fullName: null,
   email: null,
   password: null,
   strength: null,
   showPassword: false,
   loading: false,
   formSubmitted: false,
-  isDisabled: computed('loading', 'isValid', function() {
-    return this.get('loading') || !this.get('isValid');
-  }),
 
   validations: {
 
@@ -30,7 +26,7 @@ export default Ember.Controller.extend(
         message: 'Can\'t be blank.'
       },
       inline: validator(function() {
-        let fn = this.get('fullname');
+        let fn = get(this, 'fullname');
         if (/@/.test(fn)) {
           return 'Please enter your full name.';
         }
@@ -56,7 +52,7 @@ export default Ember.Controller.extend(
         }
       },
       inline: validator(function() {
-        let pw = this.get('password');
+        const pw = get(this, 'password');
         if (!/\d/.test(pw) &&
             !/[A-Z]/.test(pw) &&
             !/[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/.test(pw)) {
@@ -65,97 +61,81 @@ export default Ember.Controller.extend(
       })
     }
   },
+  isDisabled: computed('loading', 'isValid', function() {
+    return get(this, 'loading') || !get(this, 'isValid');
+  }),
+  type: computed('showPassword', function() {
+    return get(this, 'showPassword') ? 'text' : 'password';
+  }),
 
-  type: function() {
-    return this.get('showPassword') ? 'text' : 'password';
-  }.property('showPassword'),
-
-  setFirstAndLastNamesFromFullName: function() {
-    let fullname = this.get('fullname').split(' ');
-    let firstname = fullname.shift();
-    let lastname = fullname.shift() || '';
-    this.set('firstname', firstname);
-    this.set('lastname', lastname);
-  }.observes('fullname'),
-
-  passwordStrength: function() {
-    let numErrors = this.get('errors.password.length');
-
-    let pw = this.get('password');
-    let specials = [
+  //TODO: This can be refactored to a component which calls this function on input keypress instead of using an observer.
+  passwordStrength: observer('password', function() {
+    const numErrors = get(this, 'errors.password.length');
+    const pw = get(this, 'password');
+    const specials = [
       /\d/.test(pw),
       /[A-Z]/.test(pw),
       /[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/.test(pw)
     ].reduce((prev, curr)=> {
       return Number(curr) + Number(prev);
     });
-
     if (numErrors === 0 && (specials > 1)) {
-      this.set('strength', 'strong');
+      set(this, 'strength', 'strong');
     } else if (numErrors <= 1) {
-      this.set('strength', 'medium');
+      set(this, 'strength', 'medium');
     } else {
-      this.set('strength', 'weak');
+      set(this, 'strength', 'weak');
     }
-  }.observes('password'),
-
-  displayMessage(resp) {
-    const errorCode = get(resp, 'status_code');
-    let message;
-    if (errorCode === 400) { //Bad Request
-      message = 'Error: Invalid email or an account already exists with this email.';
-    } else {
-      message = get(resp, 'message');
-    }
-    if (message) {
-      this.flashMessages.add({
-        message: message,
-        type: 'warning',
-        timeout: 7000,
-        class: 'fadeIn'
-      });
-    }
-  },
+  }),
 
   actions: {
     signupAndAuthenticate: function() {
-      if (!this.get('isDisabled')) {
-        this.set('formSubmitted', true);
-        let credentials = this.getProperties('firstname', 'lastname', 'email', 'password');
-        this.set('loading', true);
+      if (!get(this, 'isDisabled')) {
+        set(this, 'formSubmitted', true);
+        const firstname = this._getFirstAndLastNames(get(this, 'fullname')).firstName;
+        const lastname = this._getFirstAndLastNames(get(this, 'fullname')).lastName;
+        const credentials = getProperties(this, 'email', 'password');
+        setProperties(credentials, { firstname, lastname});
+        set(this, 'loading', true);
         /*
           Signup with repositive.
          */
-        ajax({
-          url: ENV.APIRoutes[ENV['ember-simple-auth'].signupRoute],
-          type: 'POST',
+        get(this, 'ajax').request(ENV.APIRoutes[ENV['ember-simple-auth'].signupRoute], {
+          method: 'POST',
           data: credentials
         })
-        .then((resp)=> { // signup has suceeded, now login
-          this.get('session').authenticate('authenticator:repositive', credentials)
-          .then(() => this.get('session').set('data.firstVisit', true))
-          .then(() => {
-            this.get('session').set('data.displayWelcomeMessage', false);
-            this.set('loading', false);
+          .then(resp => { // signup has suceeded, now login
+            return get(this, 'session').authenticate('authenticator:repositive', credentials);
           })
-          .catch(this.displayMessage.bind(this));
-        })
-        .catch(err => { // error with signup
-          this.set('loading', false);
-          if (err.jqXHR !== undefined) {
-            /*
-              if the error is 4XX or 5XX server resp display the error messages.
-             */
-            this.displayMessage(err.jqXHR.responseJSON);
-          } else {
-            throw err;
-          }
-        });
+          .then(() => {
+            set(this, 'session.data.firstVisit', true);
+            set(this, 'session.data.displayWelcomeMessage', false);
+            set(this, 'loading', false);
+          })
+          .catch(err => { // error with signup
+            set(this, 'loading', false);
+            this._displayMessage(err);
+          });
       }
     },
 
     toggleCheckbox: function() {
       this.toggleProperty('showPassword');
     }
+  },
+
+  _displayMessage(resp) {
+    if (isBadRequestError(resp)) {
+      this._addFlashMessage('Error: Invalid email or an account already exists with this email.', 'warning');
+    } else {
+      this._addFlashMessage(get(resp, 'message'), 'warning');
+    }
+  },
+
+  _getFirstAndLastNames: function(fullName) {
+    const fullNameArray = fullName.split(' ');
+    const firstName = fullNameArray.shift();
+    const lastName = fullNameArray.join(' ') || '';
+    return { firstName, lastName };
   }
 });
