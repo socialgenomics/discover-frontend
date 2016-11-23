b8b2209f40a190a2b7933b195f6efeeb8ae8924d
Dennis Schwartz <dennis@repositive.io>
2016-11-23 10:15:32 +0000

    Added d3 map for homepage. Integration still missing.

--- app/components/home-page-map.js
+++ app/components/home-page-map.js
@@ -8,29 +8,45 @@ export default Ember.Component.extend({
   didInsertElement: function() {
     const width = this.$().innerWidth();
     const height = this.$().innerHeight() > 0 ? this.$().innerHeight() : 500;
-    console.log(`Width: ${width}`);
-    console.log(`Height: ${height}`);
-    console.log(this.$());
-    let svg = d3.select('.map-search').append('svg')
+
+
+    let svg = d3.select('.map-search').insert("svg",":first-child")
       .attr('width', width)
-      .attr('height', height);
+      .attr('height', height)
+      .attr('id', 'map');
 
     let g = svg.append('g');
 
     let mercator = d3.geoMercator()
-      .center([0, 50])
-      .scale(140);
+      .center([0, 20])
+      .scale(150);
 
     let path = d3.geoPath().projection(mercator);
 
-    d3.json("assets/worldmap.json", function(error, topology) {
+    d3.json("assets/worldmap.json", function(error, topo) {
 
+      console.log(topojson.feature(topo, topo.objects.countries));
       g.selectAll("path")
-        .data(topojson.object(topology, topology.objects.countries).geometries)
+        .data(topojson.feature(topo, topo.objects.countries).features)
         .enter()
         .append("path")
         .attr("d", path)
         .attr('fill', '#888');
+
+      d3.json('assets/points.json', function (error, topology) {
+
+        let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+        svg.selectAll('.samples')
+          .data(topology.features)
+          .enter()
+          .append('path')
+          .attr('d', path)
+          .attr("cursor", "pointer")
+          .attr('fill', d => {
+            return color(d.properties.type);
+          });
+      });
 
     });
 
