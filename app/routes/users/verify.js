e23765ee9f2bf0880882f5a2372e9bb7746acb79
Sebastian Place <sebplace94@gmail.com>
2016-11-23 12:44:18 +0000

    Refactor verify route

--- app/routes/users/verify.js
+++ app/routes/users/verify.js
@@ -1,20 +1,21 @@
 import Ember from 'ember';
-import ajax from 'ic-ajax';
+// import ajax from 'ic-ajax';
 import ENV from 'repositive/config/environment';
 import {verifyEmail} from './trust';
+import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
 
-export default Ember.Route.extend({
-  session: Ember.inject.service(),
+const { inject: { service }, get, set, RSVP, Logger, Route } = Ember;
+
+export default Route.extend(FlashMessageMixin, {
+  ajax: service(),
+  session: service(),
   verificationId: null,
 
   model: function(params) {
     // return a promise.. this pauses the page rendering until the promise is resolved or rejected
     // loding page is shown whilst the promise in unresolved
     // error page is shown if the promise is rejected
-    return ajax({
-      url: ENV.APIRoutes['verify-email'] + '/' + params.verificationId,
-      type: 'GET'
-    })
+    get(this, 'ajax').request(ENV.APIRoutes['verify-email'] + '/' + params.verificationId, { method: 'GET' })
     .then(resp => {
       /**
       * Backend validated the email address - transitionTo the profile without
@@ -24,11 +25,11 @@ export default Ember.Route.extend({
         session. This means we cannot redirect to current user's profile if they're
         not logged in.
       */
-      if (this.get('session.isAuthenticated')) {
-        if (this.get('session.authenticatedUser')) {
-          this.get('session.authenticatedUser').set('isEmailValidated', true);
-          this.get('session.data').set('displayWelcomeMessage', false);
-          this.transitionTo('user', this.get('session.authenticatedUser.id'));
+      if (get(this, 'session.isAuthenticated')) {
+        if (get(this, 'session.authenticatedUser')) {
+          set(this, 'session.authenticatedUser.isEmailValidated', true);
+          set(this, 'session.data.displayWelcomeMessage', false);
+          this.transitionTo('user', get(this, 'session.authenticatedUser.id'));
         } else {
           console.warn('session.authenticatedUser is undefined but session.isAuthenticated "true"');
           this.transitionTo('users.login');
@@ -36,11 +37,11 @@ export default Ember.Route.extend({
       } else {
         this.transitionTo('root');
       }
-      this.showMessages(resp);
+      this._showMessages(resp);
     })
     .catch((err)=> {
-      Ember.Logger.error(err);
-      Ember.RSVP.resolve(); // fulfills the promise - this causes ember to render the template
+      Logger.error(err);
+      RSVP.resolve(); // fulfills the promise - this causes ember to render the template
     });
   },
 
@@ -48,21 +49,11 @@ export default Ember.Route.extend({
     resendVerifyEmail: verifyEmail
   },
 
-  showMessages: function(content) {
+  _showMessages: function(content) {
     if (content.message === 'success') {
-      this.flashMessages.add({
-        message: 'Your credential is now validated',
-        type: 'info',
-        timeout: 7000,
-        class: 'fadeInOut'
-      });
+      this._addFlashMessage('Your credential is now validated', 'success');
     } else {
-      this.flashMessages.add({
-        message: 'An unexpected error occurred',
-        type: 'warn',
-        timeout: 7000,
-        class: 'fadeInOut'
-      });
+      this._addFlashMessage('An unexpected error occurred', 'warning');
     }
   }
 });
