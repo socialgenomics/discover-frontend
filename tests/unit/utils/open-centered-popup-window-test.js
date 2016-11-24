c9598aaaa09abdf5cc4127ded7bb9f1097c7af1e
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-11-24 11:42:53 +0100

    ISSUE-262, refactor tests to use mocha

--- tests/unit/utils/open-centered-popup-window-test.js
+++ tests/unit/utils/open-centered-popup-window-test.js
@@ -1,45 +1,46 @@
-import openCenteredPopupWindow from 'frontend/utils/open-centered-popup-window';
-import { module, test } from 'qunit';
+/* jshint expr:true */
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
 import sinon from 'sinon';
+import openCenteredPopupWindow from 'repositive/utils/open-centered-popup-window';
 
-module('Unit | Utility | open centered popup window');
+describe('Open Centered Popup Window', function() {
+  it('it open new window with correct params', function () {
+    const originalWinOpen = window.open;
+    const originalWinScreen = window.screen;
+    const url = 'test';
+    const title = 'lorem ipsum';
+    const size = 100;
+    const params = {
+      width: size,
+      height: size,
+      toolbar: 'no',
+      location: 'no',
+      directories: 'no',
+      menubar: 'no',
+      copyhistory: 'no',
+      top: size / 2,
+      left: size / 2
+    };
 
-// Replace this with your real tests.
-test('it open new window with correct params', function(assert) {
-  const originalWinOpen = window.open;
-  const originalWinScreen = window.screen;
-  const url = 'test';
-  const title = 'lorem ipsum';
-  const size = 100;
-  const params = {
-    width: size,
-    height: size,
-    toolbar: 'no',
-    location: 'no',
-    directories: 'no',
-    menubar: 'no',
-    copyhistory: 'no',
-    top: size / 2,
-    left: size / 2,
-  };
+    window.open = sinon.spy();
+    window.screen = {
+      width: size * 2,
+      height: size * 2
+    };
 
-  window.open = sinon.spy();
-  window.screen = {
-    width: size * 2,
-    height: size * 2,
-  };
+    openCenteredPopupWindow(url, title, size, size);
 
-  openCenteredPopupWindow(url, title, size, size);
+    expect(
+      window.open.calledWith(
+        url,
+        title,
+        Object.keys(params).map(name => `${name}=${params[name]}`).join(', ')
+      ),
+      true
+    ).to.eql(true);
 
-  assert.ok(
-    window.open.calledWith(
-      url,
-      title,
-      Object.keys(params).map(name => `${name}=${params[name]}`).join(', ')
-    ),
-    true
-  );
-
-  window.open = originalWinOpen;
-  window.screen = originalWinScreen;
+    window.open = originalWinOpen;
+    window.screen = originalWinScreen;
+  });
 });
