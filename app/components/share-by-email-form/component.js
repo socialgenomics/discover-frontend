fe814610e3a567c548c8f502595a145e51785a8a
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-11-30 22:35:09 +0100

    add share vie email form component

---
+++ app/components/share-by-email-form/component.js
@@ -0,0 +1,90 @@
+import Ember from 'ember';
+import EmberValidations from 'ember-validations';
+import ENV from 'repositive/config/environment';
+
+const { Component, computed, get, inject: { service }, set, setProperties } = Ember;
+
+export default Component.extend(EmberValidations, {
+  ajax: service(),
+
+  tagName: 'form',
+  emailAddress: '',
+  customMessage: '',
+  actionableId: null,
+  sendSuccess: false,
+  sendError: false,
+  isLoading: false,
+
+  validations: {
+    emailAddress: {
+      presence: { message: 'Please provide email address.' },
+      format: {
+        with: /^([\w\-\.\+]+)@((?:[\w\-\.]+)(?:\.[a-zA-Z]{2,}))$/,
+        allowBlank: false,
+        message: 'Must be a valid email address.'
+      }
+    }
+  },
+
+  // TODO: add test
+  submitDisabled: computed('isLoading', 'errors.emailAddress.length', function() {
+    return get(this, 'isLoading') || get(this, 'errors.emailAddress.length') !== 0;
+  }),
+
+  // TODO: add test
+  submitButtonLabel: computed('isLoading', 'sendSuccess', 'sendError', function () {
+    switch (true) {
+      case get(this, 'isLoading') :
+        return 'Sending the email...';
+      case get(this, 'sendSuccess') || get(this, 'sendError') :
+        return 'Back to dataset';
+      default:
+        return 'Send the Email';
+    }
+  }),
+
+  submit(event) {
+    event.preventDefault();
+
+    // close modal if send action already taken by user
+    if (get(this, 'sendSuccess') || get(this, 'sendError')) {
+      get(this, 'onClose')();
+    } else {
+      set(this, 'isLoading', true);
+
+      this._sendCustomEmail()
+        .then(this._onSendSuccess.bind(this))
+        .catch(this._onSendError.bind(this));
+    }
+  },
+
+  _onSendSuccess() {
+    setProperties(this, {
+      sendSuccess: true,
+      isLoading: false
+    });
+  },
+
+  _onSendError() {
+    setProperties(this, {
+      sendError: true,
+      isLoading: false
+    });
+    // TODO need to add error state to UI
+  },
+
+  _sendCustomEmail() {
+    return get(this, 'ajax').request(
+      ENV.APIRoutes['share'],
+      {
+        method: 'POST',
+        data: {
+          to: get(this, 'emailAddress'),
+          customMessage: get(this, 'customMessage'),
+          actionable_id: get(this, 'actionableId'),
+          actionable_model: 'dataset'
+        }
+      }
+    );
+  }
+});
