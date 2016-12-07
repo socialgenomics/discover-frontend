6fba55d922729d501b3c65ccc70e01cbf4521551
Dennis Schwartz <dennis@repositive.io>
2016-12-07 13:22:36 +0000

    Added tests for query operations and updated parser.

--- tests/unit/services/search-test.js
+++ tests/unit/services/search-test.js
@@ -11,12 +11,28 @@ describeModule(
   {
     // Specify the other units that are required for this test.
     // needs: ['service:foo']
+    needs: [
+      'service:ajax',
+      'service:session',
+      'service:metrics'
+    ]
   },
   function() {
     // Replace this with your real tests.
     it('exists', function() {
       let service = this.subject();
       expect(service).to.be.ok;
+    });
+
+    it('Adds filters', function () {
+      let service = this.subject();
+      // Add a query
+      service.updateQuery('cancer AND breast');
+      let string = service.getQueryString();
+      expect(string).to.eql('(cancer AND breast)');
+      service.addPredicate('assay', 'RNA-Seq');
+      string = service.getQueryString();
+      expect(string).to.eql('(assay:RNA-Seq AND (cancer AND breast))');
     });
   }
 );
