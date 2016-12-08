dea60684e6812b4e64a43d09c3a89cab4b509320
Sebastian Place <sebplace94@gmail.com>
2016-12-08 12:56:00 +0000

    Create search route mixin. Add tests for search-controller-mixin and search-route-mixin.

--- tests/unit/mixins/search-controller-test.js
+++ tests/unit/mixins/search-controller-test.js
@@ -8,10 +8,16 @@ import Ember from 'ember';
 import SearchControllerMixin from 'repositive/mixins/search-controller';
 
 describe('SearchControllerMixin', function() {
-  // Replace this with your real tests.
-  it('works', function() {
+  it('Adds query and page queryParams to the object', function() {
     let SearchControllerObject = Ember.Object.extend(SearchControllerMixin);
     let subject = SearchControllerObject.create();
-    expect(subject).to.be.ok;
+    expect(subject.get('queryParams').toString()).to.eql('query, page');
+  });
+
+  it('Sets default values for query and page on the object', function() {
+    let SearchControllerObject = Ember.Object.extend(SearchControllerMixin);
+    let subject = SearchControllerObject.create();
+    expect(subject.get('page')).to.eql(1);
+    expect(subject.get('query')).to.eql(null);
   });
 });
