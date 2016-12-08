97583b929f569f00b088f8f7085fa66f090ad54d
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-12-08 09:29:13 +0100

    ISSUE-317, add test for url generator service

--- tests/unit/services/url-generator-test.js
+++ tests/unit/services/url-generator-test.js
@@ -1,22 +1,29 @@
 /* jshint expr:true */
 import { expect } from 'chai';
-import {
-  describeModule,
-  it
-} from 'ember-mocha';
+import { describeModule, it } from 'ember-mocha';
 
 describeModule(
   'service:url-generator',
   'UrlGeneratorService',
-  {
-    // Specify the other units that are required for this test.
-    // needs: ['service:foo']
-  },
+
   function() {
-    // Replace this with your real tests.
-    it('exists', function() {
-      let service = this.subject();
-      expect(service).to.be.ok;
+    it('throws error when called generateUrl and service is not initialized', function() {
+      const service = this.subject();
+      const errorFn = () => {
+        service.generateUrl('test', 1);
+      };
+
+      expect(errorFn).to.throw(Error, 'GenerateUrl service was not initialized');
+    });
+
+    it('returns Url when called generateUrl after service initialisation', function() {
+      const service = this.subject();
+      const routerMock = {
+        generate() { return '/test'; }
+      };
+
+      service.initialize(routerMock);
+      expect(service.generateUrl('test', 1)).to.eql('http://localhost:7357/test');
     });
   }
 );
