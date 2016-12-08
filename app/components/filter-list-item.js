43b5519aee5b8ad5ba840aca9c4789f890d230d0
Dennis Schwartz <dennis@repositive.io>
2016-12-08 12:02:38 +0000

    removed unnecessary logs.

--- app/components/filter-list-item.js
+++ app/components/filter-list-item.js
@@ -6,12 +6,9 @@ export default Component.extend({
   search: service(),
   actions: {
     addFilter(name, key) {
-      console.log('Added:', name, key);
       get(this, 'search').addPredicate(name, key);
-      // get(this, 'addFilter')(aggName, bucketKey);
     },
     removeFilter(name, key) {
-      console.log('Removed:', name, key);
       get(this, 'search').removePredicate(name, key);
     }
   }
