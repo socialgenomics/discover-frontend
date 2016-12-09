0fe3d5d3c144d73c5e07361db2a983b61861de56
danielgynn <danielgynn@hotmail.co.uk>
2016-12-09 12:31:16 +0000

    Toggle description display to 10 lines with show more/less button

--- app/components/collapsible-description.js
+++ app/components/collapsible-description.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showFullDescription: false,
+
+  actions: {
+    toggleDescription() {
+      this.toggleProperty('showFullDescription');
+    }
+  }
 });
