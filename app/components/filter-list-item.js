91dcf55d54f90df0d6470947aafc7303ccb3c5a4
Dennis Schwartz <dennis@repositive.io>
2016-12-12 11:56:01 +0000

    Display removal 'x' only when filter is active.

--- app/components/filter-list-item.js
+++ app/components/filter-list-item.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { Component, computed, get, set, inject: { service } } = Ember;
+const { Component, get, inject: { service } } = Ember;
 
 export default Component.extend({
   searchService: service('search'),
@@ -8,11 +8,6 @@ export default Component.extend({
     const filterString = `${get(this, 'aggName')}:${get(this, 'bucket.key')}`;
     return get(this, 'searchService').isFilterActive(filterString);
   }.property('model', 'active'),
-  // init() {
-  //   this._super(...arguments);
-  //   const filterString = `${get(this, 'aggName')}:${get(this, 'bucket.key')}`;
-  //   set(this, 'active', get(this, 'searchService').isFilterActive(filterString));
-  // },
   actions: {
     addFilter(name, key) {
       const queryString = get(this, 'searchService').addFilter(name, key);
