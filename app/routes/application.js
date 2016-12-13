110923eaa80d78dc97529d0d7bed7acc5b195db8
danielgynn <danielgynn@hotmail.co.uk>
2016-12-13 10:59:15 +0000

    Check currentRoute for collections pagination/filtering

--- app/routes/application.js
+++ app/routes/application.js
@@ -16,17 +16,27 @@ export default Route.extend(ApplicationRouteMixin, {
   },
   actions: {
     search(query, pageNumber) {
-      this.transitionTo('datasets.search', {
-        queryParams: {
-          query: query,
-          page: pageNumber || 1
-        }
-      });
-      get(this, 'metrics').trackEvent({
-        category: 'search',
-        action: 'query',
-        label: query
-      });
+      const currentRouteName = this.controllerFor('application').get('currentRouteName');
+      if (currentRouteName === 'collections.collection' || 'datasources.source') {
+        this.transitionTo(currentRouteName, {
+          queryParams: {
+            query: query,
+            page: pageNumber || 1
+          }
+        });
+      } else {
+        this.transitionTo('datasets.search', {
+          queryParams: {
+            query: query,
+            page: pageNumber || 1
+          }
+        });
+        get(this, 'metrics').trackEvent({
+          category: 'search',
+          action: 'query',
+          label: query
+        });
+      }
     },
     toggleModal() {
       this.controllerFor('application').toggleProperty('isShowingModal');
