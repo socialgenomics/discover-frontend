fe3601eb1a965d4a1c661ff4ec14832ca1fa2479
Dennis Schwartz <dennis@repositive.io>
2016-12-15 11:07:20 +0000

    Moved transition function to named function and renamed regex.

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,7 +5,7 @@ import BX from 'npm:../../query-parser/dist/main/b-exp-tree';
 
 const { Route, inject: { service }, get } = Ember;
 
-const re = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
+const isUUID = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
 
 export default Route.extend(ApplicationRouteMixin, {
   favouritesService: service('favourites'),
@@ -26,9 +26,28 @@ export default Route.extend(ApplicationRouteMixin, {
   model() {
     get(this, 'favouritesService').loadFavourites();
   },
+  queryAndTransition (dest, input, serializeTree, pageNumber) {
+    let query = {};
+    if (isUUID.test(input.text))
+      query = {'where.id' : input.text };
+    else
+      query = {
+        'where[$or][0][name]' : input.text,
+        'where[$or][1][properties][short_name]' : input.text,
+        'where[$or][2][properties][short_name]' : input.text.toUpperCase()
+      };
+    this.store.query('collection', query).then(collection => {
+      this.store.push(this.store.normalize('collection', collection.content[0]));
+      this.transitionTo(dest, collection.content[0].id, {
+        queryParams: {
+          query: serializeTree,
+          page: pageNumber || 1
+        }
+      });
+    });
+  },
   actions: {
     search(queryTree, pageNumber) {
-
       const searchService = get(this,  'searchService');
       const collectionF = BT.filter(queryTree, (node, left, right) => {
         return BX.isFilter(node) && node.predicate === 'collection'
@@ -36,34 +55,11 @@ export default Route.extend(ApplicationRouteMixin, {
       const datasourceF = BT.filter(queryTree, (node, left, right) => {
         return BX.isFilter(node) && node.predicate === 'datasource'
       });
-
       const serializeTree = searchService.serializeToString(queryTree);
-
-      const transition = (dest, input) => {
-        let query = {};
-        if (re.test(input.text))
-          query = {'where.id' : input.text };
-        else
-          query = {
-            'where[$or][0][name]' : input.text,
-            'where[$or][1][properties][short_name]' : input.text,
-            'where[$or][2][properties][short_name]' : input.text.toUpperCase()
-          };
-        this.store.query('collection', query).then(c => {
-          this.store.push(this.store.normalize('collection', c.content[0]));
-          this.transitionTo(dest, c.content[0].id, {
-            queryParams: {
-              query: serializeTree,
-              page: pageNumber || 1
-            }
-          });
-        });
-      };
-
       if (collectionF.length === 1) {
-        transition('collections.collection', collectionF[0]);
+        this.queryAndTransition('collections.collection', collectionF[0], serializeTree, pageNumber);
       } else if (datasourceF.length === 1) {
-        transition('datasources.source', datasourceF[0]);
+        this.queryAndTransition('datasources.source', datasourceF[0]);
       } else {
         this.transitionTo('datasets.search', {
           queryParams: {
