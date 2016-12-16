f522ea084b6951cee58b615dfd98e0632ed2afc4
Sebastian Place <sebplace94@gmail.com>
2016-12-16 15:38:48 +0000

    Rename source-meta-panel to meta-panel

--- tests/integration/components/meta-panel-test.js
+++ tests/integration/components/meta-panel-test.js
@@ -4,18 +4,18 @@ import { it, describe } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describe('Integration: SourceMetaPanelComponent', function() {
-  setupComponentTest('source-meta-panel', {
+describe('Integration: MetaPanelComponent', function() {
+  setupComponentTest('meta-panel', {
     integration: true
   });
 
   it('Renders filters when filters tab is active', function() {
-    this.render(hbs`{{source-meta-panel  model=model displayInfo=false}}`);
+    this.render(hbs`{{meta-panel  model=model displayInfo=false}}`);
     expect(this.$('.filters-list').find('h4').text().trim()).to.eql('Filters');
   });
   it('Renders info when info tab selected', function() {
     this.set('model', { name: 'ABC' });
-    this.render(hbs`{{source-meta-panel model=model}}`);
+    this.render(hbs`{{meta-panel model=model}}`);
     expect(this.$('.source-name-fallback').text().trim()).to.eql('ABC');
   });
 });
