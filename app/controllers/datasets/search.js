b271babd5a99084f1e7bc9ddec673ed1958276e2
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-01-05 08:16:18 +0100

    add controller for dataset/search

---
+++ app/controllers/datasets/search.js
@@ -0,0 +1,53 @@
+import Ember from 'ember';
+import QP from 'npm:../../query-parser';
+
+const { Controller, computed, get, set } = Ember;
+
+export default Controller.extend({
+  queryParams: ['query', 'page', 'resultsPerPage'],
+  query: null,
+  page: 1,
+  resultsPerPage: 9,
+
+  totalPages: computed('model.meta.total', 'resultsPerPage', function () {
+    return Math.ceil(get(this, 'model.meta.total') / get(this, 'resultsPerPage'));
+  }),
+
+  activeFilters: computed('query', function () {
+    const queryTree = QP.parseString(get(this, 'query'));
+
+    return QP.getFilters(queryTree).map(filter => `${filter.predicate}:${filter.text}`);
+  }),
+
+  actions: {
+    nextPage() {
+      this.incrementProperty('page');
+    },
+
+    previousPage() {
+      this.decrementProperty('page');
+    },
+
+    addFilter(predicate, text) {
+      this._toggleFilter('addFilter', predicate, text);
+    },
+
+    removeFilter(predicate, text) {
+      this._toggleFilter('removeFilter', predicate, text);
+    }
+  },
+
+  /**
+   *
+   * @param {String} action - QP method name ("addFilter" or "removeFilter")
+   * @param {String} predicate
+   * @param {String} text
+   * @private
+   */
+  _toggleFilter(action, predicate, text) {
+    const currentQueryTree = QP.parseString(get(this, 'query'));
+    const newQueryTree = QP[action](currentQueryTree, predicate, text);
+
+    set(this, 'query', QP.toBoolString(newQueryTree));
+  }
+});
