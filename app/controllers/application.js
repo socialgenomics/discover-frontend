b5a7c73a82397347595c448019125c0615109ba7
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-01-05 08:19:16 +0100

    refactor search-bar WIP

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,9 +5,22 @@ const { Controller, inject: { service }, set } = Ember;
 export default Controller.extend({
   session: service(),
   moment: service(),
+
+  // queryParams: ['query'],
+  // query: null,
   isShowingModal: false,
+
   actions: {
-    toggleModal() { this.toggleProperty('isShowingModal'); },
-    changeDefaultFormat() { set(this, 'moment.defaultFormat', 'DD.MM.YYYY'); }
+    toggleModal() {
+      this.toggleProperty('isShowingModal');
+    },
+
+    changeDefaultFormat() {
+      set(this, 'moment.defaultFormat', 'DD.MM.YYYY');
+    },
+
+    search(query) {
+      set(this, 'query', query.trim());
+    }
   }
 });
