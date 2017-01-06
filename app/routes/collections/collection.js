6da077ac0f77e9ff583c52bb71a9a74136743ede
Sebastian Place <sebplace94@gmail.com>
2017-01-06 17:24:36 +0000

    Fix collections not loading

--- app/routes/collections/collection.js
+++ app/routes/collections/collection.js
@@ -3,10 +3,11 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 import { model } from '../datasources/source';
 import ResetScrollMixin from 'repositive/mixins/reset-scroll';
 import ActionableMixin from 'repositive/mixins/actionable';
+import SearchRouteMixin from '../../mixins/search';
 
 const { get, Route , inject: { service } } = Ember;
 
-export default Route.extend(AuthenticatedRouteMixin, ResetScrollMixin, ActionableMixin, {
+export default Route.extend(AuthenticatedRouteMixin, ResetScrollMixin, ActionableMixin, SearchRouteMixin, {
   ajax: service(),
   session: service(),
 
