a9dcbe746d844648c4524acb1a8f1aa5e730f279
Dennis Schwartz <dennis@repositive.io>
2017-01-09 17:26:29 +0000

    Updated search box for open search

--- app/components/search-box.js
+++ app/components/search-box.js
@@ -1,17 +1,25 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-  session: Ember.inject.service(),
-  query: '',
+const { Component, observer, get, set, inject: { service } } = Ember;
 
+export default Component.extend({
+  searchService: service('search'),
+  // I couldn't find a different way to get the latest query from
+  // the service, into the search bar.
+  setQuery: observer('searchService.queryTree', function() {
+    const searchService = get(this, 'searchService');
+    set(this, 'query', searchService.getQueryString());
+  }),
+  init() {
+    this._super(...arguments);
+    const searchService = get(this, 'searchService');
+    set(this, 'query', searchService.getQueryString());
+  },
   actions: {
-    search: function() {
-      this.get('metrics').trackEvent({
-        category: 'search',
-        action: 'query',
-        label: this.get('query')
-      });
-      this.sendAction('action', this.get('query'));
+    search(query) {
+      const searchService = get(this, 'searchService');
+      const queryTree = searchService.updateQuery(query.trim());
+      get(this, 'search')(queryTree); //calls search on application route
     }
   }
 });
