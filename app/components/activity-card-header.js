813a8f2eb1021dff0e501d81327395a4ac5e72cf
Sebastian Place <sebplace94@gmail.com>
2017-01-20 13:40:10 +0000

    Syly activty card. Prefix font-sizes with u-

--- app/components/activity-card-header.js
+++ app/components/activity-card-header.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed, get } = Ember;
 
 export default Component.extend({
+  classNames: ['u-border-bottom', 'u-border-fat', 'u-p2'],
+  classNameBindings: ['isRegistration:u-border-color-red', 'isRequest:u-border-color-blue'],
+  isRequest: computed('type', function(){
+    if (get(this, 'type') === 'request') { return true; }
+  }),
+  isRegistration: computed('type', function(){
+    if (get(this, 'type') === 'registration') { return true; }
+  })
 });
