177197afd383eaaea519adaf6eedff42f199cd97
Dennis Schwartz <dennis@repositive.io>
2017-01-20 13:54:19 +0000

    Styling the component and datasources list.

--- app/components/collections-list-tabs.js
+++ app/components/collections-list-tabs.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+const { computed, get } = Ember;
 
 export default Ember.Component.extend({
+  isCollections: computed(function () {
+    return get(this, 'active') === 'collections';
+  }),
+  isDatasources: computed(function () {
+    return get(this, 'active') === 'datasources';
+  })
 });
