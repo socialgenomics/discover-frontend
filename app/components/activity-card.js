56a144fdf4622d99355436219796a935de1e381e
Sebastian Place <sebplace94@gmail.com>
2017-01-23 15:59:09 +0000

    Activity lists in profile page

--- app/components/activity-card.js
+++ app/components/activity-card.js
@@ -1,7 +1,36 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed, get } = Ember;
 
 export default Component.extend({
-  classNames: ['c-card']
+  classNames: ['c-card'],
+  //computed property to assign placeholder text, based on type & if own profile
+  emptyText: computed('type', 'isOwnProfile', 'isFavourite', function() {
+    const type = get(this, 'type');
+    const isOwnProfile = get(this, 'isOwnProfile');
+
+    if (get(this, 'isFavourite')) {
+      if (isOwnProfile) {
+        return "You don't have any favourite datasets yet.";
+      } else {
+        return "This user doesn't have any favourite datasets yet.";
+      }
+    }
+
+    if (type === 'request') {
+      if (isOwnProfile) {
+        return "You haven't requested any data from the community yet.";
+      } else {
+        return "This user hasn't requested any data yet.";
+      }
+    }
+
+    if (type === 'registration') {
+      if (isOwnProfile) {
+        return "You haven't registered any data yet.";
+      } else {
+        return "This user hasn't registered any data yet.";
+      }
+    }
+  })
 });
