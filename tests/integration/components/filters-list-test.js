af070930e2e451e17814051291b9609333df6ac5
Sebastian Place <sebplace94@gmail.com>
2017-01-25 15:57:02 +0000

    Fix filters-list tests

--- tests/integration/components/filters-list-test.js
+++ tests/integration/components/filters-list-test.js
@@ -9,23 +9,38 @@ describe('Integration | Component | filters list', function() {
     integration: true
   });
 
-  // it('renders list of filters by category', function() {
-  //   this.setProperties({
-  //     filters: {
-  //       displayName: 'Assay',
-  //     },
-  //     isEmptyFilters: false
-  //   });
-  //   this.render(hbs`{{filters-list filters=filters isEmptyFilters=false isLoading=isLoading}}`);
-  //   expect(this.$('.section').find('h4').text().trim()).to.eql('Assay');
-  // });
+  it('renders list of filters by category', function() {
+    this.setProperties({
+      'activeFilters': [],
+      'filters': [
+        {
+          buckets: [
+            { colour: 'cyan', doc_count: 5, key: 'Open' }
+          ],
+          displayName: 'Access',
+          name: 'access'
+        }
+      ]
+    });
+    this.render(hbs`{{filters-list filters=filters activeFilters=activeFilters}}`);
+    expect(this.$('.section').find('h4').text().trim()).to.eql('Access');
+  });
 
-  // it('renders loading text when filters are loading', function() {
-  //   this.setProperties({
-  //     isEmptyFilters: false,
-  //     isLoading: true
-  //   });
-  //   this.render(hbs`{{filters-list isEmptyFilters=isEmptyFilters isLoading=isLoading}}`);
-  //   expect(this.$('.loading-text').text().trim()).to.eql('Loading new filters...');
-  // });
+  it('renders loading text when filters are loading', function() {
+    this.setProperties({
+      'activeFilters': [],
+      'filters': [
+        {
+          buckets: [
+            { colour: 'cyan', doc_count: 5, key: 'Open' }
+          ],
+          displayName: 'Access',
+          name: 'access'
+        }
+      ],
+      'isLoading': true
+    });
+    this.render(hbs`{{filters-list filters=filters activeFilters=activeFilters isLoading=isLoading}}`);
+    expect(this.$('.loading-text').text().trim()).to.eql('Loading new filters...');
+  });
 });
