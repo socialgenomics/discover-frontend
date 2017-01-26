f0cec1eabe1034688028e625406f4c18505d2090
Sebastian Place <sebplace94@gmail.com>
2017-01-26 16:14:02 +0000

    Remove unnecessary class bindings. Add isActive condtional to helper text.

--- app/components/validated-input.js
+++ app/components/validated-input.js
@@ -1,35 +1,27 @@
 import Ember from 'ember';
 
-const { Component, computed, get, setProperties, isEmpty } = Ember;
+const { Component, computed, get, setProperties, set, isEmpty } = Ember;
 
 export default Component.extend({
   errors: null,
   isActive: false,
   hasBeenFocused: false,
   classNames: ['u-mb3'],
-  classNameBindings: ['isActive:active', 'showValid:valid', 'showInvalid:invalid'],
+  classNameBindings: ['isActive:active'],
 
-  showValid: computed('hasBeenFocused', 'errors', 'formSubmitted', function() {
+  isValid: computed('hasBeenFocused', 'errors', 'formSubmitted', function() {
     return (get(this, 'hasBeenFocused') || get(this, 'formSubmitted')) && isEmpty(get(this, 'errors'));
   }),
 
-  showInvalid: computed('hasBeenFocused', 'errors', 'formSubmitted', function() {
+  isInvalid: computed('hasBeenFocused', 'errors', 'formSubmitted', function() {
     return (get(this, 'hasBeenFocused') || get(this, 'formSubmitted')) && !isEmpty(get(this, 'errors'));
   }),
 
   actions: {
-    focusedIn() {
-      setProperties(this, {
-        'defaultPlaceholder': get(this, 'placeholder'),
-        'placeholder': '',
-        'isActive': true
-      });
-      this.sendAction();
-    },
+    focusedIn() { set(this, 'isActive', true); },
 
     focusedOut() {
       setProperties(this, {
-        'placeholder': get(this, 'defaultPlaceholder'),
         'hasBeenFocused': true,
         'isActive': false
       });
