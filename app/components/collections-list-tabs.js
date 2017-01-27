e61f817e2b32683eb75329e2c7e4b6182fdc5983
Dennis Schwartz <dennis@repositive.io>
2017-01-27 11:34:25 +0000

    Added missing u- to renamed fs and fw classes.

--- app/components/collections-list-tabs.js
+++ app/components/collections-list-tabs.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
-const { computed, get } = Ember;
+const { Component, computed, get } = Ember;
 
-export default Ember.Component.extend({
+export default Component.extend({
   isCollections: computed(function () {
     return get(this, 'active') === 'collections';
   }),
