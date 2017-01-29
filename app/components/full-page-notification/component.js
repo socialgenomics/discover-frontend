f4c3e30220db4cd33324c16cc194be1141e5a64b
danielgynn <danielgynn@hotmail.co.uk>
2017-01-29 09:47:26 +0000

    Update classes to use padding in place of margin

--- app/components/full-page-notification/component.js
+++ app/components/full-page-notification/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 const { Component, get } = Ember;
 
 export default Component.extend({
-  classNames: ['c-call-to-action-banner', 'u-flex', 'u-justify-center', 'u-items-center'],
+  classNames: ['c-call-to-action-banner', 'u-flex', 'u-justify-center', 'u-items-center', 'u-pt2'],
 
   actions: {
     trackLinkEvent() {
