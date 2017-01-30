7c1ca032b3020091b729f011057288cce9efd18c
Tom Byers <hello@tombye.rs>
2017-01-30 18:18:27 +0000

    Fixing sidebar header layout

--- app/components/meta-panel-header.js
+++ app/components/meta-panel-header.js
@@ -4,5 +4,5 @@ const { Component } = Ember;
 
 export default Component.extend({
   tagName: 'header',
-  classNames: ['grid__col grid__col--3-of-3', 'u-bc-overlay', 'u-ta-center']
+  classNames: ['grid__col grid__col--3-of-3', 'u-bc-overlay']
 });
