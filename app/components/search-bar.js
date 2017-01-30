9047777f7f3cda6ac25eae0190cd6e8e049cd7f5
danielgynn <danielgynn@hotmail.co.uk>
2017-01-30 11:59:13 +0000

    Adjust search bar size and keep inline with flex

--- app/components/search-bar.js
+++ app/components/search-bar.js
@@ -4,6 +4,7 @@ const { Component, get, observer, inject: { service }, set } = Ember;
 
 export default Component.extend({
   queryService: service('query'),
+  classNames: ['u-flex-auto'],
 
   init() {
     this._super(...arguments);
