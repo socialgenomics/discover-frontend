a9f9ba02988e64110201d31f4a1635c0907aff3a
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-01-31 22:25:24 +0100

    ISSUE-489, refacor modal code to create reusable components using new CSS structure

---
+++ app/components/repositive-modal/component.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
+
+export default ModalDialog.extend({
+  containerClassNames: ['u-bc-white', 'u-rounded', 'u-pos-fixed', 'u-overflows-hidden', 'c-modal'],
+  overlayClassNames: ['u-pos-fixed', 'u-pos-left0', 'u-pos-right0', 'u-pos-top0', 'u-full-viewport-height',
+    'u-bc-overlay-grey', 'u-zindex-overlay']
+});
--- app/components/repositive-modal/header/component.js
+++ app/components/repositive-modal/header/component.js
@@ -3,8 +3,7 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['modal-header'],
-  classNameBindings: ['alternateBackground'],
+  classNames: ['u-bc-light-bluegrey', 'u-flex', 'u-justify-between', 'u-content-center', 'u-p2'],
   tagName: 'header',
   imgSize: 36
 });
