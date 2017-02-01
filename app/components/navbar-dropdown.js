8e5f015e16743490cc5d6e79faa33ae507fee666
Sebastian Place <sebplace94@gmail.com>
2017-02-01 14:13:49 +0000

    Basic navbar dropdown functionality

--- app/components/navbar-dropdown.js
+++ app/components/navbar-dropdown.js
@@ -1,10 +1,32 @@
 import Ember from 'ember';
+import { later, cancel } from 'ember-runloop';
 
 const { Component, inject: { service }, get } = Ember;
 
 export default Component.extend({
   session: service(),
+
+  classNames: ['c-navbar__profile-dropdown', 'u-border-left', 'u-pl2'],
+
   actions: {
+    prevent() { return false; },
+
+    open(dropdown) {
+      if (this.closeTimer) {
+        cancel(this.closeTimer);
+        this.closeTimer = null;
+      } else {
+        dropdown.actions.open();
+      }
+    },
+    
+    close(dropdown) {
+      this.closeTimer = later(() => {
+        this.closeTimer = null;
+        dropdown.actions.close();
+      }, 400);
+    },
+
     logout() {
       this.flashMessages.clearMessages();
       get(this, 'metrics').trackEvent({
