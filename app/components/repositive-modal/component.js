53bcacbfe66ddcad3e2c3526beef45635b495f70
Tom Byers <hello@tombye.rs>
2017-02-01 12:24:13 +0000

    Reorganising overlay colours

--- app/components/repositive-modal/component.js
+++ app/components/repositive-modal/component.js
@@ -4,5 +4,5 @@ import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
 export default ModalDialog.extend({
   containerClassNames: ['u-bc-white', 'u-rounded', 'u-pos-fixed', 'u-overflows-hidden', 'c-modal'],
   overlayClassNames: ['u-pos-fixed', 'u-pos-left0', 'u-pos-right0', 'u-pos-top0', 'u-full-viewport-height',
-    'u-bc-overlay-grey', 'u-zindex-overlay']
+    'u-bc-overlay-light-grey', 'u-zindex-overlay']
 });
