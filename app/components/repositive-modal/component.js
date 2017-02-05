23871315e7530ae72f6598cfc758e73bfd3bd8fd
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-02-05 20:34:10 +0100

    modals refactor and cleanup

--- app/components/repositive-modal/component.js
+++ app/components/repositive-modal/component.js
@@ -1,10 +1,8 @@
-import Ember from 'ember';
-import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
+import TetherDialog from 'ember-modal-dialog/components/tether-dialog';
 
-export default ModalDialog.extend({
-  containerClassNames: ['u-bc-white', 'u-rounded', 'u-pos-fixed', 'u-overflow-hidden', 'c-modal'],
+export default TetherDialog.extend({
+  containerClassNames: ['u-bc-white', 'u-rounded', 'u-pos-fixed', 'u-overflow-hidden', 'u-zindex-modal'],
   overlayClassNames: ['u-pos-fixed', 'u-pos-left0', 'u-pos-right0', 'u-pos-top0', 'u-pos-bottom0', 'u-full-viewport-height',
     'u-bc-overlay-light-grey', 'u-zindex-overlay'],
-  clickOutsideToClose: true,
-  targetAttachment: 'center'
+  clickOutsideToClose: true
 });
