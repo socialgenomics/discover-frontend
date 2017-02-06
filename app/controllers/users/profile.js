048296956bd5cdfef067303b4cb5537d79e225b3
Sebastian Place <sebplace94@gmail.com>
2017-02-06 16:56:56 +0000

    Reset button on resolve/reject. Set button disabled when not savable. Fix policy page redirection issue.

--- app/controllers/users/profile.js
+++ app/controllers/users/profile.js
@@ -1,13 +1,34 @@
 import Ember from 'ember';
 import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend(
+const { Controller, computed, inject: { service }, get, Logger } = Ember;
+
+export default Controller.extend(
   EmberValidations,
 {
-  session: Ember.inject.service(),
-  firstname: Ember.computed.alias('session.authenticatedUser.firstname'),
-  lastname: Ember.computed.alias('session.authenticatedUser.lastname'),
-  affiliation: Ember.computed.alias('session.authenticatedUser.userProfile.workOrganisation'),
+  session: service(),
+
+  firstname: computed.alias('session.authenticatedUser.firstname'),
+  lastname: computed.alias('session.authenticatedUser.lastname'),
+  affiliation: computed.alias('session.authenticatedUser.userProfile.workOrganisation'),
+  isValid: computed.and(
+    'firstname',
+    'lastname'
+  ),
+  infoHasChanged: computed.or(
+    'session.authenticatedUser.currentState.isDirty',
+    'session.authenticatedUser.profile.currentState.isDirty'
+  ),
+
+  isSavable: computed('isValid', 'infoHasChanged', function() {
+    const isValid = get(this, 'isValid');
+    const infoHasChanged = get(this, 'infoHasChanged');
+    if (isValid && infoHasChanged) {
+      return true;
+    }
+    return false;
+  }),
+
   validations: {
     firstname: {
       presence: {
@@ -20,30 +41,13 @@ export default Ember.Controller.extend(
       }
     }
   },
-  isValid: Ember.computed.and(
-    'firstname',
-    'lastname'
-  ),
-  infoHasChanged: Ember.computed.or(
-    'session.authenticatedUser.currentState.isDirty',
-    'session.authenticatedUser.profile.currentState.isDirty'
-  ),
-
-  isSavable: Ember.computed('isValid', 'infoHasChanged', function() {
-    const isValid = this.get('isValid');
-    const infoHasChanged = this.get('infoHasChanged');
-    if (isValid && infoHasChanged) {
-      return true;
-    }
-    return false;
-  }),
 
   actions: {
-    save: function() {
+    save() {
       const user = this.get('session.authenticatedUser');
-      if (this.isSavable) {
-        user.save()
-        .then((resp) => {
+      if (get(this, 'isSavable')) {
+        return user.save()
+        .then(resp => {
           return resp.profile.save();
         })
         .then(() => {
@@ -52,8 +56,8 @@ export default Ember.Controller.extend(
             type: 'success'
           });
         })
-        .catch((err) => {
-          Ember.Logger.error(err);
+        .catch(err => {
+          Logger.error(err);
           this.flashMessages.add({
             message: 'Sorry. There was a problem saving your changes.',
             type: 'warning'
