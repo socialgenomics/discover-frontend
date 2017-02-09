499df646df635173f40d6da0326dbc453d8d8f5d
Tom Byers <hello@tombye.rs>
2017-02-09 16:33:07 +0000

    Centering repositive-modal header content

--- app/components/repositive-modal/header/component.js
+++ app/components/repositive-modal/header/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['c-modal-header', 'u-bc-light-bluegrey', 'u-flex', 'u-justify-between', 'u-content-center', 'u-py1', 'u-px2'],
+  classNames: ['c-modal-header', 'u-bc-light-bluegrey', 'u-px2'],
   classNameBindings: ['alternateBackground:alternate-background'],
   tagName: 'header',
   imgSize: 36
