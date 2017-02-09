d6f09e4a18f05ded65fa9534ce6ec7e23148b202
Sebastian Place <sebplace94@gmail.com>
2017-02-09 14:27:36 +0000

    Fix styling on open-page banner

--- app/components/full-page-notification/component.js
+++ app/components/full-page-notification/component.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['c-call-to-action-banner', 'u-flex', 'u-justify-center', 'u-items-center', 'u-pt2']
+  classNames: ['c-call-to-action-banner', 'u-flex', 'u-justify-center', 'u-items-center', 'u-p2']
 });
