df44b66b952b156e91dc2e9ba832c8d8466db85e
Sebastian Place <sebplace94@gmail.com>
2017-02-09 13:48:42 +0000

    Fix test

--- app/components/meta-panel.js
+++ app/components/meta-panel.js
@@ -4,14 +4,14 @@ const { Component, set } = Ember;
 
 export default Component.extend({
   tagName: 'section',
-  classNameBindings:['metaPanelHidden:is-hidden'],
+  classNameBindings: ['metaPanelHidden:is-hidden'],
   classNames: ['c-sidebar', 'u-pos-absolute', 'grid'],
   displayInfo: true,
   metaPanelHidden: true,
   actions: {
     showInfo() { set(this, 'displayInfo', true); },
     showFilters() { set(this, 'displayInfo', false); },
-    toggleMetaPanelVisibility() { this.toggleProperty('metaPanelHidden')}
+    toggleMetaPanelVisibility() { this.toggleProperty('metaPanelHidden'); }
   },
   didReceiveAttrs() {
     this._super(...arguments);
