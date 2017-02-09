bdefacb833782ac0142c07c9adb2491d5fd1e2b3
Sebastian Place <sebplace94@gmail.com>
2017-02-09 12:57:18 +0000

    Fix failing tests

--- app/components/repositive-modal/header/component.js
+++ app/components/repositive-modal/header/component.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['u-bc-light-bluegrey', 'u-flex', 'u-justify-between', 'u-content-center', 'u-py1', 'u-px2'],
+  classNames: ['c-modal-header', 'u-bc-light-bluegrey', 'u-flex', 'u-justify-between', 'u-content-center', 'u-py1', 'u-px2'],
+  classNameBindings: ['alternateBackground:alternate-background'],
   tagName: 'header',
   imgSize: 36
 });
