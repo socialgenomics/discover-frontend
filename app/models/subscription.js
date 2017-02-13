640528a49323f2fa154def942722599127737529
Sebastian Place <sebplace94@gmail.com>
2017-02-13 14:29:01 +0000

    Add notification model and define relationship with subscription.

--- app/models/subscription.js
+++ app/models/subscription.js
@@ -1,10 +1,11 @@
 import DS from 'ember-data';
 import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   active: attr('bool'),
   createdAt: attr('isodate'),
+  notifcations: hasMany('notification'),
   subscribable_model: attr('string'),
   subscribableId: belongsTo('subscribable'),
   userId: belongsTo('user')
