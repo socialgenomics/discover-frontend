ecb94807724d791d0e247210f521bbbef6b70aa4
Sebastian Place <sebplace94@gmail.com>
2017-02-15 14:16:04 +0000

    Style notifier for navbar.

--- app/components/navbar-notifier.js
+++ app/components/navbar-notifier.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 const { Component, computed } = Ember;
 
 export default Component.extend({
-  classNames: ['u-flex', 'u-self-stretch', 'u-shrink-none', 'u-items-center', 'u-justify-center', 'u-border-left', 'u-hv-bc-off-white'],
+  classNames: ['u-flex', 'u-self-stretch', 'u-shrink-none', 'u-items-center', 'u-justify-center', 'u-hv-bc-off-white'],
   classNameBindings: ['hasNotifications:u-tc-red:u-tc-secondary'],
   hasNotifications: computed.notEmpty('notifications'),
   actions: {
