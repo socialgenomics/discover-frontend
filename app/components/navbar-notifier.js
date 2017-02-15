f4d833fda630d4d600a085fab7501880f7829b02
Sebastian Place <sebplace94@gmail.com>
2017-02-15 12:08:40 +0000

    TDD notifier component

---
+++ app/components/navbar-notifier.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+const { Component, computed } = Ember;
+
+export default Component.extend({
+  classNames: ['u-flex', 'u-self-stretch', 'u-shrink-none', 'u-items-center', 'u-justify-center', 'u-border-left', 'u-hv-bc-off-white'],
+  classNameBindings: ['hasNotifications:u-tc-red:u-tc-secondary'],
+  hasNotifications: computed.notEmpty('notifications'),
+  actions: {
+    close(dropdown) {
+      dropdown.actions.close();
+    }
+  }
+});
