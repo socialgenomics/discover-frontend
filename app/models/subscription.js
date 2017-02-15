9477b047b5bd5822a972074232bee1fba900d4a9
Sebastian Place <sebplace94@gmail.com>
2017-02-15 15:19:47 +0000

    Correct transform name to boolean

--- app/models/subscription.js
+++ app/models/subscription.js
@@ -3,7 +3,7 @@ import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
-  active: attr('bool'),
+  active: attr('boolean'),
   notifcations: hasMany('notification'),
   subscribableModel: attr('string'),
   subscribableId: belongsTo('subscribable'),
