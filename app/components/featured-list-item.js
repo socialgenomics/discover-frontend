8c3049b35709c32c1663855fe1ca3528beda1ab3
danielgynn <danielgynn@hotmail.co.uk>
2017-02-16 15:08:16 +0000

    Include hover effect for the featured list items on home page

--- app/components/featured-list-item.js
+++ app/components/featured-list-item.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['featured-list-item']
+  classNames: ['u-hv-bc-white', 'u-px2', 'u-py1']
 });
