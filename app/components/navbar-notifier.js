ade384b2a609f68482e36ff55c946a0028342474
Sebastian Place <sebplace94@gmail.com>
2017-02-17 14:01:59 +0000

    Fix tests

--- app/components/navbar-notifier.js
+++ app/components/navbar-notifier.js
@@ -3,15 +3,19 @@ import Ember from 'ember';
 const { Component, computed, inject: { service }, get } = Ember;
 
 export default Component.extend({
-  notifications: service(),
+  notificationsService: service('notifications'),
+  session: service(),
 
   classNames: ['u-flex', 'u-self-stretch', 'u-shrink-none', 'u-items-center', 'u-justify-center', 'u-hv-bc-off-white'],
   classNameBindings: ['hasNotifications:u-tc-red:u-tc-secondary'],
   hasNotifications: computed.notEmpty('notifications'),
-  init() {
-    this._super(...arguments);
-    get(this, 'notifications').getNotifications();
-  },
+
+  notifications: computed('notificationsService', 'session', function() {
+    if (get(this, 'session.isAuthenticated')) {
+      return get(this, 'notificationsService.getNotifications')(get(this, 'session.authenticatedUser.id'));
+    }
+  }),
+
   actions: {
     close(dropdown) {
       dropdown.actions.close();
--- app/services/notifications.js
+++ app/services/notifications.js
@@ -4,20 +4,16 @@ const { Service, inject: { service }, get, Logger } = Ember;
 
 export default Service.extend({
   store: service(),
-  session: service(),
 
-  getNotifications() {
-    if (get(this, 'session.isAuthenticated')) {
-      const currentUserId = get(this, 'session.session.authenticated.user.id');
-      const store = get(this, 'store');
-      return store.query('notification', {
-        'where.user_id': currentUserId,
-        'order[0][0]': 'created_at',
-        'order[0][1]': 'DESC',
-      })
-        .then(notifications => {
-          console.log(notifications);
-        }).catch(Logger.error);
-    }
+  getNotifications(userId) {
+    const store = get(this, 'store');
+    return store.query('notification', {
+      'where.user_id': userId,
+      'order[0][0]': 'created_at',
+      'order[0][1]': 'DESC'
+    })
+      .then(notifications => {
+        console.log(notifications);
+      }).catch(Logger.error);
   }
 });
