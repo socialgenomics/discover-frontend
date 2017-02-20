91076ebeb7f64835f377c53ae29e12d6d282bfee
danielgynn <danielgynn@hotmail.co.uk>
2017-02-20 16:59:16 +0000

    Use ember-radio-button library instead of component

--- app/components/radio-button.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-const { Component, computed } = Ember;
-
-export default Component.extend({
-  classNames: ['u-block u-mb1 u-ta-left'],
-  classNameBindings: ['checked']
-});
