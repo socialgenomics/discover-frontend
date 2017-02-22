5d4bbc7b5914d0ef1b828e6b45561941a1d5991f
Sebastian Place <sebplace94@gmail.com>
2017-02-22 12:31:14 +0000

    Make whole notification clickable

--- app/components/notification-list-item.js
+++ app/components/notification-list-item.js
@@ -1,8 +1,19 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, get, set } = Ember;
 
 export default Component.extend({
   tagName: 'li',
-  classNames: ['u-p2', 'u-border-bottom']
+  classNames: ['u-p2', 'u-border-bottom', 'u-hv-bc-white', 'u-cursor-pointer'],
+
+  click() { this.touchEnd(); },
+  touchEnd() {
+    const notification = get(this, 'notification');
+    const subscribableModel = get(notification, 'subscriptionId.subscribableModel');
+    const subscribableId = get(notification, 'subscriptionId.subscribableId.id');
+    get(this, 'transitionToSubscribable')(subscribableModel, subscribableId);
+    set(notification, 'status', 'seen');
+    notification.save();
+  }
+
 });
