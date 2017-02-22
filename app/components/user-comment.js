d35bb9a6b735c593f4a04f1d27379351f20ad94a
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-02-22 22:26:40 +0100

    ISSUE-597, implement UI for editing comments

--- app/components/user-comment.js
+++ app/components/user-comment.js
@@ -1,16 +1,62 @@
 import Ember from 'ember';
 import CheckEditPermissionsMixin from 'repositive/mixins/check-edit-permissions-mixin';
+import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-const { Component, computed: { oneWay } } = Ember;
+const { Component, computed: { oneWay }, get, set, setProperties } = Ember;
+const Validations = buildValidations({
+  text: validator('presence', {
+    presence: true,
+    message: 'This field can\'t be blank.'
+  })
+});
 
-export default Component.extend(CheckEditPermissionsMixin, {
-  showCreateAccountModal: false,
+export default Component.extend(
+  CheckEditPermissionsMixin,
+  FlashMessageMixin,
+  Validations,
+  {
+    showCreateAccountModal: false,
+    inEditMode: false,
 
-  checkEditPermissionsModel: oneWay('comment'),
+    checkEditPermissionsModel: oneWay('comment'),
+    text: oneWay('comment.properties.text'),
 
-  actions: {
-    toggleCreateAccountModal() {
-      this.toggleProperty('showCreateAccountModal');
+    actions: {
+      toggleCreateAccountModal() {
+        this.toggleProperty('showCreateAccountModal');
+      },
+
+      enterEditMode() {
+        set(this, 'inEditMode', true);
+      },
+
+      cancelEditMode() {
+        setProperties(this, {
+          inEditMode: false,
+          text: get(this, 'comment.properties.text')
+        });
+      },
+
+      save() {
+        const comment = get(this, 'comment');
+
+        set(comment, 'properties.text', get(this, 'text'));
+        comment
+          .save()
+          .then(this._onEditSuccess.bind(this))
+          .catch(this._onEditError.bind(this));
+      }
+    },
+
+    _onEditSuccess() {
+      set(this, 'inEditMode', false);
+      this._addFlashMessage('Your comment has benn uploaded.', 'success');
+    },
+    _onEditError() {
+      get(this, 'comment').rollbackAttributes();
+      set(this, 'inEditMode', false);
+      this._addFlashMessage('There was problem with updating you comment.', 'warning');
     }
   }
-});
+);
