473f9cf681a26979203e1c1a15694228f16bbfd4
Sebastian Place <sebplace94@gmail.com>
2017-02-22 15:37:52 +0000

    More layout tweaks, prevent overflow. Close dropdown on notification click.

--- app/components/notification-list-item.js
+++ app/components/notification-list-item.js
@@ -14,5 +14,6 @@ export default Component.extend({
     get(this, 'transitionToSubscribable')(subscribableModel, subscribableId);
     set(notification, 'status', 'seen');
     notification.save();
+    get(this, 'close')(get(this, 'dropdown'));
   }
 });
