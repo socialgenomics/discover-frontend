6f885a6cab5412909478f59887c496182a0480b0
Sebastian Place <sebplace94@gmail.com>
2017-02-23 17:06:50 +0000

    Some changes to the data loading

--- app/routes/datasets/unfollow.js
+++ app/routes/datasets/unfollow.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
 
-const { inject: { service }, Route, get, set, RSVP, Logger } = Ember;
+const { inject: { service }, Route, get, set, Logger } = Ember;
 
 export default Route.extend(AuthenticatedRouteMixin, FlashMessageMixin, {
   session: service(),
@@ -13,10 +13,11 @@ export default Route.extend(AuthenticatedRouteMixin, FlashMessageMixin, {
       'where.user_id': userId,
       'where.subscribable_id': params.id
     })
-    .then(() => {
-      return set(this, 'subscription.active', false)
-      .then(this._addFlashMessage('You have successfully unfollowed this dataset.', 'success'));
-    })
-    .catch(Logger.error);
+      .then(subscription => {
+        set(subscription, 'active', false);
+        return subscription.save();
+      })
+        .then(this._addFlashMessage('You have successfully unfollowed this dataset.', 'success'))
+        .catch(Logger.error);
   }
 });
