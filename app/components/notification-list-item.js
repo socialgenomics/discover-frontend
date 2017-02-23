f00e6bcec003a7e4905d6c63b3a656877f1bc123
Sebastian Place <sebplace94@gmail.com>
2017-02-23 12:15:40 +0000

    Add conditional to notification to check if it is from an action. Add notification-list-item base tests

--- app/components/notification-list-item.js
+++ app/components/notification-list-item.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 
-const { Component, get, set } = Ember;
+const { Component, computed, get, set } = Ember;
 
 export default Component.extend({
   tagName: 'li',
   classNames: ['u-p2', 'u-pr3', 'u-flex', 'u-items-center', 'u-border-bottom', 'u-hv-bc-white', 'u-cursor-pointer'],
-
+  isAction: computed('notification', function() {
+    return get(this, 'notification.properties.type') === 'action' ? true : false;
+  }),
   click() { this.touchEnd(); },
   touchEnd() {
     const notification = get(this, 'notification');
--- tests/integration/components/notification-list-item-test.js
+++ tests/integration/components/notification-list-item-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
@@ -8,17 +9,67 @@ describe('Integration | Component | notification list item', function() {
     integration: true
   });
 
-  it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#notification-list-item}}
-    //     template content
-    //   {{/notification-list-item}}
-    // `);
+  function buildNotificationObj() {
+    return Ember.Object.create({
+      createdAt: new Date().toISOString(),
+      properties: {
+        type: 'action',
+        action: {
+          type: 'comment',
+          userId: {
+            displayName: 'Cat Dog',
+            userProfile: {
+              avatar: 'www.example.com/pic'
+            }
+          }
+        }
+      },
+      subscriptionId: {
+        subscribableId: {},
+        subscribableModel: 'dataset'
+      }
+    });
+  }
 
-    this.render(hbs`{{notification-list-item}}`);
-    expect(this.$()).to.have.length(1);
+  it(`if notification is from an action, it shows the user's name and avatar`, function() {
+    this.set('notification', buildNotificationObj());
+    this.render(hbs`{{notification-list-item notification=notification}}`);
+    expect(this.$('.c-notification-img-date-container').text().trim()).to.eql('a few seconds ago');
+    expect(this.$('span:first').text().trim()).to.eql('Cat Dog');
+    expect(this.$('img').attr('src')).to.eql('www.example.com/pic');
+  });
+
+  it(`if notification is from an action, there is moment converted date text`, function() {
+    const notification = buildNotificationObj();
+    this.set('notification', notification);
+    this.render(hbs`{{notification-list-item notification=notification}}`);
+    expect(this.$('.c-notification-img-date-container').text().trim()).to.eql('a few seconds ago');
+  });
+
+  it(`correct action text is show for each action type`, function() {
+    const notification = buildNotificationObj();
+    this.set('notification', notification);
+    this.render(hbs`{{notification-list-item notification=notification}}`);
+    expect(this.$('.c-notification-body span:nth-child(2)').text().trim()).to.eql('commented on');
+    this.set('notification.properties.action.type', 'tag');
+    expect(this.$('.c-notification-body span:nth-child(2)').text().trim()).to.eql('added a tag to');
+    this.set('notification.properties.action.type', 'share');
+    expect(this.$('.c-notification-body span:nth-child(2)').text().trim()).to.eql('shared');
+    this.set('notification.properties.action.type', 'favourite');
+    expect(this.$('.c-notification-body span:nth-child(2)').text().trim()).to.eql('favourited');
+  });
+
+  it(`shows dataset title when subscribable is dataset`, function() {
+    this.set('notification', buildNotificationObj());
+    this.set('notification.subscriptionId.subscribableId.dataset', { title: 'Dataset Title' });
+    this.render(hbs`{{notification-list-item notification=notification}}`);
+    expect(this.$('.c-notification-body p').text().trim()).to.eql('Dataset Title');
+  });
+
+  it(`shows request title when subscribable is request`, function() {
+    this.set('notification', buildNotificationObj());
+    this.set('notification.subscriptionId.subscribableId.request', { title: 'Request Title' });
+    this.render(hbs`{{notification-list-item notification=notification}}`);
+    expect(this.$('.c-notification-body p').text().trim()).to.eql('Request Title');
   });
 });
