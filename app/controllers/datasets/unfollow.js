33c0862eb07ef233fec4f70959141d269d82626e
danielgynn <danielgynn@hotmail.co.uk>
2017-02-27 11:26:53 +0000

    Only enable async-button when radio value is selected

--- app/controllers/datasets/unfollow.js
+++ app/controllers/datasets/unfollow.js
@@ -1,21 +1,28 @@
 import Ember from 'ember';
 
-const { Controller, Logger } = Ember;
+const { Controller, Logger, computed, get, set } = Ember;
 
 export default Controller.extend({
+  isInvalid: true,
 
   actions: {
+    enableSubmit() {
+      return set(this, 'isInvalid', false)
+    },
+
     submitForm() {
-      return this.transitionToRoute('root')
-      .then(() => {
-        this.flashMessages.add({
-          message: 'Thank you for your feedback.',
-          type: 'success'
+      if (!get(this, 'isInvalid')) {
+        return this.transitionToRoute('root')
+        .then(() => {
+          this.flashMessages.add({
+            message: 'Thank you for your feedback.',
+            type: 'success'
+          });
+        })
+        .catch((err) => {
+          Logger.error(err);
         });
-      })
-      .catch((err) => {
-        Logger.error(err);
-      });
+      }
     }
   }
 });
