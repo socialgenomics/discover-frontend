e61f86fcbf67d7708ad74a546983b8575a9558a3
danielgynn <danielgynn@hotmail.co.uk>
2017-03-02 16:46:08 +0000

    Update pagination button appearance

--- app/components/pagination-bar.js
+++ app/components/pagination-bar.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 
-const { Component, get } = Ember;
+const { Component, get, computed } = Ember;
 
 export default Component.extend({
-  classNames: ['u-mb3'],
-  
+  classNames: ['u-mb2', 'u-flex', 'u-justify-center'],
+
+  noPrevPage: computed('currentPageNumber', function() {
+    if (get(this, 'currentPageNumber') === 1) { return true; }
+  }),
+
+  noNextPage: computed('currentPageNumber', 'totalPages', function() {
+    if (get(this, 'currentPageNumber') >= get(this, 'totalPages')) { return true; }
+  }),
+
   actions: {
     nextPage() { get(this, 'nextPage')(); },
     previousPage() { get(this, 'previousPage')(); }
