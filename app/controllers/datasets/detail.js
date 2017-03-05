5ad10fe530696165261fd1ccb0f535833a2de0dc
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-03-05 19:45:02 +0100

    move dataset and request details card content to separate component

--- app/controllers/datasets/detail.js
+++ app/controllers/datasets/detail.js
@@ -1,123 +1,40 @@
 import Ember from 'ember';
-import CheckEditPermissionsMixin from 'repositive/mixins/check-edit-permissions-mixin';
-import EditModeMixin from 'repositive/mixins/edit-mode-mixin';
-import Validations from 'repositive/validations/dataset';
 
-const { Controller, computed, inject: { service }, Logger, get, set, setProperties } = Ember;
+const { Controller, computed, inject: { service }, get } = Ember;
 
-export default Controller.extend(
-  EditModeMixin,
-  Validations,
-  CheckEditPermissionsMixin,
-  {
-    session: service(),
-    urlGenerator: service(),
+export default Controller.extend({
+  session: service(),
 
-    editablePropertyKeys: ['title', 'description', 'url'],
+  datasetEditableProperties: [
+    { key: 'title' },
+    { key: 'description', multiline: true },
+    { key: 'url' }
+  ],
 
-    dataset: computed.alias('model.dataset'),
-    stats: computed.alias('model.stats'),
-    comments: computed.filterBy('dataset.actionableId.actions', 'type', 'comment'),
-    tags: computed.filterBy('dataset.actionableId.actions', 'type', 'tag'),
+  dataset: computed.alias('model.dataset'),
+  stats: computed.alias('model.stats'),
+  comments: computed.filterBy('dataset.actionableId.actions', 'type', 'comment'),
+  tags: computed.filterBy('dataset.actionableId.actions', 'type', 'tag'),
 
-    checkEditPermissionsModel: computed.oneWay('dataset'),
+  commentsSorted : computed.sort('comments', (itemA, itemB) => {
+    if (itemA.get('createdAt') < itemB.get('createdAt')) {
+      return 1;
+    } else if (itemA.get('createdAt') > itemB.get('createdAt')) {
+      return -1;
+    }
+    return 0;
+  }),
 
-    // copy of editable Properties
-    title: computed.oneWay('dataset.title'),
-    description: computed.oneWay('dataset.description'),
-    url: computed.oneWay('dataset.url'),
+  datasetsNumber: computed('stats.datasets', function() {
+    return get(this, 'stats.datasets').toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }),
 
-    commentsSorted : computed.sort('comments', (itemA, itemB) => {
-      if (itemA.get('createdAt') < itemB.get('createdAt')) {
-        return 1;
-      } else if (itemA.get('createdAt') > itemB.get('createdAt')) {
-        return -1;
-      }
-      return 0;
-    }),
-
-    datasetsNumber: computed('stats.datasets', function() {
-      return get(this, 'stats.datasets').toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    }),
-
-    datasetUrl: computed('type', 'dataset.id', function () {
-      return get(this, 'urlGenerator').generateUrl('datasets.detail', get(this, 'dataset.id'));
-    }),
-
-    actions: {
-      trackExit() {
-        const userId = get(this, 'session.authenticatedUser');
-        const dataset = get(this, 'dataset');
-        this.store.createRecord('action', {
-          actionableId: get(dataset, 'actionableId'),
-          actionable_model: dataset.constructor.modelName,
-          userId: userId,
-          type: 'access'
-        }).save().catch(Logger.error);
-
-        get(this, 'metrics').trackEvent({
-          category: 'discover_homeauth_datasetDetail',
-          action: 'download_button',
-          label: get(this, 'dataset.title')
-        });
-        window.open(get(this, 'dataset.url'), '_blank').focus();
-      },
-
-      trackLinkEvent() {
-        get(this, 'metrics').trackEvent({
-          category: 'discover_openpage_datasetBanner_searchNow',
-          action: 'link_clicked'
-        });
-      },
-
-      addComment(text) {
-        const userId = get(this, 'session.authenticatedUser');
-        const dataset = get(this, 'dataset');
-        this.store.createRecord('action', {
-          actionableId: get(dataset, 'actionableId'),
-          actionable_model: dataset.constructor.modelName,
-          userId: userId,
-          type: 'comment',
-          properties: {
-            text: text
-          }
-        }).save().catch(Logger.error);
-      },
-
-      addTag(text) {
-        const userId = get(this, 'session.authenticatedUser');
-        const dataset = get(this, 'dataset');
-        const existingTags = get(this, 'tags');
-        // if the tag already exists
-        if (existingTags.findBy('properties.text', text)) {
-          this.flashMessages.add({
-            message: 'The tag: ' + text + ' already exists.',
-            type: 'warning'
-          });
-        } else {
-          const tag = this.store.createRecord('action', {
-            actionableId: get(dataset, 'actionableId'),
-            actionable_model: dataset.constructor.modelName,
-            userId: userId,
-            type: 'tag',
-            properties: {
-              text: text
-            }
-          });
-          tag.save().catch(Logger.error);
-        }
-      },
-      toggleTagModal() {
-        this.toggleProperty('isShowingTagModal');
-      },
-
-      cancelEditMode() {
-        this.resetModuleStateOnCancel('dataset', get(this, 'editablePropertyKeys'));
-      },
-
-      save() {
-        this.saveChanges(get(this, 'dataset'), get(this, 'editablePropertyKeys'));
-      }
+  actions: {
+    trackLinkEvent() {
+      get(this, 'metrics').trackEvent({
+        category: 'discover_openpage_datasetBanner_searchNow',
+        action: 'link_clicked'
+      });
     }
   }
-);
+});
--- app/controllers/requests/detail.js
+++ app/controllers/requests/detail.js
@@ -1,94 +1,25 @@
 import Ember from 'ember';
-import CheckEditPermissionsMixin from 'repositive/mixins/check-edit-permissions-mixin';
-import EditModeMixin from 'repositive/mixins/edit-mode-mixin';
-import Validations from 'repositive/validations/request';
 
-const { Controller, computed, inject: { service }, Logger, get, set, setProperties } = Ember;
+const { Controller, computed, inject: { service } } = Ember;
 
-export default Controller.extend(
-  EditModeMixin,
-  Validations,
-  CheckEditPermissionsMixin,
-  {
-    session: service(),
-    urlGenerator: service(),
+export default Controller.extend({
+  session: service(),
 
-    editablePropertyKeys: ['title', 'description'],
+  requestEditableProperties: [
+    { key: 'title' },
+    { key: 'description', multiline: true }
+  ],
 
-    request: computed.alias('model.request'),
-    comments: computed.filterBy('request.actionableId.actions', 'type', 'comment'),
-    tags: computed.filterBy('request.actionableId.actions', 'type', 'tag'),
+  request: computed.alias('model.request'),
+  comments: computed.filterBy('request.actionableId.actions', 'type', 'comment'),
+  tags: computed.filterBy('request.actionableId.actions', 'type', 'tag'),
 
-    checkEditPermissionsModel: computed.oneWay('request'),
-
-    // copy of editable Properties
-    title: computed.oneWay('request.title'),
-    description: computed.oneWay('request.description'),
-
-    commentsSorted: computed.sort('comments', (itemA, itemB) => {
-      if (itemA.get('createdAt') < itemB.get('createdAt')) {
-        return 1;
-      } else if (itemA.get('createdAt') > itemB.get('createdAt')) {
-        return -1;
-      }
-      return 0;
-    }),
-
-    requestUrl: computed('request.id', function () {
-      return get(this, 'urlGenerator').generateUrl('requests.detail', get(this, 'request.id'));
-    }),
-
-    actions: {
-      addComment(text) {
-        const userId = this.get('session.authenticatedUser');
-        const request = this.get('request');
-        let comment = this.store.createRecord('action', {
-          actionableId: request.get('actionableId'),
-          actionable_model: request.constructor.modelName,
-          userId: userId,
-          type: 'comment',
-          properties: {
-            text: text
-          }
-        });
-        comment.save().catch(Logger.error);
-      },
-
-      addTag(text) {
-        const userId = this.get('session.authenticatedUser');
-        const request = this.get('request');
-        const existingTags = this.get('tags');
-        // if the tag already exists
-        if (existingTags.findBy('properties.text', text)) {
-          this.flashMessages.add({
-            message: 'The tag: ' + text + ' already exists.',
-            type: 'warning'
-          });
-        } else {
-          const tag = this.store.createRecord('action', {
-            actionableId: request.get('actionableId'),
-            actionable_model: request.constructor.modelName,
-            userId: userId,
-            type: 'tag',
-            properties: {
-              text: text
-            }
-          });
-          tag.save().catch(Logger.error);
-        }
-      },
-
-      toggleTagModal() {
-        this.toggleProperty('isShowingTagModal');
-      },
-
-      cancelEditMode() {
-        this.resetModuleStateOnCancel('request', get(this, 'editablePropertyKeys'));
-      },
-
-      save() {
-        this.saveChanges(get(this, 'request'), get(this, 'editablePropertyKeys'));
-      }
+  commentsSorted: computed.sort('comments', (itemA, itemB) => {
+    if (itemA.get('createdAt') < itemB.get('createdAt')) {
+      return 1;
+    } else if (itemA.get('createdAt') > itemB.get('createdAt')) {
+      return -1;
     }
-  }
-);
+    return 0;
+  })
+});
