c2384c2606e717494e9cd88ba5ca611fbaceff50
Sebastian Place <sebplace94@gmail.com>
2017-03-08 13:39:41 +0000

    Add sanitize extension for showdown. Use it on comments.

--- app/initializers/register-showdown-extensions.js
+++ app/initializers/register-showdown-extensions.js
@@ -1,9 +1,20 @@
+import sanitizeHtml from 'npm:sanitize-html';
+
 export function initialize() {
   showdown.extension('targetBlankLinks', function() {
     return [{
       type: 'html',
       regex: '<a',
       replace: '<a target="_blank" rel="noopener noreferrer"'
+    }];
+  });
+
+  showdown.extension('sanitize', function() {
+    return [{
+      type: 'html',
+      filter: function(text, converter, options) {
+        return sanitizeHtml(text);
+      }
     }];
   });
 }
