d53b6ab0dc866a2902d1015d0b6cccda9063ea75
Sebastian Place <sebplace94@gmail.com>
2017-03-10 10:00:39 +0000

    Updated is-new-label logic. Sort datasources.

--- app/components/is-new-label.js
+++ app/components/is-new-label.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
+import moment from 'moment';
 
-const { Component } = Ember;
+const { Component, computed, inject: { service }, get } = Ember;
 
 export default Component.extend({
+  moment: service(),
+
   tagName: 'span',
-  classNames: ['u-tc-red', 'u-fs0', 'u-pos-relative']
+  classNames: ['u-tc-red', 'u-fs0'],
+
+  isLessThanTwoWeeksOld: computed('updatedAt', function() {
+    const date = moment(get(this, 'updatedAt'));
+    const today = moment(new Date());
+    const timeSince = today.diff(date, 'days');
+    return timeSince <= 14;
+  })
 });
