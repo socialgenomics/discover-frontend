d3840425407b49ee3ca01d760b9555a92d685fcc
Sebastian Place <sebplace94@gmail.com>
2017-03-10 16:50:18 +0000

    Add basic grouping of attributes

--- app/components/attribute-list.js
+++ app/components/attribute-list.js
@@ -1,5 +1,49 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed, get } = Ember;
 
-export default Component.extend({});
+export default Component.extend({
+  keys: ['Assay', 'Samples', 'Tissue', 'Technology', 'Pubmed ID'],
+
+  attributes: [
+    {
+      key: 'Assay',
+      value: 'RNA-seq',
+      userId: {
+        displayName: 'Testy McTestface'
+      }
+    },
+    {
+      key: 'Assay',
+      value: 'WGS',
+      userId: {
+        displayName: 'Boris McNorris'
+      }
+    },
+    {
+      key: 'Samples',
+      value: '24',
+      userId: {
+        displayName: 'Doris In A Forest'
+      }
+    },
+    {
+      key: 'Technology',
+      value: 'ZX Spectrum',
+      userId: {
+        displayName: 'Boris McNorris'
+      }
+    }
+  ],
+
+  groups: computed('attributes', 'keys', function() {
+    const attributes = get(this, 'attributes');
+    return {
+      'Assay': attributes.filterBy('key', 'Assay'),
+      'Sample': attributes.filterBy('key', 'Sample'),
+      'Tissue': attributes.filterBy('key', 'Tissue'),
+      'Technology': attributes.filterBy('key', 'Technology'),
+      'Pubmed ID': attributes.filterBy('key', 'Pubmed ID')
+    };
+  })
+});
