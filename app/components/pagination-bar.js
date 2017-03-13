a596e61b4a5235d729ae42c41f3deb6c3622a602
danielgynn <danielgynn@hotmail.co.uk>
2017-03-13 13:37:43 +0000

    Move pagination logic to component

--- app/components/pagination-bar.js
+++ app/components/pagination-bar.js
@@ -5,6 +5,15 @@ const { Component, get, computed } = Ember;
 export default Component.extend({
   classNames: ['u-pt3', 'u-mb2', 'u-flex', 'u-justify-center', 'u-flex-wrap'],
 
+  pageNumbers: [1, 2, 3, 4, 5],
+
+  pages: computed('page', function() {
+    if (get(this, 'page') % 5 === 0) {
+      return this._updatePageNumberList(get(this, 'pageNumbers'));
+    }
+    return get(this, 'pageNumbers');
+  }),
+
   noPrevPage: computed.equal('currentPageNumber', 1),
 
   noNextPage: computed('currentPageNumber', 'totalPages', function() {
@@ -14,6 +23,13 @@ export default Component.extend({
   actions: {
     nextPage() { get(this, 'nextPage')(); },
     previousPage() { get(this, 'previousPage')(); },
-    thisPage() { get(this, 'thisPage')(); }
-  }
+    goToPage() { get(this, 'goToPage')(); }
+  },
+
+  /**
+  * @param {array} pages - the list of page numbers.
+  */
+  _updatePageNumberList(pageNumbers) {
+    return pageNumbers.map(arrayElement => arrayElement + 4);
+  },
 });
