1dd58b3af8ef2068c1c422fcaf2981784da4e153
danielgynn <danielgynn@hotmail.co.uk>
2017-03-14 16:42:59 +0000

    Add select box to change number of search results

--- app/mixins/search-controller.js
+++ app/mixins/search-controller.js
@@ -8,6 +8,7 @@ export default Mixin.create({
   query: null,
   page: 1,
   resultsPerPage: 6,
+  resultsOptions: [6, 15, 30, 90],
 
   totalPages: computed('model.meta.total', 'resultsPerPage', function () {
     return Math.ceil(get(this, 'model.meta.total') / get(this, 'resultsPerPage'));
@@ -42,6 +43,10 @@ export default Mixin.create({
 
     removeFilter(predicate, text) {
       this._toggleFilter('removeFilter', predicate, text);
+    },
+
+    selectResults(resultsPerPage) {
+      set(this, 'resultsPerPage', resultsPerPage);
     }
   },
 
