2fdb3064a38d765fe93b9ca765b91a30d7f14c7c
danielgynn <danielgynn@hotmail.co.uk>
2017-03-14 15:15:42 +0000

    Fix page number edge cases

--- app/components/pagination-bar.js
+++ app/components/pagination-bar.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { Component, get, computed } = Ember;
+const { Component, get, set, computed } = Ember;
 
 export default Component.extend({
   classNames: ['u-pt3', 'u-mb2', 'u-flex', 'u-justify-center', 'u-flex-wrap'],
@@ -8,7 +8,7 @@ export default Component.extend({
   pageNumbers: [1, 2, 3, 4, 5],
 
   pages: computed('pageNumbers', 'currentPageNumber', function() {
-    return this._updatePageNumberList(get(this, 'pageNumbers'), get(this, 'currentPageNumber'));
+    return this._updatePageNumberList(get(this, 'pageNumbers'), get(this, 'currentPageNumber'), get(this, 'totalPages'));
   }),
 
   noPrevPage: computed.equal('currentPageNumber', 1),
@@ -26,11 +26,14 @@ export default Component.extend({
   /**
   * @param {array} pages - the list of page numbers.
   */
-  _updatePageNumberList(pageNumbers, currentPageNumber) {
-    if (currentPageNumber > 2) {
-      return pageNumbers.map(page => page + currentPageNumber - 3);
-    } else {
-      return pageNumbers.map(page => page);
-    }
+  _updatePageNumberList(pageNumbers, currentPageNumber, totalPages) {
+    const popValue = 0;
+
+    if (currentPageNumber === totalPages) { set(this, 'popValue', 5); } // End of array - don't allow > totalPages
+    else if (currentPageNumber === totalPages - 1) { set(this, 'popValue', 4); } // Penultimate array item
+    else if (currentPageNumber > 2) { set(this, 'popValue', 3); } // Begninning of array - don't allow 0 and below
+    else { set(this, 'popValue', 1); } // Default page numbers
+
+    return pageNumbers.map(page => page + currentPageNumber - get(this, 'popValue'));
   }
 });
