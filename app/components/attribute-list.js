ea4472059906c516d5f21f9300dedd748d37c5a3
Sebastian Place <sebplace94@gmail.com>
2017-03-15 15:55:59 +0000

    Convert datasetAttributes into common obj. Convert attribute actions into common obj.

--- app/components/attribute-list.js
+++ app/components/attribute-list.js
@@ -3,58 +3,42 @@ import Ember from 'ember';
 const { Component, computed, get, set } = Ember;
 
 export default Component.extend({
-  keys: ['Assay', 'Samples', 'Tissue', 'Technology', 'Pubmed ID'],
 
-  didReceiveAttrs() {
-    this._super(...arguments);
-    // debugger;
-  },
+  // attributes: [
+  //   {
+  //     key: 'Assay',
+  //     value: 'RNA-seq',
+  //     userId: {
+  //       id: '1',
+  //       displayName: 'Testy McTestface'
+  //     }
+  //   },
+  //   {
+  //     key: 'Assay',
+  //     value: 'WGS',
+  //     userId: {
+  //       id: '2',
+  //       displayName: 'Boris McNorris'
+  //     }
+  //   },
+  //   {
+  //     key: 'Samples',
+  //     value: '24',
+  //     userId: {
+  //       id: '3',
+  //       displayName: 'Doris In A Forest'
+  //     }
+  //   },
+  //   {
+  //     key: 'Technology',
+  //     value: 'ZX Spectrum',
+  //     userId: {
+  //       id: '2',
+  //       displayName: 'Boris McNorris'
+  //     }
+  //   }
+  // ],
 
-  attributes: [
-    {
-      key: 'Assay',
-      value: 'RNA-seq',
-      userId: {
-        id: '1',
-        displayName: 'Testy McTestface'
-      }
-    },
-    {
-      key: 'Assay',
-      value: 'WGS',
-      userId: {
-        id: '2',
-        displayName: 'Boris McNorris'
-      }
-    },
-    {
-      key: 'Samples',
-      value: '24',
-      userId: {
-        id: '3',
-        displayName: 'Doris In A Forest'
-      }
-    },
-    {
-      key: 'Technology',
-      value: 'ZX Spectrum',
-      userId: {
-        id: '2',
-        displayName: 'Boris McNorris'
-      }
-    }
-  ],
-
-  //Target structure of attr object
-  // {
-  //   key:
-  //   value:
-  //   actionId: ?
-  //   userId: ?
-  // }
-
-  //Get actions
-  //Convert attr actions to common object
   //Convert dataset attrs to common object
   //Merge these into one array.
 
@@ -70,17 +54,43 @@ export default Component.extend({
     pubmed: ['123']
   },
 
-  groups: computed('attributes', function() {
-    const attributes = get(this, 'attributes');
-    return {
-      'Assay': attributes.filterBy('key', 'Assay'),
-      'Sample': attributes.filterBy('key', 'Sample'),
-      'Tissue': attributes.filterBy('key', 'Tissue'),
-      'Technology': attributes.filterBy('key', 'Technology'),
-      'Pubmed ID': attributes.filterBy('key', 'Pubmed ID')
-    };
+  userGeneratedAttributes: computed('attributeActions', function() {
+    return get(this, 'attributeActions')
+      .map(attribute => this._convertActionToCommonObj(attribute));
   }),
+
+  datasetAttributes: computed('attributesFromDataset', function() {
+    const attributesFromDataset = get(this, 'attributesFromDataset');
+
+    return Object.keys(attributesFromDataset).reduce((attrObjects, key) => {
+      attributesFromDataset[key].map(value => {
+        attrObjects.push({ key, value });
+      });
+      return attrObjects;
+    }, []);
+  }),
+
+  // groups: computed('attributes', function() {
+  //   const attributes = get(this, 'attributes');
+  //   return {
+  //     'Assay': attributes.filterBy('key', 'Assay'),
+  //     'Sample': attributes.filterBy('key', 'Sample'),
+  //     'Tissue': attributes.filterBy('key', 'Tissue'),
+  //     'Technology': attributes.filterBy('key', 'Technology'),
+  //     'Pubmed ID': attributes.filterBy('key', 'Pubmed ID')
+  //   };
+  // }),
+
   actions: {
     setOpenInput(key) { set(this, 'openInput', key); }
-  }
+  },
+
+  _convertActionToCommonObj(attribute) {
+    return {
+      key: get(attribute, 'properties.key'),
+      value: get(attribute, 'properties.value'),
+      actionId: get(attribute, 'id'),
+      userId: get(attribute, 'userId.id')
+    };
+  },
 });
