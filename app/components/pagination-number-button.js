490a54c2fdf93be6fccfcd6161afb3cb42bb7957
danielgynn <danielgynn@hotmail.co.uk>
2017-03-16 10:19:02 +0000

    Fix pagination-number-button style issue

--- app/components/pagination-number-button.js
+++ app/components/pagination-number-button.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 const { Component, computed, get } = Ember;
 
 export default Component.extend({
-  tagName: 'p',
-
   isActive: computed('page', 'currentPageNumber', function() {
     return get(this, 'page') === get(this, 'currentPageNumber');
   }),
