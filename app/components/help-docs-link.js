694e474ebfd2afd0cd84b29cc6a1144dd77bfae8
danielgynn <danielgynn@hotmail.co.uk>
2017-03-20 12:53:09 +0000

    Set active help-docs-link to bold

--- app/components/help-docs-link.js
+++ app/components/help-docs-link.js
@@ -4,7 +4,7 @@ const { Component, get, computed } = Ember;
 
 export default Component.extend({
   tagName: 'li',
-  classNames: ['u-p1', 'u-hv-bc-darken5'],
+  classNames: ['c-help-link', 'u-p1', 'u-hv-bc-darken5'],
 
   isActive: computed('query', function() {
     return ('?help=' + get(this, 'query') === window.location.search);
