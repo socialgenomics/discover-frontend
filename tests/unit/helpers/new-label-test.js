deb45de505391875a01962949cbc1243341213c6
Sebastian Place <sebplace94@gmail.com>
2017-03-20 16:02:23 +0000

    Restructure tests

--- tests/unit/helpers/new-label-test.js
+++ tests/unit/helpers/new-label-test.js
@@ -5,23 +5,27 @@ import { newLabel } from 'repositive/helpers/new-label';
 import moment from 'moment';
 
 describe('newLabelHelper', function() {
-  it('returns nothing for a date not within default days', function() {
-    const result = newLabel([new Date('December 17, 1995')]);
-    expect(result).to.be.undefined;
-  });
+  describe('date is older than days', function() {
+    it('returns nothing for a date not within default days', function() {
+      const result = newLabel([new Date('December 17, 1995')]);
+      expect(result).to.be.undefined;
+    });
 
-  it('should return new when no days are specified and date is within default days', function() {
-    const result = newLabel([moment(new Date()).subtract(14, 'days')]);
-    expect(result.string).to.eql('<span class="u-tc-red u-fs0"> NEW </span>');
-  });
+    it('returns nothing when the date is not within the days specified', function() {
+      const result = newLabel([moment(new Date()).subtract(21, 'days'), 20]);
+      expect(result).to.be.undefined;
+    });
+  })
 
-  it('should return nothing when the date is not within the days specified', function() {
-    const result = newLabel([moment(new Date()).subtract(21, 'days'), 20]);
-    expect(result).to.be.undefined;
-  });
+  describe('date is within days', function() {
+    it('returns new when no days are specified', function() {
+      const result = newLabel([moment(new Date()).subtract(14, 'days')]);
+      expect(result.string).to.eql('<span class="u-tc-red u-fs0"> NEW </span>');
+    });
 
-  it('should return new when the date is within the days specified', function() {
-    const result = newLabel([moment(new Date()).subtract(19, 'days'), 20]);
-    expect(result.string).to.eql('<span class="u-tc-red u-fs0"> NEW </span>');
-  });
+    it('returns new when the date is within the days specified', function() {
+      const result = newLabel([moment(new Date()).subtract(19, 'days'), 20]);
+      expect(result.string).to.eql('<span class="u-tc-red u-fs0"> NEW </span>');
+    });
+  })
 });
