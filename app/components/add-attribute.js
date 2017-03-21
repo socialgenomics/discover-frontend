9a3a89311061ae0ac300f9e8f35642d0a51a64fe
Sebastian Place <sebplace94@gmail.com>
2017-03-21 16:36:47 +0000

    Add presence validation to add-attribute. Create duplicateValidator.

--- app/components/add-attribute.js
+++ app/components/add-attribute.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
+import { buildValidations } from 'ember-cp-validations';
+import presenceValidator from 'repositive/validations/presenceValidator';
+import duplicateValidator from 'repositive/validations/duplicateValidator';
 
 const { Component, get } = Ember;
+const Validations = buildValidations({
+  attributeValue: [
+    presenceValidator('You must enter a value.')
+  ]
+});
 
-export default Component.extend({
+export default Component.extend(Validations, {
   actions: {
     addAttribute() {
       get(this, 'addAttribute')(get(this, 'group'), get(this, 'attributeValue'));
