006f4db5ce4ea690392469ce78610c3f26a4c4ea
Sebastian Place <sebplace94@gmail.com>
2017-03-21 14:42:27 +0000

    Add sttribute tests

--- tests/integration/components/add-attribute-test.js
+++ tests/integration/components/add-attribute-test.js
@@ -2,23 +2,38 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
 
 describe('Integration | Component | add attribute', function() {
   setupComponentTest('add-attribute', {
     integration: true
   });
+  describe('actions', function() {
+    beforeEach(function () {
+      this.setProperties({
+        'addAttribute': sinon.spy(),
+        'closeInput': sinon.spy(),
+        'group': 'assay'
+      })
+      this.render(hbs`{{add-attribute addAttribute=addAttribute closeInput=closeInput group=group}}`);
+    });
 
-  it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#add-attribute}}
-    //     template content
-    //   {{/add-attribute}}
-    // `);
+    describe('addAttribute', function() {
+      it('calls passed in addAttribute function with correct args', function() {
+        this.$('input').val('ABC').change();
+        this.$('button.c-btn-primary').click();
 
-    this.render(hbs`{{add-attribute}}`);
-    expect(this.$()).to.have.length(1);
+        expect(this.get('addAttribute').calledOnce).to.be.true;
+        expect(this.get('addAttribute').calledWith('assay', 'ABC')).to.be.true;
+      });
+    });
+
+    describe('cancel', function() {
+      it('calls passed in addAttribute function with correct args', function() {
+        this.$('button.c-btn-text-secondary').click();
+
+        expect(this.get('closeInput').calledOnce).to.be.true;
+      });
+    });
   });
 });
