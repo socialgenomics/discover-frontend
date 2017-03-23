fc53058feddde11f9ba51287fd9d09323a9eb745
danielgynn <danielgynn@hotmail.co.uk>
2017-03-23 09:18:23 +0000

    Test more use cases for _updatePageNumberList

--- tests/unit/components/pagination-bar-test.js
+++ tests/unit/components/pagination-bar-test.js
@@ -2,7 +2,9 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 
-describe('Unit | Component | filters list', function() {
+const { setProperties } = Ember;
+
+describe('Unit | Component | pagination bar', function() {
   setupComponentTest('pagination-bar', {
     unit: true,
     needs: [
@@ -12,17 +14,47 @@ describe('Unit | Component | filters list', function() {
   });
 
   describe('_updatePageNumberList', function () {
-    const props = {
+    let props = {
       pageNumbers: [1, 2, 3, 4, 5],
-      currentPageNumber: 7,
-      totalPages: 10
+      currentPageNumber: 1,
+      totalPages: 5
     };
+
+    it('should return default page numbers', function () {
+      const component = this.subject();
+
+      expect(component._updatePageNumberList(
+        props.pageNumbers,
+        props.currentPageNumber,
+        props.totalPages).toString()).to.eql('1,2,3,4,5');
+    });
+
     it('should return updated page numbers array', function () {
       const component = this.subject();
+
+      setProperties(props, {
+        currentPageNumber: 7,
+        totalPages: 10
+      });
+
       expect(component._updatePageNumberList(
         props.pageNumbers,
         props.currentPageNumber,
         props.totalPages).toString()).to.eql('5,6,7,8,9');
+    });
+
+    it('should return end of page numbers array for penultimate page', function () {
+      const component = this.subject();
+
+      setProperties(props, {
+        currentPageNumber: 14,
+        totalPages: 15
+      });
+
+      expect(component._updatePageNumberList(
+        props.pageNumbers,
+        props.currentPageNumber,
+        props.totalPages).toString()).to.eql('11,12,13,14,15');
     });
   });
 });
