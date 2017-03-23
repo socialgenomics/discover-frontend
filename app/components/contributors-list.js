43b3346fa964045a8730641bbaf90a5de18571f2
danielgynn <danielgynn@hotmail.co.uk>
2017-03-23 14:04:55 +0000

    Get contributors id list from userGeneratedAttributes

--- app/components/contributors-list.js
+++ app/components/contributors-list.js
@@ -1,7 +1,24 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { inject: { service }, Component, get, computed, Logger } = Ember;
 
 export default Component.extend({
-  classNames: ['u-border-top']
+  store: service(),
+
+  classNames: ['u-border-top'],
+
+  contributorList: computed.mapBy('userAttrs', 'userId'),
+  uniqContributors: computed.uniq('contributorList'),
+
+  contributors: computed('uniqContributors', function() {
+    const contr = get(this, 'uniqContributors').toString();
+
+    return this._getContributors(contr);
+  }),
+
+  _getContributors(contrId) {
+    const store = get(this, 'store');
+
+    return store.findRecord('user', contrId);
+  },
 });
