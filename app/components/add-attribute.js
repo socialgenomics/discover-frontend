477a51c3f14b50ff512a46945dcd9225ca6562cd
Sebastian Place <sebplace94@gmail.com>
2017-03-23 16:34:44 +0000

    Issue 687 -Attribute validation (#735)
    * Add presence validation to add-attribute. Create duplicateValidator.
    
    * Rm duplicate validator. Check for duplicates another way
    
    * Add tests for addAttr validations

--- app/components/add-attribute.js
+++ app/components/add-attribute.js
@@ -1,12 +1,26 @@
 import Ember from 'ember';
+import { buildValidations } from 'ember-cp-validations';
+import presenceValidator from 'repositive/validations/presenceValidator';
 
-const { Component, get } = Ember;
+const { Component, get, set, computed } = Ember;
 
-export default Component.extend({
+const Validations = buildValidations({
+  attributeValue: presenceValidator('You must enter a value.')
+});
+
+export default Component.extend(Validations, {
+  isNotUnique: computed('attributesForKey', 'attributeValue', function() {
+    const attrInput = (get(this, 'attributeValue') || '').toLowerCase();
+    return get(this, 'attributesForKey')
+      .any(attr => attr.value.toLowerCase() === attrInput);
+  }),
+
   actions: {
-    addAttribute() {
-      get(this, 'addAttribute')(get(this, 'group'), get(this, 'attributeValue'));
-      get(this, 'closeInput')();
+    add() {
+      if (get(this, 'validations.isValid') && !get(this, 'isNotUnique')) {
+        get(this, 'addAttribute')(get(this, 'group'), get(this, 'attributeValue'));
+        get(this, 'closeInput')();
+      }
     },
     cancel() { get(this, 'closeInput')(); }
   }
