95660217f1d6226a268f92989109869fda078f9e
danielgynn <danielgynn@hotmail.co.uk>
2017-03-24 11:35:24 +0000

    Make whole link content block clickable

--- app/components/help-docs-link.js
+++ app/components/help-docs-link.js
@@ -4,7 +4,7 @@ const { Component, get, computed } = Ember;
 
 export default Component.extend({
   tagName: 'li',
-  classNames: ['c-help-link', 'u-p1', 'u-hv-bc-darken5'],
+  classNames: ['c-help-link'],
 
   isActive: computed('query', function() {
     const queryLink = '?help=' + get(this, 'query');
