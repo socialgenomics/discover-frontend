d3d8bc3ac33c95a74b5d94eb4221da62737f400a
Sebastian Place <sebplace94@gmail.com>
2017-03-24 13:56:35 +0000

    Issue 688 - Edit and delete attributes (#739)
    * Allow attribute-list-item to be editted by the user that owns it.
    
    * attr changes can be saved
    
    * Fix pending button state
    
    * Remove all validated-input submit attributes. Enclose all validated-inputs in forms.
    
    * Align edit-pencil correctly.
    
    * Remove + icon (/repositive/report-issues/issues/692)
    
    * Make vaguely responsive
    
    * Reset page on transition (#738)
    
    * Delete attrs

---
+++ app/components/attribute-list-item.js
@@ -0,0 +1,54 @@
+import Ember from 'ember';
+import { buildValidations } from 'ember-cp-validations';
+import presenceValidator from 'repositive/validations/presenceValidator';
+import CheckEditPermissionsMixin from 'repositive/mixins/check-edit-permissions-mixin';
+import EditModeMixin from 'repositive/mixins/edit-mode-mixin';
+
+const { Component, computed, get, set, inject: { service }, Logger } = Ember;
+
+const Validations = buildValidations({
+  value: presenceValidator('You must enter a value.')
+});
+
+export default Component.extend(
+  CheckEditPermissionsMixin,
+  EditModeMixin,
+  Validations,
+  {
+    store: service(),
+
+    value: computed.oneWay('attribute.value'),
+    editablePropertyKeys: ['value'],
+    checkEditPermissionsModel: computed.oneWay('attribute'),
+    isNotUnique: computed('attributesForKey', 'value', function() {
+      const attrInput = (get(this, 'value') || '').toLowerCase();
+      return get(this, 'attributesForKey')
+        .any(attr => attr.value.toLowerCase() === attrInput);
+    }),
+    actions: {
+      cancelEditMode() {
+        this.resetModuleStateOnCancel('attribute', get(this, 'editablePropertyKeys'));
+      },
+
+      save() {
+        if (get(this, 'validations.isValid') && !get(this, 'isNotUnique')) {
+          const attribute = get(this, 'attribute');
+          const attrAction = get(this, 'store')
+            .peekRecord('action', get(attribute, 'actionId'));
+          set(attrAction, 'properties.value', get(this, 'value'));
+          set(attribute, 'value', get(this, 'value'));
+          return this.persistChanges(attrAction);
+        }
+      },
+
+      deleteAttr() {
+        const attribute = get(this, 'attribute');
+        const attrAction = get(this, 'store')
+          .peekRecord('action', get(attribute, 'actionId'));
+        return attrAction.destroyRecord()
+          .then(this._addFlashMessage('Attribute successfully deleted.', 'success'))
+          .catch(Logger.error)
+      }
+    }
+  }
+);
