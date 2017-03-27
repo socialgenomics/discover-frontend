1f1637a971da8b0fd18bde9168b3ed6841fc6a19
danielgynn <danielgynn@hotmail.co.uk>
2017-03-27 14:11:51 +0100

    Loop help-tabs object rather than duplicate manually

--- app/components/help-tabs.js
+++ app/components/help-tabs.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-  queryParams: ['tab'],
-  tab: 'searching'
+const { Component } = Ember;
+
+export default Component.extend({
+  tabs: [
+    { title: 'Searching for Data', target: 'help.searching-for-data', queryParams: 'search-query', noBorder: true },
+    { title: 'Requesting Data', target: 'help.requesting-data', queryParams: 'what-is' },
+    { title: 'Registering new Data', target: 'help.registering-new-data', queryParams: 'data-reg' },
+    { title: 'Your Account', target: 'help.your-account', queryParams: 'access' },
+    { title: 'Other Common Issues', target: 'help.other', queryParams: 'why-tag' }
+  ]
 });
