80ba4cfb22e362dc10d22efe9c66bce698874a5f
danielgynn <danielgynn@hotmail.co.uk>
2017-03-30 10:32:25 +0100

    Style and responsiveness adjustments

--- app/components/results-select.js
+++ app/components/results-select.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 const { Component, get } = Ember;
 
 export default Component.extend({
-  classNames: ['u-flex', 'u-justify-end', 'u-items-center'],
+  classNames: ['u-flex', 'u-justify-end', 'u-items-center', 'u-hide-on-sm-md'],
 
   actions: {
     setResultsPerPage(resultsPerPage) { get(this, 'setResultsPerPage')(resultsPerPage); }
