2603ce80177d2273cc791f82b3dfc8368c200679
Sebastian Place <sebplace94@gmail.com>
2017-04-03 11:37:25 +0100

    Write integration tests for new tab component

--- tests/integration/components/tab-list-action-item-test.js
+++ tests/integration/components/tab-list-action-item-test.js
@@ -1,24 +1,43 @@
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import { beforeEach, describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
 
 describe('Integration | Component | tab list action item', function() {
   setupComponentTest('tab-list-action-item', {
     integration: true
   });
 
-  it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#tab-list-action-item}}
-    //     template content
-    //   {{/tab-list-action-item}}
-    // `);
+  describe('tab is active', function() {
+    it('it should have an active class', function() {
+      this.render(hbs`{{tab-list-action-item activeTab='abc' tabName='abc'}}`);
+      expect(this.$('li').hasClass('u-bc-white')).to.be.true;
+    });
+  })
 
-    this.render(hbs`{{tab-list-action-item}}`);
-    expect(this.$()).to.have.length(1);
+  describe('tab is inactive', function() {
+    beforeEach(function() {
+      this.set('setActiveTab', sinon.spy());
+      this.render(hbs`{{tab-list-action-item
+        activeTab='abc'
+        tabName='def'
+        setActiveTab=setActiveTab
+      }}`);
+    });
+
+    it('background should be off-white', function() {
+      expect(this.$('li').hasClass('u-bc-off-white')).to.be.true;
+    });
+
+    it('should have a hover effect', function() {
+      expect(this.$('li').hasClass('u-hv-bc-off-white')).to.be.true;
+    });
+
+    it('on click, the setActiveTab function is called with the tabName', function() {
+      this.$('li').click();
+      expect(this.get('setActiveTab').calledOnce).to.be.true;
+      expect(this.get('setActiveTab').calledWith('def')).to.be.true;
+    });
   });
 });
