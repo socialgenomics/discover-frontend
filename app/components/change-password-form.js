f46fad8617f8de85c8985be65425e8aaf9a4c41d
danielgynn <danielgynn@hotmail.co.uk>
2017-04-06 14:09:37 +0100

    Move validations logic to component

--- app/components/change-password-form.js
+++ app/components/change-password-form.js
@@ -1,9 +1,38 @@
 import Ember from 'ember';
+import { buildValidations } from 'ember-cp-validations';
+import presenceValidator from 'repositive/validations/presenceValidator';
+import lengthValidator from 'repositive/validations/lengthValidator';
+import passwordFormatValidator from 'repositive/validations/passwordFormatValidator';
+import { errorMessages, lengths, lengthTypes } from 'repositive/validations/validations-config';
 
-const { Component, get, set } = Ember;
+const { Component, get, set, computed } = Ember;
 
-export default Component.extend({
+const Validations = buildValidations({
+  password1: [
+    presenceValidator(errorMessages.newPassword),
+    lengthValidator(lengths.password, lengthTypes.min),
+    passwordFormatValidator()
+  ],
+  password2: [
+    presenceValidator(errorMessages.matchingPassword),
+    lengthValidator(lengths.password, lengthTypes.min),
+    passwordFormatValidator()
+  ]
+});
+
+export default Component.extend(Validations, {
   tagName: 'form',
+
+  resetKey: null,
+  password1: null,
+  password2: null,
+  loading: false,
+  passwordChanged: false,
+
+  isDisabled: computed('loading', 'validations.isValid', 'password1', 'password2', function() {
+    return !get(this, 'validations.isValid') || get(this, 'loading') ||
+      get(this, 'password1') !== get(this, 'password2');
+  }),
 
   actions: {
     submitForm() {
