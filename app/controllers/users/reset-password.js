3341994bb0cef2d7b201b27c14d8ea896e5a562a
danielgynn <danielgynn@hotmail.co.uk>
2017-04-10 10:55:42 +0100

    Revert reset-password to old flow

--- app/controllers/users/reset-password.js
+++ app/controllers/users/reset-password.js
@@ -1,11 +1,39 @@
 import Ember from 'ember';
 import ENV from 'repositive/config/environment';
 import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
+import { buildValidations } from 'ember-cp-validations';
+import presenceValidator from 'repositive/validations/presenceValidator';
+import lengthValidator from 'repositive/validations/lengthValidator';
+import passwordFormatValidator from 'repositive/validations/passwordFormatValidator';
+import { errorMessages, lengths, lengthTypes } from 'repositive/validations/validations-config';
 
-const { Controller, Logger, get, set, setProperties, inject: { service }, RSVP } = Ember;
+const { Controller, computed, Logger, get, set, setProperties, inject: { service }, RSVP } = Ember;
+const Validations = buildValidations({
+  password1: [
+    presenceValidator(errorMessages.newPassword),
+    lengthValidator(lengths.password, lengthTypes.min),
+    passwordFormatValidator()
+  ],
+  password2: [
+    presenceValidator(errorMessages.matchingPassword),
+    lengthValidator(lengths.password, lengthTypes.min),
+    passwordFormatValidator()
+  ]
+});
 
-export default Controller.extend(FlashMessageMixin, {
+export default Controller.extend(Validations, FlashMessageMixin, {
   ajax: service(),
+
+  resetKey: null,
+  password1: null,
+  password2: null,
+  loading: false,
+  passwordChanged: false,
+
+  isDisabled: computed('loading', 'validations.isValid', 'password1', 'password2', function() {
+    return !get(this, 'validations.isValid') || get(this, 'loading') ||
+      get(this, 'password1') !== get(this, 'password2');
+  }),
 
   actions: {
     submitForm() {
