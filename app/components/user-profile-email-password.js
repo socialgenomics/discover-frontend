443b903c14c790971880f76a04aa3d1f6d1c8fbf
danielgynn <danielgynn@hotmail.co.uk>
2017-04-11 11:20:39 +0100

    Add different states for email verification

--- app/components/user-profile-email-password.js
+++ app/components/user-profile-email-password.js
@@ -1,12 +1,27 @@
 import Ember from 'ember';
 import ENV from 'repositive/config/environment';
 import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
+import { buildValidations } from 'ember-cp-validations';
+import presenceValidator from 'repositive/validations/presenceValidator';
+import emailFormatValidator from 'repositive/validations/emailFormatValidator';
+import { errorMessages } from 'repositive/validations/validations-config';
 
-const { Component, get, set, Logger, inject: { service } } = Ember;
+const { Component, get, set, setProperties, computed, Logger, inject: { service } } = Ember;
+
+const Validations = buildValidations({
+  email: [
+    presenceValidator(errorMessages.blankEmail),
+    emailFormatValidator()
+  ]
+});
 
 export function verifyEmail() {
   const email = get(this, 'credential.main_credential.email');
-  set(this, 'sentEmail', true);
+  setProperties(this, {
+    'sentEmail': true,
+    'loading': true,
+    'addingCredential': false
+  });
   get(this, 'ajax').request(ENV.APIRoutes['verify-email-resend'] + `/${email}`, { method: 'GET' })
     .then(() => {
       this._addFlashMessage('We have sent a verification email to your inbox', 'success');
@@ -17,12 +32,22 @@ export function verifyEmail() {
     });
 }
 
-export default Component.extend(FlashMessageMixin, {
+export default Component.extend(FlashMessageMixin, Validations, {
   session: service(),
   ajax: service(),
+
+  loading: false,
   sentEmail: false,
+  addingCredential: false,
+
+  isInvalid: computed.not('validations.isValid'),
+  isDisabled: computed.or('loading', 'isInvalid'),
 
   actions: {
-    resendVerifyEmail: verifyEmail
+    resendVerifyEmail: verifyEmail,
+
+    addCredential() {
+      set(this, 'addingCredential', true);
+    }
   }
 });
