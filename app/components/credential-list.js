c39a9da562376152723381149c1863927bf2322f
danielgynn <danielgynn@hotmail.co.uk>
2017-04-13 15:04:27 +0100

    Prepare set to primary and delete credential options

--- app/components/credential-list.js
+++ app/components/credential-list.js
@@ -1,11 +1,32 @@
 import Ember from 'ember';
+import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
 
-const { Component } = Ember;
+const { Component, get, set, Logger, inject: { service } } = Ember;
 
-export default Component.extend({
+export default Component.extend(FlashMessageMixin, {
+  store: service(),
+
   actions: {
     addCredential() {
       this.sendAction('addCredential')
+    },
+
+    makePrimary(email) {
+      return get(this, 'store').query('credential', {
+        'where.email': email
+      })
+        .then(credential => {
+          set(credential, 'primary', true)
+        })
+        .catch(Logger.error);
+    },
+
+    deleteCredential(credential) {
+      credential.destroyRecord()
+      .then(() => {
+        this._addFlashMessage('The credential ' + credential.email + 'has been deleted successfully.', 'success');
+      })
+      .catch(Logger.error);
     }
   }
 });
