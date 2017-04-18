67442a77ae665a9e4102143626c82632a6993a9d
danielgynn <danielgynn@hotmail.co.uk>
2017-04-18 16:21:47 +0100

    Allow twitter links from handles

--- app/components/account-link.js
+++ app/components/account-link.js
@@ -5,5 +5,10 @@ const { Component, get, computed } = Ember;
 export default Component.extend({
   accountImg: computed('accountName', function() {
     return get(this, 'accountName').toLowerCase();
+  }),
+
+  accountLink: computed('account', function() {
+    if (get(this, 'account').charAt(0) === '@') return 'https://twitter.com/' + get(this, 'account');
+    else return get(this, 'account');
   })
 });
