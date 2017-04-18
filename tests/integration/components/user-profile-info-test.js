34799cf8f63b04dd6076cd59ca5120ffa48022f1
Sebastian Place <sebplace94@gmail.com>
2017-04-18 15:20:23 +0100

    Add placeholder link to edit profile. Cpmnponentize info section. Write tests.

---
+++ tests/integration/components/user-profile-info-test.js
@@ -0,0 +1,66 @@
+import { expect } from 'chai';
+import { beforeEach, describe, it } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
+
+describe('Integration | Component | user profile info', function() {
+  const user = {
+    displayName: 'ABC 123',
+    profile: {
+      avatar: 'https://dg2kcfbxc77v1.cloudfront.net/assets/images/avatar/frog-1ee2413df3627d1724a7f26ff7f9fc3a.png',
+      bio: 'A short bio',
+      location: 'Cambridge',
+      work_organisation: 'Repositive',
+      work_role: 'Developer'
+    },
+    createdAt: moment(new Date('December 17, 1995'))
+  };
+  setupComponentTest('user-profile-info', {
+    integration: true
+  });
+  describe('when the profile belongs to the current user', function() {
+    beforeEach(function() {
+      this.setProperties({
+        'isOwnProfile': true,
+        'user': user,
+        'isVerified': true
+      });
+      this.render(hbs`{{user-profile-info
+        isOwnProfile=isOwnProfile
+        user=user
+        isVerified=isVerified}}`);
+    });
+
+    it('edit profile icon is displayed', function() {
+      expect(this.$('i').hasClass('fa-pencil')).to.be.true;
+    });
+
+    describe('when the user isVerified', function() {
+      it('renders verified account badge', function() {
+        expect(this.$('h4').text().trim()).to.eql('Verified Account');
+      });
+    });
+
+    describe('when the user is not verified', function() {
+      it('should diplay message telling user to verify account', function() {
+        this.set('isVerified', false);
+        expect(this.$('h4').text().trim()).to.eql('Please verify your email');
+      });
+    });
+
+    describe('user has no work_organisation', function() {
+      it('renders link to edit profile', function() {
+        this.set('user.profile.work_organisation', null);
+        expect(this.$('a').eq(0).text().trim()).to.eql('Add where you work');
+      });
+    });
+
+    describe('user has no work_role', function() {
+      it('renders link to edit profile', function() {
+        this.set('user.profile.work_role', null);
+        expect(this.$('a').eq(1).text().trim()).to.eql('Add your job role');
+      });
+    });
+  });
+});
