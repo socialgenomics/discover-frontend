4ecab0a1aced3a53f2aa4c1083340ef582aadee0
danielgynn <danielgynn@hotmail.co.uk>
2017-04-19 12:44:14 +0100

    Test component and check if accounts is empty

--- tests/integration/components/account-link-test.js
+++ tests/integration/components/account-link-test.js
@@ -8,17 +8,23 @@ describe('Integration | Component | account link', function() {
     integration: true
   });
 
-  it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#account-link}}
-    //     template content
-    //   {{/account-link}}
-    // `);
+  it('renders correct data and icon for account link', function() {
+    this.setProperties({
+      account: 'https://google.com/repositive',
+      accountName: 'Google'
+    });
+    this.render(hbs`{{account-link account=account accountName=accountName}}`);
+    expect(this.$('a.u-flex p.u-tc-secondary').text().trim()).to.eql('Google');
+    expect(this.$('a.u-flex svg')).to.have.length(1);
+  });
 
-    this.render(hbs`{{account-link}}`);
-    expect(this.$()).to.have.length(1);
+  it('renders correct account link for twitter', function() {
+    this.setProperties({
+      account: '@repositiveio',
+      accountName: 'Twitter'
+    });
+    this.render(hbs`{{account-link account=account accountName=accountName}}`);
+    const $accountLink = this.$('a.u-flex');
+    expect($accountLink.attr('href')).to.eql('https://twitter.com/@repositiveio');
   });
 });
