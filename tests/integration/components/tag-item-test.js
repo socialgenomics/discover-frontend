2607c42160692d4d9e97eb234d5d96a045e46d6e
Sebastian Place <sebplace94@gmail.com>
2017-04-20 11:37:33 +0100

    Add more tag-item tests. Style fixes

--- tests/integration/components/tag-item-test.js
+++ tests/integration/components/tag-item-test.js
@@ -21,9 +21,7 @@ describe('Integration | Component | tag item', function() {
       this.setProperties({
         type: 'tag',
         userId: '1',
-        tag: {
-          destroyRecord: sinon.spy()
-        }
+        tag: {}
       });
       this.render(hbs`{{tag-item
         type=type
@@ -33,9 +31,13 @@ describe('Integration | Component | tag item', function() {
         session=session}}`);
     });
 
+    it('renders the text within a link', function() {
+      expect(this.$('a').text().trim()).to.eql('ABC');
+    });
+
     describe('when the tag belongs to the user', function() {
       it('renders a delete button', function() {
-        expect(this.$('span i').hasClass('fa-times')).to.be.true;
+        expect(this.$('.t-tag-item-delete i').hasClass('fa-times')).to.be.true;
       });
     });
 
@@ -43,6 +45,36 @@ describe('Integration | Component | tag item', function() {
       it('does not render a delete button', function() {
         this.set('userId', '2');
         expect(this.$('span i').hasClass('fa-times')).to.be.false;
+      });
+    });
+  });
+
+  describe('given a tag of type interest', function() {
+    beforeEach(function() {
+      this.setProperties({
+        userId: '1',
+        type: 'interest',
+        delete: sinon.spy()
+      });
+      this.render(hbs`{{tag-item
+        type=type
+        tagText=tagText
+        userId=userId
+        delete=delete
+        session=session}}`);
+    });
+
+    it('displays the text and is not a link', function() {
+      expect(this.$('span').eq(0).text().trim()).to.eql('ABC');
+    });
+
+    describe('when a userId is provided', function() {
+      it('should render delete button if it belongs to the user', function() {
+        expect(this.$('i').hasClass('fa-times')).to.be.true;
+      });
+      it('clicking the delete button should call delete', function() {
+        this.$('.t-tag-item-delete').click();
+        expect(this.get('delete').calledOnce).to.be.true;
       });
     });
   });
