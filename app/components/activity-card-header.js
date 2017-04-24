0338c7b0536e7d6fb82cb96b6d79ae97ca4d9354
danielgynn <danielgynn@hotmail.co.uk>
2017-04-24 12:28:08 +0100

    Include card borders

--- app/components/activity-card-header.js
+++ app/components/activity-card-header.js
@@ -4,7 +4,7 @@ const { Component, computed, get } = Ember;
 
 export default Component.extend({
   classNames: ['u-border-bottom', 'u-border-fat', 'u-p2'],
-  classNameBindings: ['isRegistration:u-border-color-red', 'isRequest:u-border-color-blue', 'isFavourite:u-border-color-yellow'],
+  classNameBindings: ['isRegistration:u-border-color-red', 'isRequest:u-border-color-blue', 'isFavourite:u-border-color-yellow', 'isContribution:u-border-color-teal', 'isDiscussion:u-border-color-grey'],
   isRequest: computed('group', function() {
     if (get(this, 'group') === 'request') { return true; }
   }),
@@ -13,5 +13,11 @@ export default Component.extend({
   }),
   isFavourite: computed('group', function() {
     if (get(this, 'group') === 'favourite') { return true; }
+  }),
+  isContribution: computed('group', function() {
+    if (get(this, 'group') === 'contribution') { return true; }
+  }),
+  isDiscussion: computed('group', function() {
+    if (get(this, 'group') === 'discussion') { return true; }
   })
 });
