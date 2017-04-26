6465a79e1c28e5de0cc5b862f8260605ea5cc724
Sebastian Place <sebplace94@gmail.com>
2017-04-26 14:41:15 +0100

    776 - Avatar upload fixes (#772)
    * Send avatar to correct route. Only allow certain image types
    
    * Attach fileKey to upload
    
    * Remove unneccessary colour assertions
    
    * Fix method name

--- config/environment.js
+++ config/environment.js
@@ -46,20 +46,21 @@ module.exports = function(environment) {
         Mapping of route names to API paths, needed for non ember-data calls to
         the API.
        */
-      APIRoutes : (function() {
+      APIRoutes: (function() {
         var mapping = {
+          'avatar': '/avatar',
           'auth.oauth': '/auth/oauth',
-          'users.login' : '/auth/login',
-          'users.logout' : '/auth/logout',
-          'users.signup' : '/auth/register',
-          'datasets.search' : '/search',
+          'users.login': '/auth/login',
+          'users.logout': '/auth/logout',
+          'users.signup': '/auth/register',
+          'datasets.search': '/search',
           'collection-stats': '/collection-stats/{collection_id}',
           'collection-datasets': '/collection-datasets/{collection_id}',
           'stats': '/stats',
-          'datasets.trending' : '/trending',
-          'datasources' : '/datasources',
-          'favourite-datasets' : '/favourites/{user_id}/dataset',
-          'favourite-requests' : '/favourites/{user_id}/request',
+          'datasets.trending': '/trending',
+          'datasources': '/datasources',
+          'favourite-datasets': '/favourites/{user_id}/dataset',
+          'favourite-requests': '/favourites/{user_id}/request',
           'users.profiles': '/users/{id}/profile',
           'reset-password': '/auth/password-reset',
           'verify-email': '/auth/verify',
@@ -77,14 +78,14 @@ module.exports = function(environment) {
       /*
         conf for the auth system
        */
-      'ember-simple-auth' : {
+      'ember-simple-auth': {
         routeAfterAuthentication: 'root',
         routeIfAlreadyAuthenticated: 'root',
         authenticationRoute: 'users.login',
         signupRoute: 'users.signup',
         logoutRoute: 'users.logout'
       },
-      'simple-auth-cookie-store' : {
+      'simple-auth-cookie-store': {
         cookieName: 'repositive.io'
       },
       flashMessageDefaults: {
@@ -105,11 +106,11 @@ module.exports = function(environment) {
       torii: {
         providers: {
           'google-oauth2': {
-            apiKey:      '625615936247-udf8t0o94vmk5tp48pbsdsog84g4vu93.apps.googleusercontent.com',
+            apiKey: '625615936247-udf8t0o94vmk5tp48pbsdsog84g4vu93.apps.googleusercontent.com',
             redirectUri: 'http://localhost:4200'
           },
           'linked-in-oauth2': {
-            apiKey:      '77moxyb7enik8v',
+            apiKey: '77moxyb7enik8v',
             redirectUri: 'http://localhost:4200'
           }
         }
@@ -122,7 +123,7 @@ module.exports = function(environment) {
         'connect-src': "'self' 'unsafe-inline' ws://docker-vm:49152 https://api-ping.intercom.io https://nexus-websocket-a.intercom.io https://nexus-websocket-b.intercom.io wss://nexus-websocket-a.intercom.io wss://nexus-websocket-b.intercom.io https://api-iam.intercom.io",
         'img-src': "'self' data: *.googleusercontent.com http://www.google-analytics.com https://www.gravatar.com http://i2.wp.com/dg2kcfbxc77v1.cloudfront.net http://i0.wp.com/dg2kcfbxc77v1.cloudfront.net/assets https://js.intercomcdn.com https://static.intercomassets.com/ https://dg2kcfbxc77v1.cloudfront.net/ https://s3.amazonaws.com/datasource-logos/",
         'media-src': "'self'",
-        'frame-src':"'self' 'unsafe-inline' https://repositive.typeform.com/to/pktwPz https://repositive.typeform.com/to/viIWx1"
+        'frame-src': "'self' 'unsafe-inline' https://repositive.typeform.com/to/pktwPz https://repositive.typeform.com/to/viIWx1"
       },
       metricsAdapters: [
         {
