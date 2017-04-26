c0d541ca8754c2cc8dfa0371cb3c364ed39662bf
Sebastian Place <sebplace94@gmail.com>
2017-04-26 16:46:38 +0100

    update user model

--- app/models/user.js
+++ app/models/user.js
@@ -8,6 +8,15 @@ const { computed, isPresent, get } = Ember;
 export default Model.extend({
   createdAt: attr('isodate'),
   credentials: hasMany('credentials'),
+  firstname: attr('string'),
+  isCurrentUser: attr('boolean', { defaultValue: false }), // checks the current authenticated user
+  isEmailValidated: attr('boolean', { defaultValue: false }), // checks if email has been validated
+  lastname: attr('string'),
+  profile: attr('object'),
+  reputation: attr('object'),
+  updatedAt: attr('isodate'),
+  username: attr('string'),
+  userSetting: belongsTo('userSetting'),
   displayName: computed('username', 'firstname', 'lastname', function() {
     if (isPresent(get(this, 'firstname')) || isPresent(get(this, 'lastname'))) {
       const firstName = get(this, 'firstname') || '';
@@ -16,13 +25,5 @@ export default Model.extend({
     } else {
       return 'User' + get(this, 'username');
     }
-  }),
-  firstname: attr('string'),
-  isCurrentUser: attr('boolean', { defaultValue: false }), // checks the current authenticated user
-  isEmailValidated: attr('boolean', { defaultValue: false }), // checks if email has been validated
-  lastname: attr('string'),
-  updatedAt: attr('isodate'),
-  username: attr('string'),
-  profile: attr('object'),
-  userSetting: belongsTo('userSetting')
+  })
 });
