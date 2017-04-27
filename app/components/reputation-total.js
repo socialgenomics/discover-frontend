d9edf10bc2a5f92265d2810674233da9051abd08
Sebastian Place <sebplace94@gmail.com>
2017-04-27 11:07:30 +0100

    Add reputation colours and classes

--- app/components/reputation-total.js
+++ app/components/reputation-total.js
@@ -6,22 +6,27 @@ export default Component.extend({
   //takes reputation object
   //renders the total of the value in the reputation object
   //depending on the total, the color will change
+  classNames: ['u-p1 u-circle u-fs0 u-tc-secondary'],
+  classNameBindings: ['backgroundColour'],
   tagName: 'span',
+
   reputation: {
-    quality: 10,
+    quality: 0,
     verification: 5,
     ownership: 4,
     contribution: 1
   },
+  //this can be move to the user model
   total: computed('reputation', function() {
     const reputation = get(this, 'reputation');
     return Object.keys(reputation)
       .reduce((sum, key) => sum + reputation[key], 0)
   }),
-  colour: computed('total', function() {
+
+  backgroundColour: computed('total', function() {
     const total = get(this, 'total');
-    if (total === 0) { return 'grey'; }
-    if (total > 0 && total <= 10) { return 'yellow'; }
-    if (total > 10) { return 'teal'; }
+    if (total === 0) { return 'u-bc-very-light-grey'; }
+    if (total > 0 && total <= 10) { return 'u-bc-light-yellow'; }
+    if (total > 10) { return 'u-bc-light-teal'; }
   })
 });
