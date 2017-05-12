2123ed4035886026129a57622e4c3bb47152b1a6
Sebastian Place <sebplace94@gmail.com>
2017-05-12 19:09:28 +0100

    [B] Replace instances of resetKey to reset_key (#797)

--- app/routes/users/reset-password.js
+++ app/routes/users/reset-password.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 const { Route, get, set } = Ember;
 
 export default Route.extend({
-  resetKey: null,
+  reset_key: null,
   model(params) {
-    set(this, 'resetKey', params.resetKey);
+    set(this, 'reset_key', params.reset_key);
   },
   setupController(controller) {
-    set(controller, 'resetKey', get(this, 'resetKey'));
+    set(controller, 'reset_key', get(this, 'reset_key'));
   }
 });
