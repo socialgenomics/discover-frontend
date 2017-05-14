37b959a3c33b724c7f29168862411f8b7db1d0e9
Rafal Leszczynski <raf.leszcz@gmail.com>
2017-05-14 14:07:06 +0200

    ISSUE-777, button replacement WIP

--- app/components/rui/r-button-flat/component.js
+++
@@ -1,6 +0,0 @@
-import BaseButton from '../r-button-base/component';
-
-export default BaseButton.extend({
-  classNames: ['u-py2'],
-  classNameBindings: ['primary:c-btn-primary:c-btn-text-secondary']
-});
--- app/components/rui/r-button/component.js
+++ app/components/rui/r-button/component.js
@@ -1,10 +1,57 @@
-import BaseButton from '../r-button-base/component';
+import Ember from 'ember';
 
-export default BaseButton.extend({
+const { Component, get, Logger, set } = Ember;
+
+export default Component.extend({
+  layoutName: 'components/rui/r-button',
+  tagName: 'button',
+  attributeBindings: ['disabled'],
+  classNames: ['c-btn'],
   classNameBindings: [
-    'primary:c-btn-primary:c-btn-secondary',
-    'raised:c-btn--shadow',
+    // types
+    'primary:c-btn-primary',
+    'secondary:c-btn-secondary',
+    'cancel:c-btn-cancel',
+
+    // states
+    'loading:c-btn-loading',
+
+    // sizes
     'small:u-px2',
     'small:u-py1'
-  ]
+  ],
+
+  options: {
+    defaultType: 'secondary',
+    type: ['primary', 'secondary', 'cancel'],
+    defaultSize: 'small',
+    size: ['small', 'big']
+  },
+
+  init() {
+    this._super(...arguments);
+
+    // set option flags used for className bindings
+    ['type', 'size'].forEach(key => this._setOptionFlag(key));
+  },
+
+  click() {
+    const clickHandler = get(this, 'clickHandler');
+
+    if (clickHandler) { clickHandler(); }
+  },
+
+  /**
+   * @desc sets flag based of option value passed during component invocation
+   * @param {String} optionName
+   * @private
+   */
+  _setOptionFlag(optionName) {
+    const value = get(this, optionName);
+    const flagName = get(this, `options.${optionName}`).indexOf(value) === -1 ?
+      get(this, `options.default${optionName.capitalize()}`) :
+      value;
+
+    set(this, flagName, true);
+  }
 });
