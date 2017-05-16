f1a21b240eacdfe7bc5358dc570385a6211f47ec
Sebastian Place <sebplace94@gmail.com>
2017-05-16 11:31:04 +0100

    Test function in generative manner

--- tests/unit/utils/numbers-test.js
+++ tests/unit/utils/numbers-test.js
@@ -3,8 +3,10 @@ import { describe, it } from 'mocha';
 import { getRandomInt } from 'repositive/utils/numbers';
 
 describe('getRandomInt', function() {
-  it('should take two numbers and return a number with that range', function() {
-    expect(getRandomInt(1, 5)).to.be.least(1);
-    expect(getRandomInt(1, 5)).to.be.most(5);
+  it('should take two numbers and return a number within that range', function() {
+    for (let i = 0; i < 100; i++) {
+      expect(getRandomInt(i, i * 2)).to.be.least(i);
+      expect(getRandomInt(i, i * 2)).to.be.most(i * 2);
+    }
   });
 });
