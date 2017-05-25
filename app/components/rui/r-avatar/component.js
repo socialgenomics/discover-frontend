abf7ba6736637457a6480a3a827dbd87440a08b7
danielgynn <danielgynn@hotmail.co.uk>
2017-05-25 10:57:53 +0100

    Specify avatar size options

--- app/components/rui/r-avatar/component.js
+++ app/components/rui/r-avatar/component.js
@@ -1,7 +1,52 @@
 import Ember from 'ember';
 
-const { Component, get } = Ember;
+const { Component, get, set } = Ember;
 
 export default Component.extend({
-  classNames: ['mr2']
+  tagName: 'img',
+  classNames: ['mr2', 'circle'],
+
+  classNameBindings: [
+    'small:icon-small',
+    'medium:icon-medium',
+    'large:icon-large',
+    'x-large:icon-x-large'
+  ],
+
+  options: {
+    size: ['small', 'medium', 'large', 'x-large']
+  },
+
+  defaults: {
+    size: 'medium'
+  },
+
+  init() {
+    this._super(...arguments);
+
+    // set option flags used for className bindings
+    Object.keys(get(this, 'options')).forEach(key => this._setOptionFlag(key));
+  },
+
+  /**
+   * @desc sets flag based of option value passed during component invocation
+   * @param {String} flagType
+   * @private
+   */
+  _setOptionFlag(flagType) {
+    const flagName = get(this, flagType);
+
+    set(this, (this._isValidFlagName(flagType, flagName) ? flagName : get(this, `defaults.${flagType}`)), true);
+  },
+
+  /**
+   * @desc checks if given flag name is valid
+   * @param {String} flagType
+   * @param {String} flagName
+   * @returns {Boolean}
+   * @private
+   */
+  _isValidFlagName(flagType, flagName) {
+    return get(this, `options.${flagType}`).indexOf(flagName) !== -1;
+  }
 });
