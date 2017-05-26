ed755e2344b3b5d16361608560a00d844afa4555
danielgynn <danielgynn@hotmail.co.uk>
2017-05-26 13:50:08 +0100

    Create base tabs-list-item component

---
+++ app/components/rui/r-tab-list-item/component.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+const { Component, computed, get } = Ember;
+
+export default Component.extend({
+  classNames: ['flex-auto','border-right'],
+  classNameBindings: ['active:bc-white:bc-very-light-grey'],
+
+  active: computed(function () {
+    return window.location.pathname.indexOf(get(this, 'target').split('.').pop()) > -1;
+  })
+});
