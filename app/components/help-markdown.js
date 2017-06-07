10a3194296657e7365ca1ab7a28775054ce02a7e
Sebastian Place <sebplace94@gmail.com>
2017-06-07 15:01:52 +0100

    debugging help issue

--- app/components/help-markdown.js
+++ app/components/help-markdown.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['mb4', 'px4']
+  classNames: ['mb4', 'px4'],
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    debugger;
+  }
 });
