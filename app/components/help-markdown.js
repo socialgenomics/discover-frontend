6b88bdf78e067b0b7fd7196a3db1867044979ac2
Sebastian Place <sebplace94@gmail.com>
2017-06-07 15:16:16 +0100

    Fix help docs infinite loop

--- app/components/help-markdown.js
+++ app/components/help-markdown.js
@@ -3,10 +3,5 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['mb4', 'px4'],
-
-  didReceiveAttrs() {
-    this._super(...arguments);
-    debugger;
-  }
+  classNames: ['mb4', 'px4']
 });
--- app/routes/help/searching-for-data.js
+++ app/routes/help/searching-for-data.js
@@ -6,7 +6,7 @@ export default Route.extend({
   controllerName: 'help',
 
   model(params) {
-    return '/help/searching-for-data/' + params.query;
+    return 'help/searching-for-data/' + params.query;
   },
 
   setupController(controller, model) {
