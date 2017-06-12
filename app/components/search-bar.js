7406622c656ef6ded43796904369e59f80b7a892
Steven Williams <steve@repositive.io>
2017-06-12 09:04:22 +0100

    Update search term examples

--- app/components/search-bar.js
+++ app/components/search-bar.js
@@ -3,48 +3,47 @@ import { getRandomElement } from 'repositive/utils/arrays'
 const { Component, get, inject: { service }, setProperties, computed } = Ember;
 
 export default Component.extend({
-  queryService: service('query'),
+    queryService: service('query'),
 
-  placeholderValues: [
-    `title:cancer description:glioblastoma`,
-    `(tissue:brain OR tissue:cortex) description:methylation`,
-    `"oxidative stress" assay:RNA-Seq`,
-    `access:Open description:sleep`,
-    `(ALS OR "Amyotrophic Lateral Sclerosis" OR MND) (assay:WGS OR assay:RNA OR assay:sequencing) NOT datasource:SRA`,
-    `ethnicity:asian NOT cancer`,
-    `Caucasian sex:female`
-  ],
+    placeholderValues: [
+        `obesity AND microbiome`,
+        `autism AND assay:"RNA seq"`,
+        `infant stool AND assay:wgs`,
+        `Alzheimer's disease`,
+        `fetal epigenome`,
+        `human populations AND (assay:"Whole Genome Sequencing" OR assay:WGS)`
+    ],
 
-  init() {
-    this._super(...arguments);
-    const queryService = get(this, 'queryService');
-    setProperties(this, {
-      'query': queryService.getQueryString(),
-      'placeholder': this._getSearchPlaceholder(get(this, 'placeholderValues'))
-    });
-  },
-
-  query: computed('queryService.queryString', {
-    get() {
-      return get(this, 'queryService').getQueryString();
+    init() {
+        this._super(...arguments);
+        const queryService = get(this, 'queryService');
+        setProperties(this, {
+            'query': queryService.getQueryString(),
+            'placeholder': this._getSearchPlaceholder(get(this, 'placeholderValues'))
+        });
     },
-    set(key, value) {
-      return value;
-    }
-  }),
 
-  actions: {
-    search(query) {
-      get(this, 'search')(query.trim()); //calls search on application route
-    }
-  },
+    query: computed('queryService.queryString', {
+        get() {
+            return get(this, 'queryService').getQueryString();
+        },
+        set(key, value) {
+            return value;
+        }
+    }),
 
-  /**
-   * @desc returns random search term placeholder
-   * @returns {String}
-   * @private
-   */
-  _getSearchPlaceholder(placeholderList) {
-    return getRandomElement(placeholderList);
-  }
+    actions: {
+        search(query) {
+            get(this, 'search')(query.trim()); //calls search on application route
+        }
+    },
+
+    /**
+     * @desc returns random search term placeholder
+     * @returns {String}
+     * @private
+     */
+    _getSearchPlaceholder(placeholderList) {
+        return getRandomElement(placeholderList);
+    }
 });
