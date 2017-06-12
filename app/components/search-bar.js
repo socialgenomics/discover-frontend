857e8def3563e8287854f2bd57c083798e3fe107
Steven Williams <steve@repositive.io>
2017-06-12 09:11:54 +0100

    Fix indentation

--- app/components/search-bar.js
+++ app/components/search-bar.js
@@ -3,47 +3,47 @@ import { getRandomElement } from 'repositive/utils/arrays'
 const { Component, get, inject: { service }, setProperties, computed } = Ember;
 
 export default Component.extend({
-    queryService: service('query'),
+  queryService: service('query'),
 
-    placeholderValues: [
-        `obesity AND microbiome`,
-        `autism AND assay:"RNA seq"`,
-        `infant stool AND assay:wgs`,
-        `Alzheimer's disease`,
-        `fetal epigenome`,
-        `human populations AND (assay:"Whole Genome Sequencing" OR assay:WGS)`
-    ],
+  placeholderValues: [
+    `obesity AND microbiome`,
+    `autism AND assay:"RNA seq"`,
+    `infant stool AND assay:wgs`,
+    `Alzheimer's disease`,
+    `fetal epigenome`,
+    `human populations AND (assay:"Whole Genome Sequencing" OR assay:WGS)`
+  ],
 
-    init() {
-        this._super(...arguments);
-        const queryService = get(this, 'queryService');
-        setProperties(this, {
-            'query': queryService.getQueryString(),
-            'placeholder': this._getSearchPlaceholder(get(this, 'placeholderValues'))
-        });
+  init() {
+    this._super(...arguments);
+    const queryService = get(this, 'queryService');
+    setProperties(this, {
+      'query': queryService.getQueryString(),
+      'placeholder': this._getSearchPlaceholder(get(this, 'placeholderValues'))
+    });
+  },
+
+  query: computed('queryService.queryString', {
+    get() {
+      return get(this, 'queryService').getQueryString();
     },
-
-    query: computed('queryService.queryString', {
-        get() {
-            return get(this, 'queryService').getQueryString();
-        },
-        set(key, value) {
-            return value;
-        }
-    }),
-
-    actions: {
-        search(query) {
-            get(this, 'search')(query.trim()); //calls search on application route
-        }
-    },
-
-    /**
-     * @desc returns random search term placeholder
-     * @returns {String}
-     * @private
-     */
-    _getSearchPlaceholder(placeholderList) {
-        return getRandomElement(placeholderList);
+    set(key, value) {
+      return value;
     }
+  }),
+
+  actions: {
+    search(query) {
+      get(this, 'search')(query.trim()); //calls search on application route
+    }
+  },
+
+  /**
+   * @desc returns random search term placeholder
+   * @returns {String}
+   * @private
+   */
+  _getSearchPlaceholder(placeholderList) {
+    return getRandomElement(placeholderList);
+  }
 });
