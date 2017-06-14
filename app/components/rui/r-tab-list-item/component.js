fd697afc800eedbe5d16a7fcacf6a764ae0b013c
Sebastian Place <sebplace94@gmail.com>
2017-06-14 12:51:22 +0100

    Remove duplicated tab-list-item logic

--- app/components/rui/r-tab-list-item/component.js
+++ app/components/rui/r-tab-list-item/component.js
@@ -6,7 +6,6 @@ export default Component.extend({
   tagName: 'li',
   classNames: ['flex-auto cursor-pointer fc-secondary fs2 fw-med p2'],
   classNameBindings: ['isActive:bc-white:bc-very-light-grey', 'isActive::u-hv-bc-very-light-grey'],
-
   isActive: computed('tabName', 'activeTab', 'setActiveTab', function() {
     if (get(this, 'setActiveTab')) {
       return get(this, 'tabName') === get(this, 'activeTab');
