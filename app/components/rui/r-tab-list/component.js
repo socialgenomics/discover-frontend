70c88abb9ed0afd928c53064caa468174b0f3081
Tom Byers <hello@tombye.rs>
2017-06-15 10:29:38 +0100

    Removing everything to do with tabs from discover

--- app/components/rui/r-tab-list/component.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-const { Component } = Ember;
-
-export default Component.extend({
-  classNames: ['list-flush-left flex fc-primary ta-center mb0 r-tab-list'],
-  tagName: 'ul'
-});
