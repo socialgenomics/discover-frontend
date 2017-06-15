28a32d995a1102cf65c1809b8073f7d35cbd1d8d
Sebastian Place <sebplace94@gmail.com>
2017-06-15 12:35:01 +0100

    [S] Add buttons (#994) (#842)
    * Swap out link-button
    
    * Swap out async buttons
    
    * Swap out all r-buttons
    
    * Remove button css

--- app/components/rui/r-async-button/component.js
+++
@@ -1,4 +0,0 @@
-import AsyncButton from 'ember-async-button/components/async-button';
-import ButtonMixin from 'repositive/mixins/rui/r-button-mixin';
-
-export default AsyncButton.extend(ButtonMixin, {});
--- app/components/rui/r-link-button/component.js
+++
@@ -1,6 +0,0 @@
-import Button from '../r-button/component';
-
-export default Button.extend({
-  tagName: 'a',
-  attributeBindings: ['href', 'target']
-});
