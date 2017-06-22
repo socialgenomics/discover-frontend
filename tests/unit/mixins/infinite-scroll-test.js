c518634ab3ee8395c65d26dffadc377f3707cd23
Sebastian Place <sebplace94@gmail.com>
2017-06-22 16:50:20 +0100

    Add tests for mixin methods

--- tests/unit/mixins/infinite-scroll-test.js
+++ tests/unit/mixins/infinite-scroll-test.js
@@ -3,6 +3,8 @@ import { describe, it } from 'mocha';
 import Ember from 'ember';
 import InfiniteScrollMixin from 'repositive/mixins/infinite-scroll';
 
+const { get, set } = Ember;
+
 describe('Unit | Mixin | infinite scroll', function() {
   describe('_buildRequestObj', function() {
     it('creates an object with limit and offset', function() {
@@ -19,6 +21,23 @@ describe('Unit | Mixin | infinite scroll', function() {
 
       expect(Object.keys(subject._buildRequestObj(10, 20, { 'abc': 123 })))
         .to.have.members(['offset', 'limit', 'order[0][0]', 'order[0][1]', 'abc']);
+    });
+  });
+
+  describe('_cacheFirstResult', function() {
+    it('sets allItemsLoaded to true if the ids match', function() {
+      let InfiniteScrollObject = Ember.Object.extend(InfiniteScrollMixin);
+      let subject = InfiniteScrollObject.create();
+      set(subject, 'previousFirstResultId', 'a1');
+      subject._cacheFirstResult('a1');
+      expect(get(subject, 'allItemsLoaded')).to.be.true;
+    });
+
+    it('sets previousFirstResultId if ids do not match', function() {
+      let InfiniteScrollObject = Ember.Object.extend(InfiniteScrollMixin);
+      let subject = InfiniteScrollObject.create();
+      subject._cacheFirstResult('a1');
+      expect(get(subject, 'previousFirstResultId')).to.eql('a1');
     });
   })
 });
