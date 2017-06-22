483a74fcba89d775a59b9f8b06787651f7bf6f84
Sebastian Place <sebplace94@gmail.com>
2017-06-22 15:47:53 +0100

    Move infinite-scroll request code to mixin

--- app/routes/registrations/index.js
+++ app/routes/registrations/index.js
@@ -1,14 +1,10 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import InfiniteScrollMixin from 'repositive/mixins/infinite-scroll';
 
-const { Route, get, set, Logger, merge } = Ember;
+const { Route, get } = Ember;
 
-export default Route.extend(AuthenticatedRouteMixin, {
-  offset: 0,
-  resultsPerPage: 12,
-  previousFirstResultId: null,
-  allItemsLoaded: false,
-
+export default Route.extend(AuthenticatedRouteMixin, InfiniteScrollMixin, {
   model() {
     const offset = get(this, 'offset');
     const resultsPerPage = get(this, 'resultsPerPage');
@@ -17,54 +13,5 @@ export default Route.extend(AuthenticatedRouteMixin, {
       'dataset',
       this._buildRequestObj(offset, resultsPerPage, { 'where.user_id.$ne': 'null' }
     ));
-  },
-
-  actions: {
-    loadMore() {
-      if (!get(this, 'allItemsLoaded')) {
-        return this._loadMore(get(this, 'offset'), get(this, 'resultsPerPage'));
-      }
-    }
-  },
-
-  _loadMore(currentOffset, resultsPerPage) {
-    const newOffset = currentOffset + resultsPerPage;
-    set(this, 'offset', newOffset);
-
-    return this._makeRequest(
-      'dataset',
-      this._buildRequestObj(newOffset, resultsPerPage, { 'where.user_id.$ne': 'null' }
-    ))
-      .then(this._handleResponse.bind(this))
-      .catch(Logger.error)
-  },
-
-  _handleResponse(resp) {
-    const model = get(this, 'controller.model');
-    /**
-     * Cache first result id.
-     * Because we don't have the total number of results, this is necessary
-     * as we need a stop condition for loadMore()
-     */
-    const firstResultId = get(resp, 'firstObject.id');
-    firstResultId === get(this, 'previousFirstResultId') ?
-      set(this, 'allItemsLoaded', true) :
-      set(this, 'previousFirstResultId', firstResultId);
-
-    return model.pushObjects(get(resp, 'content'));
-  },
-
-  _buildRequestObj(offset, limit, customProps = {}) {
-    const dataObj = {
-      offset,
-      limit,
-      'order[0][0]': 'updated_at',
-      'order[0][1]': 'DESC'
-    }
-    return merge(dataObj, customProps);
-  },
-
-  _makeRequest(modelName, requestObj) {
-    return this.store.query(modelName, requestObj);
   }
 });
