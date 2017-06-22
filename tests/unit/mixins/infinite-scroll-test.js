469db9a23544d003bdce3ae2e732aa2205962a9f
Sebastian Place <sebplace94@gmail.com>
2017-06-22 15:43:53 +0100

    Add infinite-scroll mixin

---
+++ tests/unit/mixins/infinite-scroll-test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import Ember from 'ember';
+import InfiniteScrollMixin from 'repositive/mixins/infinite-scroll';
+
+describe('Unit | Mixin | infinite scroll', function() {
+  // Replace this with your real tests.
+  it('works', function() {
+    let InfiniteScrollObject = Ember.Object.extend(InfiniteScrollMixin);
+    let subject = InfiniteScrollObject.create();
+    expect(subject).to.be.ok;
+  });
+});
