a2a4e6748c98cfe9c67f6a8cd6da07261c2cb000
Rafał Leszczyński <raf.leszcz@gmail.com>
2017-06-28 10:27:20 +0200

    [F]  Disable save button if no change in user profile  (#756)
    * ISSUE-756, disable save button if no change in user profile + refactoring
    
    * ISSUE-756, fix disabled button behavior to properly reanable it when content changes

--- app/components/user-profile-external-accounts/component.js
+++ app/components/user-profile-external-accounts/component.js
@@ -6,8 +6,9 @@ import { buildValidations } from 'ember-cp-validations';
 import { errorMessages, patterns } from 'repositive/validations/validations-config';
 import { validator } from 'ember-cp-validations';
 import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
+import UserProfileMixin, { createIsDirtyComputed, createUserAttrKey } from 'repositive/mixins/user-edit-profile-mixin';
 
-const { Component, inject: { service }, Logger, set, get, isEmpty } = Ember;
+const { Component, inject: { service }, set, get, isEmpty } = Ember;
 const Validations = buildValidations({
   [createUserAttrKey('profile.accounts.googlePlus')]: validator('format', {
     regex: patterns.google,
@@ -36,17 +37,15 @@ const Validations = buildValidations({
   })
 });
 
-/**
- * @desc creates full path user attribute key
- * @param {String} key
- * @returns {String}
- */
-function createUserAttrKey(key) {
-  return `userProfileData.${key}`;
-}
+const editableAttrs = ['profile.accounts.googlePlus', 'profile.accounts.linkedIn', 'profile.accounts.twitter',
+  'profile.accounts.researchGate', 'profile.accounts.orcid'];
 
-export default Component.extend(Validations, FlashMessageMixin, {
+export default Component.extend(Validations, FlashMessageMixin, UserProfileMixin, {
   store: service(),
+
+  initialEditableAttrsValues: null,
+
+  attrsStateIsDirty: createIsDirtyComputed(editableAttrs),
 
   init() {
     this._super(...arguments);
@@ -78,6 +77,8 @@ export default Component.extend(Validations, FlashMessageMixin, {
         placeholder: 'e.g. https://orcid.org/0000-0000-0000-0000'
       }
     ]);
+
+    set(this, 'initialEditableAttrsValues', this._getEditableAttributeValues(editableAttrs));
   },
 
   didReceiveAttrs() {
@@ -85,49 +86,5 @@ export default Component.extend(Validations, FlashMessageMixin, {
     if (isEmpty(get(this, 'userProfileData.profile.accounts'))) {
       set(this, 'userProfileData.profile.accounts', {});
     }
-  },
-
-  actions: {
-    save() {
-      const userModel = get(this, 'store').peekRecord('user', get(this, 'userId'));
-      const userProfileData = get(this, 'userProfileData');
-      Object.keys(userProfileData).forEach(attr => set(userModel, attr, userProfileData[attr]));
-      return userModel
-        .save()
-        .then(this._onSaveSuccess.bind(this))
-        .catch(this._onSaveError.bind(this, userModel));
-    }
-  },
-
-
-  /**
-   * @desc save success handler
-   * @private
-   */
-  _onSaveSuccess() {
-    this._addFlashMessage('Your external accounts have been updated.', 'success');
-    get(this, 'transitionToProfile')(get(this, 'userId'));
-  },
-
-  /**
-   * @desc save error handler
-   * @param {Ember.DS.Model} userModel
-   * @param {Error} error
-   * @private
-   */
-  _onSaveError(userModel, error) {
-    userModel.rollbackAttributes();
-    Logger.error(error);
-    this._addFlashMessage('Sorry. There was a problem saving your changes.', 'warning');
-  },
-
-  /**
-   * @desc wrapper for private function for creating correct key path to user property (here just for testing purpose)
-   * @param {String }key
-   * @returns {String}
-   * @private
-   */
-  _createUserAttrKey(key) {
-    return createUserAttrKey(key);
   }
 });
