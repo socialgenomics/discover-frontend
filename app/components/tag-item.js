6727d200497d0c1523e46fbe4dd80482438f0cd6
Sebastian Place <sebplace94@gmail.com>
2017-07-03 09:23:59 +0100

    Fix tag deletion

--- app/components/tag-item.js
+++ app/components/tag-item.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
 
-const { inject: { service }, Component, computed, get, Logger } = Ember;
+const { inject: { service }, Component, computed, get } = Ember;
 
 export default Component.extend(FlashMessageMixin, {
   session: service(),
+
   classNames: ['t-tag-item', 'p2', 'bc-very-light-grey', 'mr2', 'mb2', 'rounded', 'border', 'u-hv-bc-very-light-grey', 'inline-block'],
+
   belongsToUser: computed('session', 'userId', function () {
     const currentUserId = get(this, 'session.session.authenticated.user.id');
     const tagUserId = get(this, 'userId');
@@ -17,25 +19,13 @@ export default Component.extend(FlashMessageMixin, {
   }),
 
   actions: {
-    //TODO refactor
     delete() {
       const tag = get(this, 'tag');
       if (tag) {
-        tag.destroyRecord()
-          .then(this._handleDeleteSuccess.bind(this))
-          .catch(this._handleDeleteError.bind(this));
+        return get(this, 'deleteAction')(tag);
       } else {
         get(this, 'delete')(get(this, 'tagText'));
       }
     }
-  },
-
-  _handleDeleteSuccess() {
-    this._addFlashMessage('Tag successfully deleted.', 'success');
-  },
-
-  _handleDeleteError(error) {
-    this._addFlashMessage('Something went wrong while deleting your tag. Please try again later.', 'warning');
-    Logger.error(error)
   }
 });
