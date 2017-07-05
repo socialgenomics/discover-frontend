dcf9a01983f415d35b08f7a1bdccfb2b6fc16d84
Sebastian Place <sebplace94@gmail.com>
2017-07-05 15:46:11 +0100

    Cleanup action serializer

--- app/serializers/action.js
+++ app/serializers/action.js
@@ -3,10 +3,11 @@ import JSONSerializer from 'ember-data/serializers/json';
 import DS from 'ember-data';
 import { keyForAttribute, normalizeKeyName } from './application';
 
-const { isNone, get } = Ember;
+const { isNone, get , String: { underscore } } = Ember;
 
 export default JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
   keyForAttribute: keyForAttribute,
+
   keyForRelationship: function (key, kind, method) {
     if (method === 'serialize') {
       if (key.slice(-2) === 'Id' && key !== 'userId') {
@@ -14,14 +15,15 @@ export default JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
       }
       return normalizeKeyName(key);
     } else {
-      return Ember.String.underscore(key).toLowerCase();
+      return underscore(key).toLowerCase();
     }
   },
 
   normalize(typeClass, hash) {
     const fields = get(typeClass, 'fields');
-    fields.forEach((field) => {
-      const payloadField = Ember.String.underscore(field);
+
+    fields.forEach(field => {
+      const payloadField = underscore(field);
       if (field === payloadField) { return; }
 
       hash[field] = hash[payloadField];
@@ -33,12 +35,14 @@ export default JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
       hash[keyName] = hash.actionable_id;
       delete hash.actionable_id;
     }
+
     return this._super.apply(this, arguments);
   },
 
   serializeBelongsTo(snapshot, json, relationship) {
     let key = relationship.key;
     const belongsTo = snapshot.belongsTo(key);
+
     key = this.keyForRelationship ? this.keyForRelationship(key, "belongsTo", "serialize") : key;
     //check json if actionable_id already exists
     if (isNone(json[key])) {
