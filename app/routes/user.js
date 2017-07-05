5f020b384d2a0011abd81948c99b1aec7f2459b3
Sebastian Place <sebplace94@gmail.com>
2017-07-05 15:40:55 +0100

    Add jsdoc for getUniqueIds

--- app/routes/user.js
+++ app/routes/user.js
@@ -100,9 +100,15 @@ export default Route.extend({
     });
   },
 
-  _getUniqueIds(arrayOfObjs, modelType) {
+  /**
+   * @desc returns list of each action's model id
+   * @param  {Array} arrayOfActions the actions to be reduced
+   * @param  {String} modelType name of the model you want to get ids for
+   * @return {Array} A list of model(dataset/request) ids
+   */
+  _getUniqueIds(arrayOfActions, modelType) {
     const keyName = modelType + 'Id';
-    return arrayOfObjs
+    return arrayOfActions
       .filterBy('actionable_model', modelType)
       .mapBy(keyName)
       .mapBy('id')
