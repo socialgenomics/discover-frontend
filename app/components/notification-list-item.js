b30449ae3dc22098cb9c581fe408dda815c8fa23
Sebastian Place <sebplace94@gmail.com>
2017-07-11 11:22:42 +0100

    Cleanup notification-list

--- app/components/notification-list-item.js
+++ app/components/notification-list-item.js
@@ -7,15 +7,21 @@ export default Component.extend({
   classNames: ['p3', 'pr4', 'flex', 'items-center', 'border-bottom', 'u-hv-bc-white', 'cursor-pointer', 'relative'],
 
   isAction: computed.equal('notification.properties.type', 'action'),
+  model: computed('notification.subscriptionId.{datasetId,requestId}', function() {
+    const datasetId = get(this, 'notification.subscriptionId.datasetId');
+    const requestId = get(this, 'notification.subscriptionId.requestId');
+    if (datasetId.content) { return datasetId; }
+    if (requestId.content) { return requestId; }
+    return false;
+  }),
 
   click() { this.touchEnd(); },
 
   touchEnd() {
     const notification = get(this, 'notification');
     const subscribableModel = get(notification, 'subscriptionId.subscribableModel');
-    const subscribableId = get(notification, `subscriptionId.${subscribableModel}Id.id`);
 
-    get(this, 'transitionToSubscribable')(subscribableModel, subscribableId);
+    get(this, 'transitionToSubscribable')(subscribableModel, get(this, 'model.id'));
     set(notification, 'status', 'read');
     get(this, 'close')(get(this, 'dropdown'));
     notification
