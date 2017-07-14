27c328cc34715c60ef84c789780e5589c2320de0
Sebastian Place <sebplace94@gmail.com>
2017-07-14 11:05:22 +0100

    Test for subscription serializer

---
+++ tests/unit/serializers/subscription-test.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupModelTest } from 'ember-mocha';
+
+describe('Unit | Serializer | subscription !T', function() {
+  setupModelTest('subscription', {
+    // Specify the other units that are required for this test.
+    needs: [
+      'serializer:subscription',
+      'model:user',
+      'model:notification',
+      'model:dataset',
+      'model:request'
+    ]
+  });
+
+  describe('on serialization', function() {
+    it('serialized subscription has correct keys', function() {
+      const record = this.subject();
+      const serializedRecord = record.serialize();
+
+      expect('active' in serializedRecord).to.be.true;
+      expect('subscribable_id' in serializedRecord).to.be.true;
+      expect('subscribable_model' in serializedRecord).to.be.true;
+      expect('user_id' in serializedRecord).to.be.true;
+    });
+  });
+});
