f4862b9cd42ba8a9cf96e616a7c226451c6b303d
Sebastian Place <sebplace94@gmail.com>
2017-07-14 15:17:29 +0100

    Fix dataset missing datasource data issue

--- app/serializers/dataset.js
+++ app/serializers/dataset.js
@@ -1,6 +1,7 @@
+import Ember from 'ember';
 import JSONSerializer from 'ember-data/serializers/json';
 import DS from 'ember-data';
-import { keyForAttribute } from './application';
+import { keyForAttribute, normalizeKeyName } from './application';
 
 export default JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
@@ -13,6 +14,17 @@ export default JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
     json.external_id = json.externalId;
     delete json.externalId;
     return json;
+  },
+
+  keyForRelationship: function (key, kind, method) {
+    if (method === 'serialize') {
+      if (key === 'actionableId' || key === 'subscribableId') {
+        return 'id';
+      }
+      return normalizeKeyName(key);
+    } else {
+      return Ember.String.underscore(key).toLowerCase();
+    }
   },
 
   keyForAttribute: keyForAttribute
--- app/serializers/request.js
+++ app/serializers/request.js
@@ -1,11 +1,23 @@
+import Ember from 'ember';
 import JSONSerializer from 'ember-data/serializers/json';
-import { keyForAttribute} from './application';
+import { keyForAttribute, normalizeKeyName } from './application';
 
 export default JSONSerializer.extend({
   serialize: function(snapshot) {
     let json =  snapshot._attributes;
     json.user_id = snapshot.belongsTo('userId').id;
     return json;
+  },
+
+  keyForRelationship: function (key, kind, method) {
+    if (method === 'serialize') {
+      if (key === 'actionableId' || key === 'subscribableId') {
+        return 'id';
+      }
+      return normalizeKeyName(key);
+    } else {
+      return Ember.String.underscore(key).toLowerCase();
+    }
   },
 
   keyForAttribute: keyForAttribute
