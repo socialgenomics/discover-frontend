19470acf6446920d6e77666de3aeb73f496ded65
Sebastian Place <sebplace94@gmail.com>
2017-07-20 12:35:07 +0100

    Add help content for reputation

--- app/routes/help.js
+++ app/routes/help.js
@@ -57,7 +57,8 @@ const navigationData = [
       { target: 'help.other', query: 'incorrect-tag', text: 'What should I do if a previous tag is incorrect?' },
       { target: 'help.other', query: 'contribute-discussion', text: 'How can I contribute to a discussion?' },
       { target: 'help.other', query: 'good-tag', text: 'What makes a good tag?' },
-      { target: 'help.other', query: 'format', text: 'How can I format my descriptions and discussions?' }
+      { target: 'help.other', query: 'format', text: 'How can I format my descriptions and discussions?' },
+      { target: 'help.other', query: 'reputation', text: 'What is the number next to my profile image?' }
     ]
   }
 ];
