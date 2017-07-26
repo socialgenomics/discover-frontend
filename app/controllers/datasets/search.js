f1e50d4f585fac555edeff46954aed767a40f5b2
Sebastian Place <sebplace94@gmail.com>
2017-07-26 11:55:35 +0100

    Disable request form for unauthenticated users

--- app/controllers/datasets/search.js
+++ app/controllers/datasets/search.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 import searchControllerMixin from 'repositive/mixins/search-controller';
 import RequestSubmissionMixin from 'repositive/mixins/request-submission';
 
-const { Controller } = Ember;
+const { Controller, inject: { service } } = Ember;
 
 export default Controller.extend(searchControllerMixin, RequestSubmissionMixin, {
+  session: service(),
   isRootRoute: window.location.pathname === '/'
 });
