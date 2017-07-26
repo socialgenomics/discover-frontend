149bfdcde07411a915867d77024b565c07822f41
Sebastian Place <sebplace94@gmail.com>
2017-07-26 18:22:37 +0100

    Install ember-i18n and have a play around

--- app/components/add-comment/component.js
+++ app/components/add-comment/component.js
@@ -6,8 +6,27 @@ const { Component, get, set, isEmpty, computed, inject: { service } } = Ember;
 
 const Validations = buildValidations({ comment: emptyValidator() });
 
+/**
+ * @desc builds an error message using i18n
+ * @param {string} context name of current context e.g. dataset
+ * @param {Object} errorResp the error response
+ * @return {string} User-friendly error message
+ */
+export function buildErrorMessage(context, errorResp, i18n) {
+  if ('props' in errorResp) {
+    const invalidObj = Object.keys(errorResp.props)[0];
+    const invalidObjFirstKey = Object.keys(invalidObj)[0];
+    debugger;
+    const errorPath = `${context}.${errorResp.category}.${invalidObj}.${invalidObjFirstKey}`;
+    return i18n.t(errorPath, errorResp.props);
+  }
+  return i18n.t(`${context}.${errorResp.category}.default`);
+}
+
 export default Component.extend(Validations, {
   session: service(),
+  i18n: service(),
+
   isActive: false,
 
   classNames: 'flex items-start',
@@ -17,6 +36,22 @@ export default Component.extend(Validations, {
   isValid: computed('validations.isValid', function() {
     return get(this, 'validations.isValid');
   }),
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    const context = 'dataset';
+    const errorResp = {
+      category: 'invalid-syntax',
+      props: {
+        tag: { 'min-length': '5' }
+      }
+    };
+    const i18n = get(this, 'i18n');
+    const errMsg = buildErrorMessage(context, errorResp, i18n);
+    debugger;
+    // const datasetErr = i18n.t(`${context}.${errorResp.category}`, errorResp.props);
+  },
 
   actions: {
     showButtons: function() {
---
+++ app/locales/en/translations.js
@@ -0,0 +1,10 @@
+export default {
+  'dataset': {
+    'invalid-syntax': {
+      default: 'Incorrect formatting.',
+      tag: {
+        'min-length': 'The tag must be {{tag.min-length}} characters long.'
+      }
+    }
+  }
+};
