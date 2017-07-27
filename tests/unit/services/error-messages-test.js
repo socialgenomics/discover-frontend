8b57857d7286f8df0916d14958c9075218f711fa
Sebastian Place <sebplace94@gmail.com>
2017-07-27 13:44:15 +0100

    Add unit test

--- tests/unit/services/error-messages-test.js
+++ tests/unit/services/error-messages-test.js
@@ -7,10 +7,27 @@ describe('Unit | Service | error messages', function() {
     // Specify the other units that are required for this test.
     needs: ['service:i18n']
   });
+  describe('getErrorPath', function() {
+    const context = 'dataset';
+    const fakeErrorResp = {
+      category: 'invalid-syntax',
+      props: {
+        tag: { 'min-length': '5' }
+      }
+    };
 
-  // Replace this with your real tests.
-  it('exists', function() {
-    let service = this.subject();
-    expect(service).to.be.ok;
-  });
+    it('returns path to default error message when no props provided', function() {
+      const service = this.subject();
+
+      const result = service._getErrorPath(context, { category: 'invalid-syntax' });
+      expect(result).to.eql('dataset.invalid-syntax.default');
+    });
+
+    it('returns path to most detail interpolated message', function() {
+      const service = this.subject();
+
+      const result = service._getErrorPath(context, fakeErrorResp)
+      expect(result).to.eql('dataset.invalid-syntax.tag.min-length');
+    });
+  })
 });
