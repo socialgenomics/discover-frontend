1210cbd5d7d4f2c05db0aa154e653627b96bd919
Sebastian Place <sebplace94@gmail.com>
2017-08-24 11:33:51 +0100

    Rewrite intercom metrics adapter

--- app/metrics-adapters/intercom.js
+++ app/metrics-adapters/intercom.js
@@ -1,96 +1,72 @@
 import Ember from 'ember';
 import BaseAdapter from 'ember-metrics/metrics-adapters/base';
 import objectTransforms from 'ember-metrics/utils/object-transforms';
-// import Intercom from 'intercom-client ';
+import canUseDOM from 'ember-metrics/utils/can-use-dom';
 
-const { copy, get } = Ember;
+const { $, assert, get } = Ember;
 const { compact } = objectTransforms;
 
 export default BaseAdapter.extend({
-  toStringExtension() {
-    return 'intercom';
-  },
-  /* eslint-disable */
-  init() {
-    (function() {
-      var w = window;
-      var ic = w.Intercom;
-      if (typeof ic === 'function') {
-        ic('reattach_activator');
-        ic('update', intercomSettings);
-      } else {
-        var d = document;
-        var i = function() {
-          i.c(arguments);
-        };
-        i.q = [];
-        i.c = function(args) {
-          i.q.push(args);
-        };
-        w.Intercom = i;
+  booted: false,
 
-        function l() {
-          var s = d.createElement('script');
-          s.type = 'text/javascript';
-          s.async = true;
-          s.src = 'https://widget.intercom.io/widget/blyi5u84';
-          var x = d.getElementsByTagName('script')[0];
-          x.parentNode.insertBefore(s, x);
-        }
-        if (w.attachEvent) {
-          w.attachEvent('onload', l);
-        } else {
-          w.addEventListener('load', l, false);
-        }
-      }
-    })();
+  toStringExtension() { return 'Intercom'; },
+
+  init() {
+    const { appId } = get(this, 'config');
+
+    assert(`[ember-metrics] You must pass a valid \`appId\` to the ${this.toString()} adapter`, appId);
+
+    if (canUseDOM) {
+      /* eslint-disable */
+      (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',{});}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;
+      s.src=`https://widget.intercom.io/widget/${appId}`;
+      var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);} l(); }})()
+      /* eslint-enable */
+    }
   },
-  /* eslint-enable */
 
   identify(options = {}) {
-    const config = copy(get(this, 'config'));
-    const { id } = config;
+    const { appId } = get(this, 'config');
     const compactedOptions = compact(options);
     const { email, firstname, lastname, username } = compactedOptions;
     const userId = compactedOptions.id;
     const fullname = firstname + ' ' + lastname;
-
-    window.Intercom('boot', {
-      app_id: id,
+    const method = this.booted ? 'update' : 'boot';
+    const props = {
+      app_id: appId,
       user_id: username || userId,
       name: fullname,
       email: email
-    });
+    };
 
-    window.Intercom('update', {
-      user_id: username || userId,
-      name: fullname,
-      email: email
-    });
+    if (canUseDOM) {
+      window.Intercom(method, props);
+      this.booted = true;
+    }
   },
 
   trackEvent(options = {}) {
-    let { category, action, label, value } = options;
-    let actionName = category + ' ' + action;
-    window.Intercom('trackEvent', actionName, {
-      label: label,
-      value: value
-    });
+    const { category, action, label, value } = options;
+    const actionName = category + ' ' + action;
+    const eventData = { label, value };
+    if (canUseDOM) {
+      window.Intercom('trackEvent', actionName, eventData);
+    }
   },
 
   trackPage(options = {}) {
-    let { page, title } = options;
-    let action = 'navigate: ' + title;
-    window.Intercom('trackEvent', action, {
-      page: page,
-      title: title
-    });
-  },
-
-  alias() {
+    const { page, title } = options;
+    const action = 'navigate: ' + title;
+    const eventData = { page, title };
+    if (canUseDOM) {
+      window.Intercom('trackEvent', action, eventData);
+    }
   },
 
   willDestroy() {
-    delete window.Intercom;
+    if (canUseDOM) {
+      $('script[src*="intercom"]').remove();
+      delete window.Intercom;
+    }
   }
 });
