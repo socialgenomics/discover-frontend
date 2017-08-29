3187e6377ef9504f2443df7fea3e9060ea28565f
Rafał Leszczyński <raf.leszcz@gmail.com>
2017-08-29 13:27:04 +0200

    [B] Persist meta panel visibility state in local storage (#909)

--- app/components/meta-panel/component.js
+++ app/components/meta-panel/component.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import { storageFor } from 'ember-local-storage';
 
-const { Component, set, inject: { service } } = Ember;
+
+const { Component, get, set, inject: { service } } = Ember;
 
 export default Component.extend({
   session: service(),
@@ -11,12 +13,14 @@ export default Component.extend({
   displayInfo: true,
   metaPanelHidden: false,
 
+  uiState: storageFor('uiState'),
+
   init() {
     this._super(...arguments);
 
     const VIEWPORT_WIDTH = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     //Only hide the side panel by default if on mobile.
-    if (VIEWPORT_WIDTH < 680) { set(this, 'metaPanelHidden', true) }
+    if (VIEWPORT_WIDTH < 680 || get(this, 'uiState.hideMetaPanel') ) { set(this, 'metaPanelHidden', true) }
   },
 
   didReceiveAttrs() {
@@ -29,6 +33,18 @@ export default Component.extend({
   actions: {
     showInfo() { set(this, 'displayInfo', true); },
     showFilters() { set(this, 'displayInfo', false); },
-    toggleMetaPanelVisibility() { this.toggleProperty('metaPanelHidden'); }
+    toggleMetaPanelVisibility() {
+      this.toggleProperty('metaPanelHidden');
+      this._setVisibilityPreference(get(this, 'metaPanelHidden'));
+    }
+  },
+
+  /**
+   * @desc save current hidden preference to session
+   * @param {Boolean} hidden
+   * @private
+   */
+  _setVisibilityPreference(hidden) {
+    set(this, 'uiState.hideMetaPanel', hidden);
   }
 });
