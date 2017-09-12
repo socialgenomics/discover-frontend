28000a69230ef81d83757240c85d7c5d18d61022
Sebastian Place <sebplace94@gmail.com>
2017-09-12 12:50:27 +0100

    Cleanup

--- app/services/query.js
+++ app/services/query.js
@@ -3,16 +3,7 @@ import Ember from 'ember';
 const { Service, get, set } = Ember;
 
 export default Service.extend({
-  // queryString: '',
   queryTree: null,
-
-  // getQueryString() {
-  //   return get(this, 'queryString');
-  // },
-  //
-  // setQueryString(newString) {
-  //   set(this, 'queryString', newString);
-  // },
 
   getQueryTree() {
     return get(this, 'queryTree');
