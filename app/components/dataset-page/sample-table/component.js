02e6cb35090e066299751f318c91cc1a65576337
Ruben Paz <me@ruben.io>
2017-09-19 12:03:22 +0100

    Backend send a sample summary object (#935)
    * Backend send a sample summary object
    
    * Make it not SRA specific
    
    * Remove unneeded test
    
    * Handle table headers properly
    
    * Link the url and sample id tds
    
    * Add condition to ensure url is present
    
    * Remove url from table
    
    * Remove unneeded css rule
    
    * Add basic unit tests

--- app/components/dataset-page/sample-table/component.js
+++ app/components/dataset-page/sample-table/component.js
@@ -2,11 +2,52 @@ import Ember from 'ember';
 
 const { Component, computed, get } = Ember;
 
+function toHTML(samples) {
+  function getRows() {
+    return samples.map((obj) => {
+      const row = Object.keys(obj).reduce(
+        (acc, key) => {
+          if (obj.url && (key === 'Sample ID')) {
+            const link = `<a href=${obj.url} target='_blank'>`;
+            return acc + `<td>${link}${obj[key]}</a></td>`;
+          } else if (key === 'url') {
+            return acc;
+          } else {
+            return acc + `<td>${obj[key]}</td>`;
+          }
+        },
+        ''
+      );
+      return '<tr>' + row + '</tr>';
+    }).reduce((acc, currentValue) => acc + currentValue, '');
+  }
+
+  function getHeaders() {
+    const headers = Object.keys(samples[0] || {}).map((k) => {
+      return k.split('_').map(w => {
+        const gr = w.split('')
+        return gr.shift().toUpperCase() + gr.join('');
+      })
+        .join(' ');
+    })
+      .filter(header => header !== 'Url')
+      .map(header => `<th>${header}</th>`)
+      .join('');
+
+    return `<tr>${headers}</tr>`
+  }
+
+  return `
+    <table>
+      ${getHeaders()}
+      ${getRows()}
+    </table>
+  `;
+}
+
 export default Component.extend({
   classNames: ['c-sample-table'],
-  cleanTable: computed('table', function() {
-    const table = get(this, 'table');
-    const tableStart = table.indexOf('<table');
-    return table.substring(tableStart);
+  table: computed('samples', function() {
+    return toHTML(get(this, 'samples'));
   })
 });
