6bc0151df738e2288490682ea5d5a9f9bccf45a8
Ruben Paz <me@ruben.io>
2017-09-21 20:54:44 +0100

    [B] Verify the email if the user does not have an active session

--- app/routes/users/verify.js
+++ app/routes/users/verify.js
@@ -3,7 +3,7 @@ import ENV from 'repositive/config/environment';
 import { getLatestSecondaryCredential, mainCredential, secondaryCredentials, fetchCredentials } from 'repositive/utils/credentials';
 import FlashMessageMixin from 'repositive/mixins/flash-message-mixin';
 import VerificationMixin from 'repositive/mixins/verification';
-
+s
 const { inject: { service }, get, set, Logger, Route, RSVP } = Ember;
 
 export default Route.extend(FlashMessageMixin, VerificationMixin, {
@@ -11,38 +11,40 @@ export default Route.extend(FlashMessageMixin, VerificationMixin, {
   session: service(),
 
   model(params) {
-    const userId = get(this, 'session.session.content.authenticated.user.id');
-    return RSVP.hash({
-      'verificationResp': get(this, 'ajax')
-        .request(ENV.APIRoutes['verify-email'] + '/' + params.verification_id, { method: 'GET' }),
-      'credentials': fetchCredentials(this.store, userId),
-      'user': this.store.findRecord('user', userId)
-    })
-      .then(resp => {
-        if (get(resp, 'credentials.length') === 1) {
-          return { user: resp.user }
-        }
+    return get(this, 'ajax').request(ENV.APIRoutes['verify-email'] + '/' + params.verification_id, { method: 'GET'})
+      .then(verificationResp => {
+        set(this, 'session.session.content.authenticated.token', verificationResp.token);
 
-        const credentialId = getLatestSecondaryCredential(secondaryCredentials(resp.credentials)).id;
-        set(this, 'session.session.content.authenticated.token', resp.verificationResp.token);
-
+        const self = this;
         return RSVP.hash({
-          'user': resp.user,
-          'makePrimaryResp': this._makeCredentialPrimary(credentialId)
-        });
-      })
-      .then(resp => {
-        if (get(this, 'session.isAuthenticated')) {
-          const user = resp.user;
-          set(user, 'verified', true);
-          return user.save();
-        } else {
-          this.transitionTo('root');
-        }
-      })
-      .catch(err => {
-        Logger.error(err);
-        set(this, 'controller.timeout_error', true);
+          'credentials': fetchCredentials(this.store, verificationResp.user_id),
+          'user': this.store.findRecord('user', verificationResp.user_id)
+        })
+          .then(resp => {
+            if (get(resp, 'credentials.length') === 1) {
+              return { user: resp.user }
+            }
+
+            const credentialId = getLatestSecondaryCredential(secondaryCredentials(resp.credentials)).id;
+
+            return RSVP.hash({
+              'user': resp.user,
+              'makePrimaryResp': this._makeCredentialPrimary(credentialId)
+            });
+          })
+          .then(resp => {
+            if (get(this, 'session.isAuthenticated')) {
+              const user = resp.user;
+              set(user, 'verified', true);
+              return user.save();
+            } else {
+              this.transitionTo('root');
+            }
+          })
+          .catch(err => {
+            Logger.error(err);
+            set(this, 'controller.timeout_error', true);
+          });
       });
   },
 
