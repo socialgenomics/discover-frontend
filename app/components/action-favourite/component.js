83c4454ada4c1699396e69fad55a025d08d126ef
Leo jPod <leo.jpod@lilo.org>
2018-08-21 18:00:25 +0200

    wip: the favorites are not added to the bookmark service rather than to vault

--- app/components/action-favourite/component.js
+++ app/components/action-favourite/component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-import { createActionData } from 'repositive/utils/actions';
+// import { createActionData } from 'repositive/utils/actions';
 
 const { inject: { service }, Component, computed, isPresent, Logger, get, set } = Ember;
 
@@ -13,6 +13,11 @@ export default Component.extend({
   classNames: ['fc-secondary', 'cursor-pointer' ],
   isSubmitting: false,
   showCreateAccountModal: false,
+
+
+  //data props
+  model: null,
+  modelType: null,
 
   isStarred: computed('favouritesService.userFavourites', function() {
     return isPresent(get(this, 'favouritesService')
@@ -62,16 +67,14 @@ export default Component.extend({
 
   _addFavourite() {
     const currentModel = get(this, 'model'); //can be request or dataset
-    const currentUser = get(this, 'session.authenticatedUser');
-    const store = get(this, 'store');
+    const modelType = get(this, 'modelType');
+    const favouritesService = get(this, 'favouritesService');
 
     set(this, 'isSubmitting', true);
 
-    return store
-      .createRecord('action', createActionData(currentModel, currentUser, 'favourite'))
-      .save()
-      .then(this._handleSaveSuccess.bind(this, currentModel))
-      .catch(Logger.error);
+    return favouritesService.createFavorite(get(currentModel, 'id'), modelType)
+      .then((newFav) => this._handleSaveSuccess(currentModel, newFav))
+      .catch(Logger.error)
   },
 
   _deleteFavourite(favourite) {
@@ -99,4 +102,7 @@ export default Component.extend({
       value: false
     });
   }
+}).reopenClass({
+  positionalParams: ['model', 'modelType']
+
 });
--- app/services/favourites.js
+++ app/services/favourites.js
@@ -1,14 +1,20 @@
 import Service from '@ember/service';
 import { inject as service } from '@ember/service';
 import { get, getWithDefault, set } from '@ember/object';
-import Logger from 'repositive/utils/logger';
+import {all, resolve } from 'rsvp';
+import R from 'npm:ramda';
+import ENV from 'repositive/config/environment';
+import Ember from 'ember';
+const {Logger} = Ember;
 
 export default Service.extend({
+  ajax: service(),
   store: service(),
   session: service(),
   flashMessages: service(),
 
   userFavourites: undefined, //list of actions where type = 'favourite'
+  bookmarks: null,
 
   loadFavourites() {
     if (get(this, 'userFavourites') === undefined && get(this, 'session.isAuthenticated')) { //favourites haven't been loaded yet
@@ -29,7 +35,7 @@ export default Service.extend({
   },
   fetchFavorites() {
 
-  }
+  },
 
   removeFavourite(favourite) {
     set(this, 'userFavourites', get(this, 'userFavourites').without(favourite));
@@ -38,11 +44,82 @@ export default Service.extend({
 
   pushFavourite(favourite) {
     get(this, 'userFavourites').push(favourite);
+    console.log('userFavourites -> ', get(this, 'userFavourites'));
     this.notifyPropertyChange('userFavourites');
   },
 
   getFavourite(modelId, modelName) {
     const keyName = modelName + 'Id.id';
     return getWithDefault(this, 'userFavourites', []).findBy(keyName, modelId);
+  },
+
+  async createFavorite(resource_id, resource_type) {
+    try {
+      const bookmarks = await get(this, 'bookmarks') || [];
+      const currentUserId = get(this, 'session.authenticatedUser.id');
+      console.log('currentUserId -> ', currentUserId);
+      const response = await this._createBookmark(resource_id, resource_type, currentUserId, 'user');
+      console.log('response -> ', response);
+      set(this, 'bookmarks', resolve([...bookmarks, response]));
+      return response;
+    } catch (err) {
+      Logger.error(err);
+      throw new Error("We couldn't create the bookmark. Try again later.");
+    }
+  },
+
+
+  _createBookmark(resource_id, resource_type, owner_id, owner_type) {
+    return get(this, 'ajax')
+      .request(ENV.APIRoutes['new-bookmarks']['create-bookmark'], {
+        method: 'POST',
+        contentType: 'application/json',
+        data: { resource_id, resource_type, owner_id, owner_type }
+      })
+      .then(R.prop('result'))
+      .then((bookmark) => this._loadBookmarkResource(bookmark));
+  },
+
+  _fetchCollections() {
+    return get(this, 'ajax')
+      .request(`${ENV.APIRoutes['new-bookmarks']['view-collections']}?owner_id=${get(this, 'organisationId')}`)
+      .then(R.prop('result'));
+  },
+
+  _fetchBookmarks() {
+    return get(this, 'ajax')
+      .request(`${ENV.APIRoutes['new-bookmarks']['view-bookmarks']}?owner_id=${get(this, 'organisationId')}`)
+      .then(R.prop('result'))
+      .then((bookmarks) =>
+        // TODO one day we should changed that to let the service deal with it
+        all(bookmarks.map((bookmark) => this._loadBookmarkModel(bookmark)))
+      ).then((bookmarks) =>
+        // TODO
+        all(bookmarks.map((bookmark) => this._loadBookmarkModelOwner(bookmark)))
+      );
+  },
+
+  _loadBookmarkResource(bookmark) {
+    const { resource_type, resource_id } = bookmark;
+    const store = get(this, 'store');
+    return store.findRecord(resource_type, resource_id)
+      .then((resource) => {
+        return { ...bookmark, resource };
+      });
+  },
+
+  _loadBookmarkModelOwner(bookmark) {
+    const ownerId = get(bookmark, 'model.organisation_id.value');
+    if (ownerId) {
+      return get(this, 'ajax').request(ENV.APIRoutes['organisation-profile'].replace('{id}', ownerId))
+        .then(R.prop('result'))
+        .then((model_owner) => {
+          return { ...bookmark, model_owner };
+        });
+    } else {
+      return bookmark;
+    }
   }
+
+
 });
