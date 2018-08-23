7b4a3af1aa86d48a3e2dc710444687ab4958bbbc
Leo jPod <leo.jpod@lilo.org>
2018-08-23 17:39:55 +0200

    apply the same logic to the request model

--- app/models/request.js
+++ app/models/request.js
@@ -1,16 +1,28 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { inject as service } from '@ember/service';
 import { belongsTo, hasMany } from 'ember-data/relationships';
+import { get } from '@ember/object';
+import computed from  'ember-macro-helpers/computed';
 
 export default Model.extend({
   createdAt: attr('isodate'),
   description: attr('string'),
-  stats: attr('object'),
+  _stats: attr('object'),
   title: attr('string'),
   isNHLBI: attr('boolean'),
   updatedAt: attr('isodate'),
 
   actions: hasMany('action'),
   subscriptions: hasMany('subscription'),
-  userId: belongsTo('user')
+  userId: belongsTo('user'),
+
+  stats: computed('_stats', function (stats) {
+    return {
+      ...stats,
+      favourite: get(this, 'favourites').getCount(get(this, 'id'))
+    };
+  }),
+
+  favourites: service()
 });
