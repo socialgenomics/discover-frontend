f1bc3902afe047873bb4c6c29862fbc58bff5bfb
Leo jPod <leo.jpod@lilo.org>
2018-09-17 12:06:49 +0200

    Revert "start to fix both DIS-266 and DIS-259"
    This reverts commit 158fb0910e7835a5c730b6252752b586285193a2.

--- app/models/request.js
+++ app/models/request.js
@@ -2,10 +2,12 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
-export default Model.extend({
+import CanBeFavMixin from 'repositive/mixins/can-be-fav-model-mixin';
+
+export default Model.extend(CanBeFavMixin, {
   createdAt: attr('isodate'),
   description: attr('string'),
-  stats: attr('object'),
+  _stats: attr('object'),
   title: attr('string'),
   isNHLBI: attr('boolean'),
   updatedAt: attr('isodate'),
