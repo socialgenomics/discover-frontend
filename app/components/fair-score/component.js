cc7dd50922c4b03ad1911dd9bae457a6d33b82d2
Leo Jpod <leojpod@users.noreply.github.com>
2018-09-27 15:38:24 +0200

    [F] - DIS-219 - basic implementation for the FAIRness score visualisation (#1041)
    * basic implementation for the FAIRness score
    
    * open the link in a new window to prevent the weird back error
    
    * manually hack around to prevent opening the fair score links
    
    * remove log

---
+++ app/components/fair-score/component.js
@@ -0,0 +1,34 @@
+import Component from '@ember/component';
+import { get } from '@ember/object';
+import { htmlSafe } from '@ember/string';
+import coreapi from 'npm:coreapi'
+import insignia from 'npm:fairshakeinsignia';
+import schema from 'npm:fairshakeinsignia/dist/schema.js';
+
+export default Component.extend({
+  attributeBindings: ['style'],
+  doi: null,
+
+  style: htmlSafe('width: 40px; height: 40px;'),
+
+  didRender() {
+    this._super(...arguments);
+
+    const elem = get(this, 'element');
+    const url = get(this, 'doi');
+    const client = new coreapi.Client();
+    client.action(schema, ['score', 'list'], {url})
+      .then((results) => {
+        insignia.build_svg(
+          elem,
+          results.scores,
+          {
+            tooltips: (rubric, metric, score) =>
+              `Score: ${(score * 100).toFixed(0)}%<br />${results.metrics[metric]}`
+          }
+        );
+      });
+  }
+}).reopen({
+  positionalParams: ['doi']
+});
