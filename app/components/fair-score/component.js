7ef2fd1cdfb0951b77c1c3659bdeb0642eb31ffa
Leo jPod <leo.jpod@lilo.org>
2018-09-27 13:47:25 +0200

    manually hack around to prevent opening the fair score links

--- app/components/fair-score/component.js
+++ app/components/fair-score/component.js
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
 import { get } from '@ember/object';
 import { htmlSafe } from '@ember/string';
+import coreapi from 'npm:coreapi'
 import insignia from 'npm:fairshakeinsignia';
+import schema from 'npm:fairshakeinsignia/dist/schema.js';
 
 export default Component.extend({
   attributeBindings: ['style'],
@@ -14,10 +16,18 @@ export default Component.extend({
 
     const elem = get(this, 'element');
     const url = get(this, 'doi');
-    insignia.build_svg_from_score(
-      elem,
-      { url }
-    );
+    const client = new coreapi.Client();
+    client.action(schema, ['score', 'list'], {url})
+      .then((results) => {
+        insignia.build_svg(
+          elem,
+          results.scores,
+          {
+            tooltips: (rubric, metric, score) =>
+              `Score: ${(score * 100).toFixed(0)}%<br />${results.metrics[metric]}`
+          }
+        );
+      });
   }
 }).reopen({
   positionalParams: ['doi']
