e0caa6fb35bb373b73eb7d51a1c07de5c80623a5
Leo jPod <leo.jpod@lilo.org>
2018-10-02 15:33:52 +0200

    update the components for the new r-avatar

--- app/components/r-avatar/component.js
+++ app/components/r-avatar/component.js
@@ -1,5 +1,9 @@
 import RAvatar from 'ui-library/components/r-avatar/component';
+import { reads } from '@ember/object/computed';
 
 export default RAvatar.extend({
-  fallbackSrc: '/assets/images/avatar/dog.png'
+  fallbackSrc: '/assets/images/avatar/dog.png',
+  src: reads('imageSrc')
+}).reopenClass({
+  positionalParams: ['imageSrc']
 });
