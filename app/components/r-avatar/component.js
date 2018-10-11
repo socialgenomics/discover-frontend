44d14a0fd3f7efd9bf95f76f9eb5a568bff1a362
Leo Jpod <leojpod@users.noreply.github.com>
2018-10-11 14:57:49 +0200

    [F] - fix the avatar issue... (#1043)
    * fix the avatar issue...
    
    * remove logs

--- app/components/r-avatar/component.js
+++ app/components/r-avatar/component.js
@@ -1,9 +1,17 @@
 import RAvatar from 'ui-library/components/r-avatar/component';
-import { reads } from '@ember/object/computed';
+import { get, set } from '@ember/object';
+import { observer } from '@ember/object';
 
 export default RAvatar.extend({
   fallbackSrc: '/assets/images/avatar/dog.png',
-  src: reads('imageSrc')
+  checkImage: observer('imageSrc', function () {
+    const imageSrc = get(this, 'imageSrc');
+    set(this, 'src', imageSrc);
+  }),
+  init() {
+    set(this, 'src', get(this, 'imageSrc'));
+    this._super(...arguments);
+  }
 }).reopenClass({
   positionalParams: ['imageSrc']
 });
