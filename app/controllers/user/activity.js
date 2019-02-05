1946d216ab693fd67a045bc6996fad596840c6bb
Jesus Cano <jcanovel@gmail.com>
2019-02-05 16:01:50 +0000

    Remove Favourites from Activity

--- app/controllers/user/activity.js
+++ app/controllers/user/activity.js
@@ -1,20 +1,14 @@
 import Controller from '@ember/controller';
-import { get } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import computed from 'ember-macro-helpers/computed';
-import R from 'npm:ramda';
-
 
 export default Controller.extend({
   session: service(),
   favourites: service(),
 
   user: alias('model.user'),
-  favouriteResources: computed('user.id', 'favourites.othersBookmarks', (userId, bookmarksPerUserId) =>
-    get(bookmarksPerUserId, userId)
-      .then(R.map(R.prop('resource')))
-  ),
+
   isOwnProfile: computed('model.user.id', 'session.authenticatedUser.id', function(profileUserId, sessionUserId) {
     if (sessionUserId === profileUserId) {
       return true;
