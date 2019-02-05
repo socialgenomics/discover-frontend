376263bbcb7ef0d82180a97401c87486c1340df0
Jesus Cano <jcanovel@gmail.com>
2019-02-05 16:29:29 +0000

    DIS-344 Split profile and activity (#1067)
    * Initial commit
    
    * Add user.js parent route
    
    * Style of the page header
    
    * Update app/routes/user/activity.js
    
    Co-Authored-By: jpcano <jcanovel@gmail.com>
    
    * Update app/routes/user/index.js
    
    Co-Authored-By: jpcano <jcanovel@gmail.com>
    
    * Add missed bracket
    
    * Remove Favourites from Activity
    
    * Remove favourites requests in parent route
    
    * Remove unused service

--- app/controllers/user/activity.js
+++ app/controllers/user/activity.js
@@ -3,11 +3,11 @@ import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import computed from 'ember-macro-helpers/computed';
 
-
 export default Controller.extend({
   session: service(),
 
   user: alias('model.user'),
+
   isOwnProfile: computed('model.user.id', 'session.authenticatedUser.id', function(profileUserId, sessionUserId) {
     if (sessionUserId === profileUserId) {
       return true;
