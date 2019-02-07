557968d52e8a1986226ea82471e65aae168c6e9d
Jesus Cano <jcanovel@gmail.com>
2019-02-07 17:23:42 +0000

    Link collections and favourites on the profile card

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -2,15 +2,29 @@ import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import computed from 'ember-macro-helpers/computed';
-
+import { get } from "@ember/object";
 
 export default Controller.extend({
   session: service(),
+  favourites: service(),
+  collections: service(),
 
   user: alias('model.user'),
   isOwnProfile: computed('model.user.id', 'session.authenticatedUser.id', function(profileUserId, sessionUserId) {
     if (sessionUserId === profileUserId) {
       return true;
     }
-  })
+  }),
+
+  allUserBookmarks: computed(
+    "user.id",
+    "favourites.bookmarksPerUserId",
+    (userId, bookmarksPerUserId) => get(bookmarksPerUserId, userId)
+  ),
+
+  allUserCollections: computed(
+    "user.id",
+    "collections.collectionsPerUserId",
+    (userId, collectionsPerUserId) => get(collectionsPerUserId, userId)
+  )
 });
