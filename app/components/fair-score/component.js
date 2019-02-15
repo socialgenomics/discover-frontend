272b625c7f9a5dd4491f64b629af208ce93caba7
Leo jPod <leo.jpod@lilo.org>
2019-02-15 14:12:31 +0100

    [B] DIS-376 default FAIRscores to something more consistent

--- app/components/fair-score/component.js
+++ app/components/fair-score/component.js
@@ -1,34 +1,29 @@
-import Component from '@ember/component';
-import { get } from '@ember/object';
-import { htmlSafe } from '@ember/string';
-import coreapi from 'npm:coreapi';
-import insignia from 'npm:fairshakeinsignia';
-import schema from 'npm:fairshakeinsignia/dist/schema.js';
+import Component from "@ember/component";
+import { get } from "@ember/object";
+import { htmlSafe } from "@ember/string";
+import coreapi from "npm:coreapi";
+import insignia from "npm:fairshakeinsignia";
+import schema from "npm:fairshakeinsignia/dist/schema.js";
 
 export default Component.extend({
-  attributeBindings: ['style'],
+  attributeBindings: ["style"],
   doi: null,
 
-  style: htmlSafe('width: 40px; height: 40px;'),
+  style: htmlSafe("width: 40px; height: 40px;"),
 
   didRender() {
     this._super(...arguments);
 
-    const elem = get(this, 'element');
-    const url = get(this, 'doi');
+    const elem = get(this, "element");
+    const url = get(this, "doi") || "no-doi-given";
     const client = new coreapi.Client();
-    client.action(schema, ['score', 'list'], {url})
-      .then((results) => {
-        insignia.build_svg(
-          elem,
-          results.scores,
-          {
-            tooltips: (rubric, metric, score) =>
-              `Score: ${(score * 100).toFixed(0)}%<br />${results.metrics[metric]}`
-          }
-        );
+    client.action(schema, ["score", "list"], { url }).then(results => {
+      insignia.build_svg(elem, results.scores, {
+        tooltips: (rubric, metric, score) =>
+          `Score: ${(score * 100).toFixed(0)}%<br />${results.metrics[metric]}`
       });
+    });
   }
 }).reopen({
-  positionalParams: ['doi']
+  positionalParams: ["doi"]
 });
